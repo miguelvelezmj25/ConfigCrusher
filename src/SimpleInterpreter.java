6f734e7a908931cb59babc37544f3da43aeacfe3
Miguel Velez <miguelvelez@mijecu25.com>
2017-01-27 16:35:05 -0500

    Inital commit

---
+++ src/SimpleInterpreter.java
@@ -0,0 +1,12 @@
+/**
+ * SimpleInterpreter class.
+ *
+ * @author Miguel Velez - miguelvelezmj25
+ * @version 0.1.0
+ */
+public class SimpleInterpreter {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}

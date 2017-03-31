8e56d29f82575b42886b4f0d9d7bf1dbb2410041
Miguel Velez <miguelvelez@mijecu25.com>
2017-03-31 15:21:10 -0400

    Executed Incling algorithm with a feature model

--- src/main/java/edu/cmu/cs/mvelezce/analysis/Helper.java
+++ src/main/java/edu/cmu/cs/mvelezce/analysis/Helper.java
@@ -1,9 +1,16 @@
 package edu.cmu.cs.mvelezce.analysis;
 
+import de.ovgu.featureide.fm.core.base.IFeature;
 import de.ovgu.featureide.fm.core.base.IFeatureModel;
-import de.ovgu.featureide.fm.core.base.impl.Feature;
-import de.ovgu.featureide.fm.core.base.impl.FeatureModel;
+import de.ovgu.featureide.fm.core.base.IFeatureModelFactory;
+import de.ovgu.featureide.fm.core.base.impl.DefaultFeatureModelFactory;
+import de.ovgu.featureide.fm.core.base.impl.FMFactoryManager;
+import de.ovgu.featureide.fm.core.configuration.Selection;
 import de.ovgu.featureide.fm.core.editing.AdvancedNodeCreator;
+import de.ovgu.featureide.fm.core.filter.AbstractFeatureFilter;
+import de.ovgu.featureide.fm.core.job.LongRunningWrapper;
+import de.ovgu.featureide.fm.core.job.monitor.IMonitor;
+import de.ovgu.featureide.fm.core.job.monitor.NullMonitor;
 import edu.cmu.cs.mvelezce.language.ast.expression.ExpressionConfigurationConstant;
 import org.apache.commons.math3.util.Combinations;
 import org.prop4j.Node;
@@ -20,30 +27,68 @@ import java.util.*;
  */
 public class Helper {
 
-    public void incling() {
-//        final AdvancedNodeCreator advancedNodeCreator = new AdvancedNodeCreator(fm, new AbstractFeatureFilter());
-//        advancedNodeCreator.setCnfType(AdvancedNodeCreator.CNFType.Regular);
-//        advancedNodeCreator.setIncludeBooleanValues(false);
-//
-//        Node createNodes = advancedNodeCreator.createNodes();
-//        SatInstance satInstance = new SatInstance(createNodes);
-//        PairWiseConfigurationGenerator gen = getGenerator(satInstance, solutionCount);
-//        exec(satInstance, gen, monitor);
+    public void incling() throws InterruptedException {
+        IFeatureModelFactory factory = DefaultFeatureModelFactory.getInstance();
+        IFeatureModel featureModel = factory.createFeatureModel();
+        IFeature feature = factory.createFeature(featureModel, "root");
+        featureModel.addFeature(feature);
+        featureModel.getStructure().setRoot(feature.getStructure());
 
-        IFeatureModel fm = new FeatureModel("miguel");
-        fm.addFeature(new Feature(fm, "A"));
-        AdvancedNodeCreator advancedNodeCreator = new AdvancedNodeCreator(fm);
-//        advancedNodeCreator.setCnfType(AdvancedNodeCreator.CNFType.Regular);
-//        advancedNodeCreator.setIncludeBooleanValues(false);
+        feature = factory.createFeature(featureModel, "A");
+        featureModel.addFeature(feature);
+        feature.getStructure().addChild(feature.getStructure());
+
+        feature = factory.createFeature(featureModel, "B");
+        featureModel.addFeature(feature);
+        feature.getStructure().addChild(feature.getStructure());
+
+        feature = factory.createFeature(featureModel, "C");
+        featureModel.addFeature(feature);
+        feature.getStructure().addChild(feature.getStructure());
+
+        feature = factory.createFeature(featureModel, "D");
+        featureModel.addFeature(feature);
+        feature.getStructure().addChild(feature.getStructure());
+
+        AdvancedNodeCreator advancedNodeCreator = new AdvancedNodeCreator(featureModel, new AbstractFeatureFilter());
+        advancedNodeCreator.setCnfType(AdvancedNodeCreator.CNFType.Regular);
+        advancedNodeCreator.setIncludeBooleanValues(false);
+
         Node createNodes = advancedNodeCreator.createNodes();
-        createNodes.setChildren(createNodes);
         SatInstance satInstance = new SatInstance(createNodes);
-        PairWiseConfigurationGenerator gen = getGenerator(satInstance, 10);
-        int a = 0;
+        PairWiseConfigurationGenerator generator = new PairWiseConfigurationGenerator(satInstance, 200);
+//        satInstance.convertToString(generator.q.take().getModel());
+
+        this.exec(satInstance, generator, new NullMonitor());
+
     }
 
-    protected PairWiseConfigurationGenerator getGenerator(SatInstance solver, int solutionCount) {
-        return new PairWiseConfigurationGenerator(solver, solutionCount);
+    protected void exec(final SatInstance satInstance, final PairWiseConfigurationGenerator generator, IMonitor monitor) {
+        final Thread consumer = new Thread() {
+            @Override
+            public void run() {
+                int foundConfigurations = 0;
+                while (true) {
+                    try {
+                        generateConfiguration(satInstance.convertToString(generator.q.take().getModel()));
+                        foundConfigurations++;
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
+                foundConfigurations += generator.q.size();
+                for (org.prop4j.analyses.PairWiseConfigurationGenerator.Configuration c : generator.q) {
+                    generateConfiguration(satInstance.convertToString(c.getModel()));
+                }
+            }
+
+            private void generateConfiguration(List<String> solution) {
+                System.out.println(solution);
+            }
+        };
+        consumer.start();
+        LongRunningWrapper.runMethod(generator, monitor);
+        consumer.interrupt();
     }
 
 

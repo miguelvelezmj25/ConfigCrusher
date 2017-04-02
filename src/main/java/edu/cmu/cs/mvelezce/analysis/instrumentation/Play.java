7fc0e0cfd176aaeb0bcfbd0b268cc8c0cc50862b
Miguel Velez <miguelvelez@mijecu25.com>
2017-04-02 17:35:42 -0400

    Added code to read class file with asm

--- src/main/java/edu/cmu/cs/mvelezce/analysis/instrumentation/Play.java
+++ src/main/java/edu/cmu/cs/mvelezce/analysis/instrumentation/Play.java
@@ -2,7 +2,9 @@ package edu.cmu.cs.mvelezce.analysis.instrumentation;
 
 import jdk.internal.org.objectweb.asm.ClassReader;
 import jdk.internal.org.objectweb.asm.tree.ClassNode;
+import jdk.internal.org.objectweb.asm.tree.MethodNode;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.jar.JarEntry;
@@ -34,7 +36,19 @@ public class Play {
 //                System.out.println(classNode.name);
             }
         }
+    }
 
+    public static void readFile(String fileName) throws IOException {
+        ClassReader classReader = new ClassReader(fileName);
+        ClassNode classNode =  new ClassNode();
+        classReader.accept(classNode, 0);
+
+        for(MethodNode method : classNode.methods) {
+//            if(method.name.equals(("inc"))) {
+//                System.out.println(method.localVariables);
+//                System.out.println(method.instructions);
+//            }
+        }
     }
 
 }
--- src/test/java/edu/cmu/cs/mvelezce/analysis/instrumentation/PlayTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/analysis/instrumentation/PlayTest.java
@@ -6,10 +6,15 @@ import org.junit.Test;
  * Created by mvelezce on 3/31/17.
  */
 public class PlayTest {
-    @Test
-    public void readJar() throws Exception {
-        Play.readJar("org.sat4j.core.jar");
 
+    @Test
+    public void testReadJar() throws Exception {
+        Play.readJar("org.sat4j.core.jar");
+    }
+
+    @Test
+    public void testReadFile() throws Exception {
+        Play.readFile("edu/cmu/cs/mvelezce/analysis/instrumentation/Play");
     }
 
 }

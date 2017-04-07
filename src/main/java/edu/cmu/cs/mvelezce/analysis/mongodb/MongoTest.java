ded22e5a66a8cb2b9e64bb7e7776f13d702bf878
Miguel Velez <miguelvelez@mijecu25.com>
2017-04-06 20:29:44 -0400

    Tested querying mongo

--- src/main/java/edu/cmu/cs/mvelezce/analysis/mongodb/MongoTest.java
+++ src/main/java/edu/cmu/cs/mvelezce/analysis/mongodb/MongoTest.java
@@ -2,6 +2,7 @@ package edu.cmu.cs.mvelezce.analysis.mongodb;
 
 import edu.cmu.cs.mvelezce.mongo.connector.Casbah;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -11,14 +12,57 @@ import java.util.Map;
 public class MongoTest {
 
     public static void test() {
-        List<Map<String, String>> query = Casbah.connect();
+        List<String> fields = new ArrayList<>();
+        fields.add("Package");
+        fields.add("Class");
+        fields.add("Method");
+        fields.add("JavaLineNo");
+        fields.add("JimpleLineNo");
+        fields.add("Constraint");
+        fields.add("ConstraintPretty");
+        fields.add("bytecodeIndexes");
+        fields.add("methodBytecodeSignatureJoanaStyle");
 
+        List<String> sortBy = new ArrayList<>();
+        fields.add("Package");
+        fields.add("Class");
+        fields.add("Method");
+        fields.add("JimpleLineNo");
+
+        List<Map<String, String>> query = Casbah.connect("platypus", fields, sortBy);
+
+        String currentMethod = "";
         for(Map<String, String> result : query) {
+            String method = result.get("Method");
+
+//            if(!method.contains("loadCommandFile")) {
+//                continue;
+//            }
+
+//            if(method.contains("createCommandLine") || method.contains("appendFormatExtension") || method.contains("dummy") || method.contains("<init>") || method.contains("getRoot") || method.contains("notExecutedInCodeSection")) {
+//                continue;
+//            }
+//
+//            if(currentMethod.isEmpty()) {
+//                currentMethod = method;
+//            }
+//
+//            if(!result.get("Method").equals(currentMethod)) {
+//                break;
+//            }
+
+//            if(result.get("Constraint").equals("true")) {
+//                continue;
+//            }
+
             for(Map.Entry<String, String> entry : result.entrySet()) {
-                System.out.println(entry.getKey() + ":" + entry.getValue());
+//                String key = entry.getKey();
+
+//                if(key.equals("JavaLineNo") || key.equals("Package") || key.equals("Class") || key.equals("Method") || key.equals("JimpleLineNo") || key.equals("Constraint")) {
+                    System.out.println(entry.getKey() + ":" + entry.getValue());
+//                }
             }
+            System.out.println();
         }
-
-
     }
 }

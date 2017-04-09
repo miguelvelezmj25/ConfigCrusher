97d33293450ac1476bd891b3d9f26ffaf3e9fcf0
Miguel Velez <miguelvelez@mijecu25.com>
2017-04-09 18:27:32 -0400

    Added behavior to keep track of Region objects

--- src/main/java/edu/cmu/cs/mvelezce/analysis/taint/Regions.java
+++ src/main/java/edu/cmu/cs/mvelezce/analysis/taint/Regions.java
@@ -1,8 +1,27 @@
 package edu.cmu.cs.mvelezce.analysis.taint;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by miguelvelez on 4/7/17.
  */
 public class Regions {
-//    static Region region1 = new Region("", "", "");
+    // This looks weird
+    public static Map<Region, Region> regions = new HashMap<>();
+
+    public static Region getRegion(String regionPackage, String regionClass, String regionMethod) {
+        // TODO check for empty strings
+        Region hold = new Region(regionPackage, regionClass, regionMethod);
+        Region result = Regions.regions.get(hold);
+
+        if(result == null) {
+            // TODO this is for testing. We would change this for the full pipeline
+//            throw new NoSuchElementException("This region is not a relevant region");
+            Regions.regions.put(hold, hold);
+            return hold;
+        }
+
+        return result;
+    }
 }

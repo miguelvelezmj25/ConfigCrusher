5e5397a7186e920f3c9292ef2f7f3c3bdc2274f6
Miguel Velez <miguelvelez@mijecu25.com>
2017-04-23 14:00:01 -0400

    Made the sleep class work by itself

--- src/main/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/programs/Sleep.java
+++ src/main/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/programs/Sleep.java
@@ -10,8 +10,23 @@ public class Sleep {
     public static final String CLASS = Sleep.class.getSimpleName();
     public static final String METHOD_1 = "program1";
 
-    public void program1(boolean A) throws InterruptedException {
-        // Region program start
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("main");
+        program1(Boolean.valueOf(args[0]));
+        //        // Region program start
+//        boolean a = Boolean.getBoolean(args[0]);
+//        Thread.sleep(200);
+//        if(a) {
+//            // Region A start
+//            Thread.sleep(300);
+//            // Region A end
+//        }
+//        Thread.sleep(100);
+//        // Region program end
+    }
+
+    public static void program1(boolean A) throws InterruptedException {
+        System.out.println(A);
         boolean a = A;
         Thread.sleep(200);
         if(a) {
@@ -20,7 +35,6 @@ public class Sleep {
             // Region A end
         }
         Thread.sleep(100);
-        // Region program end
     }
 
 }
--- src/test/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/transformer/JavaRegionClassTransformerPrinterTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/transformer/JavaRegionClassTransformerPrinterTest.java
@@ -21,7 +21,7 @@ public class JavaRegionClassTransformerPrinterTest {
     public void testTransform1() throws IOException, CloneNotSupportedException {
         // Java Region
         // Indexes were gotten by looking at output of running ClassTransformerBaseTest
-        JavaRegion region = new JavaRegion(Sleep.PACKAGE, Sleep.CLASS, Sleep.METHOD_1, 14, 15);
+        JavaRegion region = new JavaRegion(Sleep.PACKAGE, Sleep.CLASS, Sleep.METHOD_1, 19, 20);
         Regions.addRegion(region);
 
         // Get class
@@ -50,10 +50,13 @@ public class JavaRegionClassTransformerPrinterTest {
 
         Assert.assertTrue(transformed);
 
-//        printer.writeClass(classNode, ClassTransformerBaseTest.CLASS_CONTAINER + Sleep.FILENAME.replace(".", "/"));
-//        String command = "java -cp " + ClassTransformerBaseTest.CLASS_CONTAINER + " " + Main.FILENAME + " " + Sleep.CLASS.toLowerCase() + " 1 A";
-//        String output = ClassTransformerBaseTest.executeCommand(command);
-//        Assert.assertNotEquals(0, output.length());
+        // Actually modify the class file
+        printer.writeClass(classNode, ClassTransformerBaseTest.CLASS_CONTAINER + Sleep.FILENAME.replace(".", "/"));
+
+        String command = "java -cp " + ClassTransformerBaseTest.CLASS_CONTAINER + " " + Sleep.FILENAME  + " true";
+        String output = ClassTransformerBaseTest.executeCommand(command);
+
+        Assert.assertNotEquals(0, output.length());
     }
 
 }

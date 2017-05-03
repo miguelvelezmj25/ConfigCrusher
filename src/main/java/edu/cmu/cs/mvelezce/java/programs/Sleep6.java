d021917bfb00a9f6579442dc230803b626b1ebf2
Miguel Velez <miguelvelez@mijecu25.com>
2017-05-03 18:55:08 -0400

    Tested more programs

--- src/main/java/edu/cmu/cs/mvelezce/java/programs/Sleep6.java
+++ src/main/java/edu/cmu/cs/mvelezce/java/programs/Sleep6.java
@@ -3,11 +3,11 @@ package edu.cmu.cs.mvelezce.java.programs;
 /**
  * Created by mvelezce on 4/21/17.
  */
-public class Sleep7 {
+public class Sleep6 {
 
-    public static final String FILENAME = Sleep7.class.getCanonicalName();
-    public static final String PACKAGE = Sleep7.class.getPackage().getName();
-    public static final String CLASS = Sleep7.class.getSimpleName();
+    public static final String FILENAME = Sleep6.class.getCanonicalName();
+    public static final String PACKAGE = Sleep6.class.getPackage().getName();
+    public static final String CLASS = Sleep6.class.getSimpleName();
     public static final String MAIN_METHOD = "main";
 
     public static void main(String[] args) throws InterruptedException {
@@ -15,18 +15,15 @@ public class Sleep7 {
         System.out.println("main");
         boolean a = Boolean.valueOf(args[0]);
         Thread.sleep(200);
-
-        int repeat;
         if(a) {
-            repeat = 5;
+            Thread.sleep(600);
         }
         else {
-            repeat = 10;
+            Thread.sleep(700);
+            return;
         }
 
-        for(int i=0; i < repeat; i++) {
-            Thread.sleep(100);
-        }
+        Thread.sleep(100);
         // Region program end
     }
 

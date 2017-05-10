fb6423f526bdfde40baa19cd813d15ff770d8e4b
Miguel Velez <miguelvelez@mijecu25.com>
2017-05-09 21:34:50 -0400

    Checked bytecode graph for long lines

--- src/test/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/bytecode/MethodGraphBuilderTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/bytecode/MethodGraphBuilderTest.java
@@ -1,7 +1,7 @@
 package edu.cmu.cs.mvelezce.tool.instrumentation.java.bytecode;
 
 import edu.cmu.cs.mvelezce.java.programs.*;
-import edu.cmu.cs.mvelezce.tool.instrumentation.java.transformer.ClassTransformerBase;
+import edu.cmu.cs.mvelezce.tool.instrumentation.java.transformer.ClassTransformerReader;
 import jdk.internal.org.objectweb.asm.tree.ClassNode;
 import jdk.internal.org.objectweb.asm.tree.MethodNode;
 import org.junit.Assert;
@@ -9,7 +9,6 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Created by mvelezce on 5/3/17.
@@ -18,18 +17,8 @@ public class MethodGraphBuilderTest {
 
     @Test
     public void testBuildMethodGraph1() throws IOException {
-        ClassTransformerBase base = new ClassTransformerBase() {
-            @Override
-            public Set<ClassNode> transformClasses() throws IOException {
-                return null;
-            }
-
-            @Override
-            public void transform(ClassNode classNode) {
-
-            }
-        };
-        ClassNode classNode = base.readClass(Sleep1.FILENAME);
+        ClassTransformerReader reader = new ClassTransformerReader();
+        ClassNode classNode = reader.readClass(Sleep1.FILENAME);
 
         List<MethodNode> methods = classNode.methods;
 
@@ -44,19 +33,26 @@ public class MethodGraphBuilderTest {
     }
 
     @Test
+    public void testBuildMethodGraph1Dash1() throws IOException {
+        ClassTransformerReader reader = new ClassTransformerReader();
+        ClassNode classNode = reader.readClass(Sleep1Dash1.FILENAME);
+
+        List<MethodNode> methods = classNode.methods;
+
+        for(MethodNode method : methods) {
+            if(method.name.equals("main")) {
+                MethodGraph methodGraph = MethodGraphBuilder.buildMethodGraph(method);
+                System.out.println(methodGraph.toDotString(method.name));
+                Assert.assertEquals(7, methodGraph.getBlockCount());
+                Assert.assertEquals(5, methodGraph.getEdgeCount());
+            }
+        }
+    }
+
+    @Test
     public void testBuildMethodGraph2() throws IOException {
-        ClassTransformerBase base = new ClassTransformerBase() {
-            @Override
-            public Set<ClassNode> transformClasses() throws IOException {
-                return null;
-            }
-
-            @Override
-            public void transform(ClassNode classNode) {
-
-            }
-        };
-        ClassNode classNode = base.readClass(Sleep2.FILENAME);
+        ClassTransformerReader reader = new ClassTransformerReader();
+        ClassNode classNode = reader.readClass(Sleep2.FILENAME);
 
         List<MethodNode> methods = classNode.methods;
 
@@ -72,18 +68,8 @@ public class MethodGraphBuilderTest {
 
     @Test
     public void testBuildMethodGraph3() throws IOException {
-        ClassTransformerBase base = new ClassTransformerBase() {
-            @Override
-            public Set<ClassNode> transformClasses() throws IOException {
-                return null;
-            }
-
-            @Override
-            public void transform(ClassNode classNode) {
-
-            }
-        };
-        ClassNode classNode = base.readClass(Sleep3.FILENAME);
+        ClassTransformerReader reader = new ClassTransformerReader();
+        ClassNode classNode = reader.readClass(Sleep3.FILENAME);
 
         List<MethodNode> methods = classNode.methods;
 
@@ -99,18 +85,8 @@ public class MethodGraphBuilderTest {
 
     @Test
     public void testBuildMethodGraph4() throws IOException {
-        ClassTransformerBase base = new ClassTransformerBase() {
-            @Override
-            public Set<ClassNode> transformClasses() throws IOException {
-                return null;
-            }
-
-            @Override
-            public void transform(ClassNode classNode) {
-
-            }
-        };
-        ClassNode classNode = base.readClass(Sleep4.FILENAME);
+        ClassTransformerReader reader = new ClassTransformerReader();
+        ClassNode classNode = reader.readClass(Sleep4.FILENAME);
 
         List<MethodNode> methods = classNode.methods;
 
@@ -126,18 +102,8 @@ public class MethodGraphBuilderTest {
 
     @Test
     public void testBuildMethodGraph5() throws IOException {
-        ClassTransformerBase base = new ClassTransformerBase() {
-            @Override
-            public Set<ClassNode> transformClasses() throws IOException {
-                return null;
-            }
-
-            @Override
-            public void transform(ClassNode classNode) {
-
-            }
-        };
-        ClassNode classNode = base.readClass(Sleep5.FILENAME);
+        ClassTransformerReader reader = new ClassTransformerReader();
+        ClassNode classNode = reader.readClass(Sleep5.FILENAME);
 
         List<MethodNode> methods = classNode.methods;
 
@@ -153,18 +119,8 @@ public class MethodGraphBuilderTest {
 
     @Test
     public void testBuildMethodGraph6() throws IOException {
-        ClassTransformerBase base = new ClassTransformerBase() {
-            @Override
-            public Set<ClassNode> transformClasses() throws IOException {
-                return null;
-            }
-
-            @Override
-            public void transform(ClassNode classNode) {
-
-            }
-        };
-        ClassNode classNode = base.readClass(Sleep6.FILENAME);
+        ClassTransformerReader reader = new ClassTransformerReader();
+        ClassNode classNode = reader.readClass(Sleep6.FILENAME);
 
         List<MethodNode> methods = classNode.methods;
 
@@ -180,18 +136,8 @@ public class MethodGraphBuilderTest {
 
     @Test
     public void testBuildMethodGraph7() throws IOException {
-        ClassTransformerBase base = new ClassTransformerBase() {
-            @Override
-            public Set<ClassNode> transformClasses() throws IOException {
-                return null;
-            }
-
-            @Override
-            public void transform(ClassNode classNode) {
-
-            }
-        };
-        ClassNode classNode = base.readClass(Sleep7.FILENAME);
+        ClassTransformerReader reader = new ClassTransformerReader();
+        ClassNode classNode = reader.readClass(Sleep7.FILENAME);
 
         List<MethodNode> methods = classNode.methods;
 
@@ -207,18 +153,8 @@ public class MethodGraphBuilderTest {
 
     @Test
     public void testBuildMethodGraph8() throws IOException {
-        ClassTransformerBase base = new ClassTransformerBase() {
-            @Override
-            public Set<ClassNode> transformClasses() throws IOException {
-                return null;
-            }
-
-            @Override
-            public void transform(ClassNode classNode) {
-
-            }
-        };
-        ClassNode classNode = base.readClass(Sleep8.FILENAME);
+        ClassTransformerReader reader = new ClassTransformerReader();
+        ClassNode classNode = reader.readClass(Sleep8.FILENAME);
 
         List<MethodNode> methods = classNode.methods;
 
@@ -234,18 +170,8 @@ public class MethodGraphBuilderTest {
 
     @Test
     public void testBuildMethodGraph9() throws IOException {
-        ClassTransformerBase base = new ClassTransformerBase() {
-            @Override
-            public Set<ClassNode> transformClasses() throws IOException {
-                return null;
-            }
-
-            @Override
-            public void transform(ClassNode classNode) {
-
-            }
-        };
-        ClassNode classNode = base.readClass(Sleep9.FILENAME);
+        ClassTransformerReader reader = new ClassTransformerReader();
+        ClassNode classNode = reader.readClass(Sleep9.FILENAME);
 
         List<MethodNode> methods = classNode.methods;
 
@@ -261,18 +187,8 @@ public class MethodGraphBuilderTest {
 
     @Test
     public void testBuildMethodGraph10() throws IOException {
-        ClassTransformerBase base = new ClassTransformerBase() {
-            @Override
-            public Set<ClassNode> transformClasses() throws IOException {
-                return null;
-            }
-
-            @Override
-            public void transform(ClassNode classNode) {
-
-            }
-        };
-        ClassNode classNode = base.readClass(Sleep10.FILENAME);
+        ClassTransformerReader reader = new ClassTransformerReader();
+        ClassNode classNode = reader.readClass(Sleep10.FILENAME);
 
         List<MethodNode> methods = classNode.methods;
 

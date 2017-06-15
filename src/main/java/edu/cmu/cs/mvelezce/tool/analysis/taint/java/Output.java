06f9beb7774bb4c6bff2a02ad9598e3916f975f3
Miguel Velez <miguelvelez@mijecu25.com>
2017-06-15 14:01:40 -0400

    Added script to check output of lotrack

---
+++ src/main/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/Output.java
@@ -0,0 +1,126 @@
+package edu.cmu.cs.mvelezce.tool.analysis.taint.java;
+
+import edu.cmu.cs.mvelezce.mongo.connector.scaladriver.ScalaMongoDriverConnector;
+import org.apache.commons.io.FileUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Created by mvelezce on 6/15/17.
+ */
+public class Output {
+
+    public static final String PACKAGE = "Package";
+    public static final String CLASS = "Class";
+    public static final String METHOD = "Method";
+    public static final String JAVA_LINE_NO = "JavaLineNo";
+    public static final String CONSTRAINT = "Constraint";
+    public static final String CONSTRAINT_PRETTY = "ConstraintPretty";
+    public static final String BYTECODE_INDEXES = "bytecodeIndexes";
+    public static final String USED_TERMS = "usedTerms";
+
+    public static final String LOTRACK_UNKNOWN_CONSTRAINT_SYMBOL = "_";
+
+    public static void main(String[] args) throws NoSuchFieldException, ParseException {
+
+        String root = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/dummy/src/";
+//        String root = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/jarchivelib/src/main/java/";
+        String[] extensions = {"java"};
+
+        Collection<File> files = FileUtils.listFiles(new File(root), extensions, true);
+        List<String> dbEntries = getDBEntries("loadtime", "Tests");
+
+        for(File file : files) {
+            Map<Integer, String> linesToConstraints = new HashMap<>();
+
+            for(String result : dbEntries) {
+                JSONParser parser = new JSONParser();
+                JSONObject entry = (JSONObject) parser.parse(result);
+                String c = (String) entry.get(Output.CLASS);
+
+                if(!c.contains(".")) {
+                    continue;
+                }
+
+                c = c.substring(c.lastIndexOf(".") + 1);
+                if (!file.getName().endsWith(c + ".java")) {
+                    continue;
+                }
+
+                String constraint = (String) entry.get(Output.CONSTRAINT);
+
+                if(constraint.equals("true") || constraint.equals("false")) {
+                    continue;
+                }
+
+                int javaLineNumber = (int)(long) entry.get(Output.JAVA_LINE_NO);
+                linesToConstraints.put(javaLineNumber, constraint);
+            }
+
+            if(linesToConstraints.isEmpty()) {
+                continue;
+            }
+
+            StringBuilder lotrackedFile = new StringBuilder();
+
+            try {
+                FileInputStream fstream = new FileInputStream(root + file.getPath().replace(root, ""));
+                DataInputStream in = new DataInputStream(fstream);
+                BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                String strLine;
+                int lineNumber = 1;
+
+                while((strLine = br.readLine()) != null) {
+//                    lotrackedFile.append(lineNumber).append("    ");
+                    lotrackedFile.append(strLine).append("    ");
+
+                    if(linesToConstraints.containsKey(lineNumber)) {
+                        lotrackedFile.append("// ").append(linesToConstraints.get(lineNumber));
+                    }
+
+                    lotrackedFile.append("\n");
+                    lineNumber++;
+                }
+
+                in.close();
+
+                try(PrintWriter out = new PrintWriter(file.getAbsolutePath().replace(".java", "") + "LOTRACK.java")  ){
+                    out.print(lotrackedFile.toString());
+                }
+            }
+            catch (Exception e){
+                System.err.println("Error: " + e.getMessage());
+            }
+        }
+    }
+
+    public static List<String> getDBEntries(String database, String program) throws NoSuchFieldException, ParseException {
+        // This is hardcode to get the output of Lotrack
+        List<String> fields = new ArrayList<>();
+        fields.add(Output.PACKAGE);
+        fields.add(Output.CLASS);
+        fields.add(Output.METHOD);
+        fields.add(Output.JAVA_LINE_NO);
+        fields.add(Output.CONSTRAINT);
+        fields.add(Output.CONSTRAINT_PRETTY);
+        fields.add(Output.BYTECODE_INDEXES);
+        fields.add(Output.USED_TERMS);
+
+        List<String> sortBy = new ArrayList<>();
+        sortBy.add(Output.PACKAGE);
+        sortBy.add(Output.CLASS);
+        sortBy.add(Output.METHOD);
+        sortBy.add(Output.JAVA_LINE_NO);
+
+        ScalaMongoDriverConnector.connect(database);
+        List<String> queryResult = ScalaMongoDriverConnector.findProjectionAscending(program, fields, sortBy);
+        ScalaMongoDriverConnector.close();
+
+        return queryResult;
+    }
+
+}

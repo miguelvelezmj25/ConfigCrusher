7daea843f1065ca7c7e485e649d904786fe2f565
Miguel Velez <miguelvelez@mijecu25.com>
2017-06-18 16:38:28 -0400

    Tested same program with different sleep values

--- src/test/java/edu/cmu/cs/mvelezce/tool/pipeline/java/JavaPipelineTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/pipeline/java/JavaPipelineTest.java
@@ -47,14 +47,14 @@ public class JavaPipelineTest {
         String entryPoint = "edu.cmu.cs.mvelezce.Sleep14";
 
         // Program arguments
-        String[] args = new String[0];
+//        String[] args = new String[0];
 
 //        String[] args = new String[1];
 //        args[0] = "-saveres";
 
-//        String[] args = new String[2];
-//        args[0] = "-delres";
-//        args[1] = "-saveres";
+        String[] args = new String[2];
+        args[0] = "-delres";
+        args[1] = "-saveres";
         // TODO do not instrument if not needed. Check with args
 
         PerformanceModel pm = JavaPipeline.buildPerformanceModel(programName, args, srcDirectory, classDirectory, entryPoint);

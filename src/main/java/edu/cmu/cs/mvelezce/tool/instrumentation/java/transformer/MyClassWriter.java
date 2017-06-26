2584a44f915344123754ccbacdc0edeb7ca6dd06
Miguel Velez <miguelvelez@mijecu25.com>
2017-06-26 14:19:19 -0400

    Added support to identify overloaded methods

---
+++ src/main/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/transformer/MyClassWriter.java
@@ -0,0 +1,41 @@
+package edu.cmu.cs.mvelezce.tool.instrumentation.java.transformer;
+
+import jdk.internal.org.objectweb.asm.ClassWriter;
+
+/**
+ * Created by mvelezce on 6/26/17.
+ */
+public class MyClassWriter extends ClassWriter {
+
+    public MyClassWriter(int i) {
+        super(i);
+    }
+
+    @Override
+    protected String getCommonSuperClass(String type1, String type2) {
+        ClassLoader type5 = this.getClass().getClassLoader();
+        Class type3;
+        Class type4;
+        try {
+            type3 = Class.forName(type1.replace('/', '.'), false, type5);
+            type4 = Class.forName(type2.replace('/', '.'), false, type5);
+        } catch (Exception type7) {
+            throw new RuntimeException(type7.toString());
+        }
+
+        if(type3.isAssignableFrom(type4)) {
+            return type1;
+        } else if(type4.isAssignableFrom(type3)) {
+            return type2;
+        } else if(!type3.isInterface() && !type4.isInterface()) {
+            do {
+                type3 = type3.getSuperclass();
+            } while(!type3.isAssignableFrom(type4));
+
+            return type3.getName().replace('.', '/');
+        } else {
+            return "java/lang/Object";
+        }
+    }
+
+}

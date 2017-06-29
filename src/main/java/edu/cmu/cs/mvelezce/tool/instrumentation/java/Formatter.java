0fe5a277eb33d3bb3643a72f72770ab2467e654e
Miguel Velez <miguelvelez@mijecu25.com>
2017-06-29 10:22:05 -0400

    Automated formatting both original and instrumented systems

--- src/main/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/Formatter.java
+++ src/main/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/Formatter.java
@@ -8,7 +8,9 @@ import edu.cmu.cs.mvelezce.tool.instrumentation.java.transformer.VariableBeforeR
 import jdk.internal.org.objectweb.asm.tree.ClassNode;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -18,49 +20,77 @@ public class Formatter {
 
     public static final String TARGET_DIRECTORY = "../performance-mapper-evaluation/instrumented";
 
-    public static void format(String[] args, String srcDirectory, String classDirectory) throws IOException, InterruptedException {
+    public static void format(String[] args, String originalSrcDirectory, String originalClassDirectory, String instrumentedSrcDirectory, String instrumentedClassDirectory) throws IOException, InterruptedException {
 //        args = new String[]{"-delres"};
         Options.getCommandLine(args);
 
         if(Options.checkIfDeleteResult()) {
-            Formatter.format(srcDirectory, classDirectory);
+            Formatter.format(originalSrcDirectory, originalClassDirectory, instrumentedSrcDirectory, instrumentedClassDirectory);
         }
     }
 
-    public static void format(String srcDirectory, String classDirectory) throws IOException, InterruptedException {
-        String[] command = {"find", srcDirectory.substring(0, srcDirectory.length()-1), "-name", "*.java"};
-        Process process = Runtime.getRuntime().exec(command);
-        process.waitFor();
+    public static void format(String originalSrcDirectory, String originalClassDirectory, String instrumentedSrcDirectory, String instrumentedClassDirectory) throws IOException, InterruptedException {
+        List<String> args = new ArrayList<>();
+        args.add(originalSrcDirectory);
+        args.add(originalClassDirectory);
+        args.add(instrumentedSrcDirectory);
+        args.add(instrumentedClassDirectory);
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(srcDirectory + "sources.txt"));
-        String line;
+        for(int i = 0; i < 2; i++) {
+            String srcDir = args.get(i * 2);
+            String classDir = args.get((i * 2) + 1);
+            String[] command = {"find", srcDir.substring(0, srcDir.length() - 1), "-name", "*.java"};
+            Process process = Runtime.getRuntime().exec(command);
+            process.waitFor();
 
-        while ((line= reader.readLine()) !=null) {
-            writer.write(line);
-            writer.write("\n");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(srcDir + "sources.txt"));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.write("\n");
+            }
+
+            writer.close();
+
+            command = new String[]{"javac", "-cp", "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/instrumented/elevator/lib/*",
+                    "-d", classDir, "@" + srcDir + "sources.txt"};
+            System.out.println(Arrays.toString(command));
+            process = Runtime.getRuntime().exec(command);
+            process.waitFor();
+
+            reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
         }
 
-        writer.close();
-
-        command = new String[]{"javac", "-cp", "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/instrumented/elevator/lib/*", "-d", classDirectory, "@" + srcDirectory + "sources.txt"};
-        System.out.println(Arrays.toString(command));
-        process = Runtime.getRuntime().exec(command);
-        process.waitFor();
-
-        reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-
-        while ((line= reader.readLine()) !=null) {
-            System.out.println(line);
-        }
-
-        BasicClassTransformer timer = new VariableBeforeReturnTransformer(classDirectory);
+        BasicClassTransformer timer = new VariableBeforeReturnTransformer(originalClassDirectory);
         Set<ClassNode> classNodes = timer.transformClasses();
 
         for(ClassNode classNode : classNodes) {
             String fileName = classNode.name;
-            System.out.println(fileName);
-            timer.writeClass(classNode, classDirectory + fileName);
+            System.out.println(originalClassDirectory + fileName);
+            timer.writeClass(classNode, originalClassDirectory + fileName);
+        }
+
+        for(ClassNode classNode : classNodes) {
+            String fileName = classNode.name;
+            timer.writeClass(classNode, originalClassDirectory + fileName);
+
+            String[] command = new String[]{"cp", originalClassDirectory + fileName + ".class", instrumentedClassDirectory + fileName + ".class"};
+            System.out.println(Arrays.toString(command));
+            Process process = Runtime.getRuntime().exec(command);
+            process.waitFor();
+
+            String line;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
         }
     }
 

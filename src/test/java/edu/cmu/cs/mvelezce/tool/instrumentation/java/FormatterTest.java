ca6eaa07e70632b1bcd84184495b2daf275017b4
Miguel Velez <miguelvelez@mijecu25.com>
2017-06-29 10:22:05 -0400

    Automated formatting both original and instrumented systems

--- src/test/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/FormatterTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/FormatterTest.java
@@ -13,18 +13,22 @@ public class FormatterTest {
 
     @Test
     public void instrumentSleep() throws IOException, InterruptedException {
-        String classDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/dummy/out/production/dummy/";
-        String srcDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/dummy/";
+        String originalClassDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/dummy/out/production/dummy/";
+        String originalSrcDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/dummy/";
+        String instrumentedClassDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/instrumented/dummy/out/production/dummy/";
+        String instrumentedSrcDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/instrumented/dummy/";
 
-        Formatter.format(srcDirectory, classDirectory);
+        Formatter.format(originalSrcDirectory, originalClassDirectory, instrumentedSrcDirectory, instrumentedClassDirectory);
     }
 
     @Test
     public void instrumentElevator() throws IOException, InterruptedException {
-        String classDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/elevator/out/production/elevator/";
-        String srcDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/elevator/";
+        String originalClassDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/elevator/out/production/elevator/";
+        String originalSrcDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/elevator/";
+        String instrumentedClassDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/instrumented/elevator/out/production/elevator/";
+        String instrumentedSrcDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/instrumented/elevator/";
 
-        Formatter.format(srcDirectory, classDirectory);
+        Formatter.format(originalSrcDirectory, originalClassDirectory, instrumentedSrcDirectory, instrumentedClassDirectory);
     }
 
 }

9c1f225c2e373c81765ac2b419adaf736cb371f8
Miguel Velez <miguelvelez@mijecu25.com>
2017-07-05 20:10:14 -0400

    Fixed how statistics are calculated for executions

--- src/test/java/edu/cmu/cs/mvelezce/tool/execute/java/BruteForceTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/execute/java/BruteForceTest.java
@@ -1,15 +1,9 @@
 package edu.cmu.cs.mvelezce.tool.execute.java;
 
-import edu.cmu.cs.mvelezce.test.util.repeat.Repeat;
-import edu.cmu.cs.mvelezce.test.util.repeat.RepeatRule;
-import edu.cmu.cs.mvelezce.tool.Options;
 import edu.cmu.cs.mvelezce.tool.performance.PerformanceEntry;
 import org.json.simple.parser.ParseException;
-import org.junit.Rule;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Set;
 
@@ -18,49 +12,6 @@ import java.util.Set;
  */
 public class BruteForceTest {
 
-    public static void saveBFPerformance(String programName, Set<PerformanceEntry> measuredPerf) throws IOException {
-        File file = new File(edu.cmu.cs.mvelezce.tool.execute.java.BruteForce.BF_RES_DIR + "/" + programName + Options.DOT_CSV);
-
-        if(file.exists()) {
-            if(!file.delete()) {
-                throw new RuntimeException("Could not delete " + file);
-            }
-        }
-
-        StringBuilder result = new StringBuilder();
-        result.append("measured,configuration,performance");
-        result.append("\n");
-
-        for(PerformanceEntry perfEntry : measuredPerf) {
-            if(perfEntry.getRegionToInnerRegions().size() != 1) {
-                throw new RuntimeException("The performance entry should only have measured the entire program " + perfEntry.getRegionToInnerRegions().keySet());
-            }
-
-            result.append("true");
-            result.append(",");
-            result.append('"');
-            result.append(perfEntry.getConfiguration());
-            result.append('"');
-            result.append(",");
-            long perf = perfEntry.getRegionsToExecutionTime().values().iterator().next();
-            result.append(perf / 1000000000.0);
-            result.append("\n");
-        }
-
-        File directory = new File(edu.cmu.cs.mvelezce.tool.execute.java.BruteForce.BF_RES_DIR);
-
-        if (!directory.exists()) {
-            directory.mkdirs();
-        }
-
-        String outputFile = directory + "/" + programName + Options.DOT_CSV;
-        file = new File(outputFile);
-        FileWriter writer = new FileWriter(file, true);
-        writer.write(result.toString());
-        writer.flush();
-        writer.close();
-    }
-
     @Test
     public void testElevator() throws IOException, ParseException, InterruptedException {
         String programName = "elevator";
@@ -68,9 +19,8 @@ public class BruteForceTest {
         String classDir = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/elevator/out/production/elevator/";
         String entryPoint = "edu.cmu.cs.mvelezce.PL_Interface_impl";
 
-//        Set<PerformanceEntry> measuredPerf = BruteForce.measure(programName, 5, srcDir, classDir, entryPoint);
-        Set<PerformanceEntry> measuredPerf = BruteForce.measure(programName, 5);
-        BruteForceTest.saveBFPerformance(programName, measuredPerf);
+//        Set<PerformanceEntry> measuredPerf = BruteForce.repeatMeasure(programName, 5, srcDir, classDir, entryPoint);
+        Set<PerformanceEntry> measuredPerf = BruteForce.repeatMeasure(programName, 5);
     }
 
     @Test
@@ -80,8 +30,7 @@ public class BruteForceTest {
         String classDir = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/zipme/out/production/zipme/";
         String entryPoint = "edu.cmu.cs.mvelezce.ZipMain";
 
-        Set<PerformanceEntry> measuredPerf = BruteForce.measure(programName, 1, srcDir, classDir, entryPoint);
-        BruteForceTest.saveBFPerformance(programName, measuredPerf);
+        Set<PerformanceEntry> measuredPerf = BruteForce.repeatMeasure(programName, 1, srcDir, classDir, entryPoint);
     }
 
 }

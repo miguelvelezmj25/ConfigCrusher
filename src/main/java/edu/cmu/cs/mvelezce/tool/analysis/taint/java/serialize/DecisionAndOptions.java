b907825a333b9f3b5be3b187e3419824f0bca28b
Miguel Velez <miguelvelez@mijecu25.com>
2017-08-25 16:22:40 -0400

    Reading and writing results

--- src/main/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/serialize/DecisionAndOptions.java
+++ src/main/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/serialize/DecisionAndOptions.java
@@ -10,11 +10,13 @@ import edu.cmu.cs.mvelezce.tool.analysis.region.JavaRegion;
 import java.io.IOException;
 import java.util.Set;
 
-@JsonDeserialize(using = DecisionAndOptions.DecisionAndOptionsDeserializer.class)
+//@JsonDeserialize(using = DecisionAndOptions.DecisionAndOptionsDeserializer.class)
 public class DecisionAndOptions {
 
     private JavaRegion region;
     private Set<Set<String>> options;
+
+    public DecisionAndOptions() { ; }
 
     public DecisionAndOptions(JavaRegion region, Set<Set<String>> options) {
         this.region = region;
@@ -37,25 +39,25 @@ public class DecisionAndOptions {
         this.options = options;
     }
 
-    public static class DecisionAndOptionsDeserializer extends JsonDeserializer<DecisionAndOptions> {
-
-        @Override
-        public DecisionAndOptions deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
-            ObjectCodec oc = jsonParser.getCodec();
-            JsonNode node = oc.readTree(jsonParser);
-
-            ObjectMapper mapper = new ObjectMapper();
-            ObjectReader reader = mapper.readerFor(new TypeReference<Set<Set<String>>>() {
-            });
-            Set<Set<String>> optionsSet = reader.readValue(node.get("options"));
-
-            JsonNode region = node.get("region");
-
-            JavaRegion javaRegion = new JavaRegion(region.get("regionID").asText(), region.get("regionPackage").asText(),
-                    region.get("regionClass").asText(), region.get("regionMethod").asText(),
-                    region.get("startBytecodeIndex").intValue());
-
-            return new DecisionAndOptions(javaRegion, optionsSet);
-        }
-    }
+//    public static class DecisionAndOptionsDeserializer extends JsonDeserializer<DecisionAndOptions> {
+//
+//        @Override
+//        public DecisionAndOptions deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+//            ObjectCodec oc = jsonParser.getCodec();
+//            JsonNode node = oc.readTree(jsonParser);
+//
+//            ObjectMapper mapper = new ObjectMapper();
+//            ObjectReader reader = mapper.readerFor(new TypeReference<Set<Set<String>>>() {
+//            });
+//            Set<Set<String>> optionsSet = reader.readValue(node.get("options"));
+//
+//            JsonNode region = node.get("region");
+//
+//            JavaRegion javaRegion = new JavaRegion(region.get("regionID").asText(), region.get("regionPackage").asText(),
+//                    region.get("regionClass").asText(), region.get("regionMethod").asText(),
+//                    region.get("startBytecodeIndex").intValue());
+//
+//            return new DecisionAndOptions(javaRegion, optionsSet);
+//        }
+//    }
 }

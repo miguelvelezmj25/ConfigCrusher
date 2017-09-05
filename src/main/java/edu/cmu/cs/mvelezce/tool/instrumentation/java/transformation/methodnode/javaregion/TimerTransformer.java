9cd6115c7aaa591054380bfcda525ac9c072cc3e
Miguel Velez <miguelvelez@mijecu25.com>
2017-09-05 14:25:53 -0400

    Instrumented methods

---
+++ src/main/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/transformation/methodnode/javaregion/TimerTransformer.java
@@ -0,0 +1,40 @@
+package edu.cmu.cs.mvelezce.tool.instrumentation.java.transformation.methodnode.javaregion;
+
+import edu.cmu.cs.mvelezce.tool.analysis.region.JavaRegion;
+import edu.cmu.cs.mvelezce.tool.instrumentation.java.instrument.classnode.ClassTransformer;
+import edu.cmu.cs.mvelezce.tool.instrumentation.java.instrument.classnode.DefaultBaseClassTransformer;
+import jdk.internal.org.objectweb.asm.Opcodes;
+import jdk.internal.org.objectweb.asm.tree.InsnList;
+import jdk.internal.org.objectweb.asm.tree.LdcInsnNode;
+import jdk.internal.org.objectweb.asm.tree.MethodInsnNode;
+
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.util.Set;
+
+public class TimerTransformer extends JavaRegionTransformer {
+
+    public TimerTransformer(String directory, Set<JavaRegion> regions) throws InvocationTargetException, NoSuchMethodException, MalformedURLException, IllegalAccessException {
+        super(directory, regions);
+        ClassTransformer classTransformer = new DefaultBaseClassTransformer(directory);
+        this.setClassTransformer(classTransformer);
+    }
+
+    @Override
+    public InsnList addInstructionsStartRegion(JavaRegion javaRegion) {
+        InsnList instructionsStartRegion = new InsnList();
+        instructionsStartRegion.add(new LdcInsnNode(javaRegion.getRegionID()));
+        instructionsStartRegion.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "edu/cmu/cs/mvelezce/tool/analysis/region/Regions", "enter", "(Ljava/lang/String;)V", false));
+
+        return instructionsStartRegion;
+    }
+
+    @Override
+    public InsnList addInstructionsEndRegion(JavaRegion javaRegion) {
+        InsnList instructionsEndRegion = new InsnList();
+        instructionsEndRegion.add(new LdcInsnNode(javaRegion.getRegionID()));
+        instructionsEndRegion.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "edu/cmu/cs/mvelezce/tool/analysis/region/Regions", "exit", "(Ljava/lang/String;)V", false));
+
+        return instructionsEndRegion;
+    }
+}

fdee133fb0df980ab5911c4718dfe45cdc2d0096
Miguel Velez <miguelvelez@mijecu25.com>
2017-09-08 15:48:35 -0400

    Got the taint analysis result from color counter

--- src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/taintflow/TaintFlowAnalysisTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/taintflow/TaintFlowAnalysisTest.java
@@ -4,8 +4,20 @@ import edu.cmu.cs.mvelezce.tool.analysis.taint.java.StaticAnalysis;
 import org.junit.Test;
 
 public class TaintFlowAnalysisTest {
+
     @Test
-    public void analyzeTest() throws Exception {
+    public void analyzeTest1() throws Exception {
+        String systemName = "running-example";
+
+        // Program arguments
+        String[] args = new String[0];
+
+        StaticAnalysis analysis = new TaintFlowAnalysis(systemName);
+        analysis.analyze(args);
+    }
+
+    @Test
+    public void runningExample() throws Exception {
         String systemName = "running-example";
 
         // Program arguments
@@ -18,14 +30,15 @@ public class TaintFlowAnalysisTest {
     }
 
     @Test
-    public void analyzeTest2() throws Exception {
-        String systemName = "running-example";
+    public void colorCounter() throws Exception {
+        String systemName = "pngtasticColorCounter";
 
         // Program arguments
-        String[] args = new String[0];
+        String[] args = new String[2];
+        args[0] = "-delres";
+        args[1] = "-saveres";
 
         StaticAnalysis analysis = new TaintFlowAnalysis(systemName);
         analysis.analyze(args);
     }
-
 }

10296af0c8d6ebb78a4c19bfc7395e1e6fd7deae
Miguel Velez <miguelvelez@mijecu25.com>
2017-09-14 15:53:17 -0400

    Added code to print pretty graph after instrumentation

--- src/main/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/BaseRegionInstrumenter.java
+++ src/main/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/BaseRegionInstrumenter.java
@@ -1,10 +1,13 @@
 package edu.cmu.cs.mvelezce.tool.instrumentation.java;
 
+import edu.cmu.cs.mvelezce.tool.Options;
 import edu.cmu.cs.mvelezce.tool.analysis.region.JavaRegion;
 
 import java.util.Set;
 
 public abstract class BaseRegionInstrumenter extends BaseInstrumenter {
+
+    public static final String DIRECTORY = Options.DIRECTORY + "/instrumentation/java/programs";
 
     private Set<JavaRegion> regions;
 

29ef19c83aadac6f6238764c1763d9ad8d60b70c
Miguel Velez <miguelvelez@mijecu25.com>
2017-09-14 14:53:20 -0400

    Testing special case of multiple returns

--- src/test/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/TimerInstrumenterTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/TimerInstrumenterTest.java
@@ -256,6 +256,32 @@ public class TimerInstrumenterTest {
     }
 
     @Test
+    public void graph0() throws NoSuchMethodException, IOException, IllegalAccessException, InvocationTargetException {
+        String programName = "graph0";
+        TimerInstrumenterTest.srcDir = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/instrumented/dummy/";
+        TimerInstrumenterTest.classDir = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/instrumented/dummy/out/production/dummy";
+
+//        this.compile();
+        this.format();
+
+        // Program arguments
+//        String[] args = new String[0];
+
+//        String[] args = new String[1];
+//        args[0] = "-saveres";
+
+        String[] args = new String[2];
+        args[0] = "-delres";
+        args[1] = "-saveres";
+
+        StaticAnalysis analysis = new TaintFlowAnalysis(programName);
+        Map<JavaRegion, Set<Set<String>>> decisionsToOptions = analysis.analyze(args);
+
+        Instrumenter instrumenter = new TimerRegionInstrumenter(TimerInstrumenterTest.classDir, decisionsToOptions.keySet());
+        instrumenter.instrument(args);
+    }
+
+    @Test
     public void colorCounter() throws NoSuchMethodException, IOException, IllegalAccessException, InvocationTargetException {
         String programName = "pngtasticColorCounter";
         TimerInstrumenterTest.srcDir = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/instrumented/pngtastic-counter/src";

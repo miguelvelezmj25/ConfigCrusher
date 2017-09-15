206b652abd26e45d518e5811057a612747dc33fa
Miguel Velez <miguelvelez@mijecu25.com>
2017-09-15 11:53:31 -0400

    Instrumented main class of color counter

--- src/main/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/bytecode/TraceClassInspector.java
+++ src/main/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/bytecode/TraceClassInspector.java
@@ -2,6 +2,7 @@ package edu.cmu.cs.mvelezce.tool.instrumentation.java.bytecode;
 
 import jdk.internal.org.objectweb.asm.ClassReader;
 import jdk.internal.org.objectweb.asm.Opcodes;
+import jdk.internal.org.objectweb.asm.tree.ClassNode;
 import jdk.internal.org.objectweb.asm.util.TraceClassVisitor;
 
 import java.io.IOException;
@@ -22,6 +23,10 @@ public class TraceClassInspector {
         TraceClassVisitor traceClassVisitor = new TraceClassVisitor(printWriter);
         MethodTracer methodTracer = new MethodTracer(Opcodes.ASM5, traceClassVisitor);
         ClassReader classReader = new ClassReader(this.className);
+        ClassNode classNode = new ClassNode();
+        classReader.accept(classNode, 0);
+
+
         classReader.accept(methodTracer, 0);
 
         return methodTracer;

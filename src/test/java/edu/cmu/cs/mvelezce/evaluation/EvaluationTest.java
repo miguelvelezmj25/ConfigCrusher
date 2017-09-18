e23e01b956f8a7e7c5224cfd46710691a228f435
Miguel Velez <miguelvelez@mijecu25.com>
2017-09-18 15:23:58 -0400

    Tested running example

--- src/test/java/edu/cmu/cs/mvelezce/evaluation/EvaluationTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/evaluation/EvaluationTest.java
@@ -25,7 +25,7 @@ import static org.junit.Assert.*;
 
 public class EvaluationTest {
     @Test
-    public void compareApproaches1() throws Exception {
+    public void compareRunningExample1() throws Exception {
         String programName = "running-example";
 
         Evaluation eval = new Evaluation(programName);

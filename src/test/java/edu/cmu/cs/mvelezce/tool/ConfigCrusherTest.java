c7f1e31d334d31d61bec0857ca7b0b93b841deda
Miguel Velez <miguelvelez@mijecu25.com>
2017-09-19 08:13:21 -0400

    Tested color counter

---
+++ src/test/java/edu/cmu/cs/mvelezce/tool/ConfigCrusherTest.java
@@ -0,0 +1,50 @@
+package edu.cmu.cs.mvelezce.tool;
+
+import edu.cmu.cs.mvelezce.tool.performancemodel.PerformanceModel;
+import org.junit.Test;
+
+public class ConfigCrusherTest {
+
+    @Test
+    public void runningExample() throws Exception {
+        String programName = "running-example";
+        String srcDir = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/instrumented/running-example";
+        String classDir = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/instrumented/running-example/target/classes";
+        String entry = "edu.cmu.cs.mvelezce.Example";
+
+        String[] args = new String[0];
+
+        args = new String[3];
+        args[0] = "-delres";
+        args[1] = "-saveres";
+        args[2] = "-i1";
+
+        ConfigCrusher approach = new ConfigCrusher(programName, srcDir, classDir, entry);
+        approach.format();
+        PerformanceModel performanceModel = approach.run(args);
+
+        performanceModel.toString();
+    }
+
+    @Test
+    public void colorCounter() throws Exception {
+        String programName = "pngtasticColorCounter";
+        String srcDir = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/instrumented/pngtastic-counter";
+        String classDir = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/instrumented/pngtastic-counter/out/production/pngtastic-counter";
+        String entry = "com.googlecode.pngtastic.Run";
+
+        String[] args = new String[0];
+
+        args = new String[3];
+        args[0] = "-delres";
+        args[1] = "-saveres";
+        args[2] = "-i1";
+
+        ConfigCrusher approach = new ConfigCrusher(programName, srcDir, classDir, entry);
+        approach.format();
+        PerformanceModel performanceModel = approach.run(args);
+
+        performanceModel.toString();
+    }
+
+}
---
+++ src/test/java/edu/cmu/cs/mvelezce/tool/performancemodel/ConfigCrusherPerformanceModelBuilderTest.java
@@ -0,0 +1,55 @@
+package edu.cmu.cs.mvelezce.tool.performancemodel;
+
+import edu.cmu.cs.mvelezce.tool.analysis.region.JavaRegion;
+import edu.cmu.cs.mvelezce.tool.analysis.region.Region;
+import edu.cmu.cs.mvelezce.tool.analysis.taint.Analysis;
+import edu.cmu.cs.mvelezce.tool.analysis.taint.java.DefaultStaticAnalysis;
+import edu.cmu.cs.mvelezce.tool.execute.java.DefaultExecutor;
+import edu.cmu.cs.mvelezce.tool.execute.java.Executor;
+import edu.cmu.cs.mvelezce.tool.instrumentation.java.BaseRegionInstrumenter;
+import edu.cmu.cs.mvelezce.tool.instrumentation.java.TimerRegionInstrumenter;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.Set;
+
+public class ConfigCrusherPerformanceModelBuilderTest {
+
+    @Test
+    public void runningExample() throws Exception {
+        String programName = "running-example";
+
+        // Program arguments
+        String[] args = new String[0];
+
+        BaseRegionInstrumenter instrumenter = new TimerRegionInstrumenter(programName);
+        instrumenter.instrument(args);
+        Map<JavaRegion, Set<Set<String>>> javaRegionsToOptionSet = instrumenter.getRegionsToOptionSet();
+
+        Analysis analysis = new DefaultStaticAnalysis();
+        Map<Region, Set<Set<String>>> regionsToOptionSet = analysis.transform(javaRegionsToOptionSet);
+
+        Executor executor = new DefaultExecutor(programName);
+        Set<PerformanceEntry2> measuredPerformance = executor.execute(args);
+
+        args = new String[2];
+        args[0] = "-delres";
+        args[1] = "-saveres";
+
+        PerformanceModelBuilder builder = new DefaultPerformanceModelBuilder(programName, measuredPerformance,
+                regionsToOptionSet);
+        builder.createModel(args);
+    }
+
+    @Test
+    public void runningExample1() throws Exception {
+        String programName = "running-example";
+
+        // Program arguments
+        String[] args = new String[0];
+
+        PerformanceModelBuilder builder = new DefaultPerformanceModelBuilder(programName);
+        builder.createModel(args);
+    }
+
+}

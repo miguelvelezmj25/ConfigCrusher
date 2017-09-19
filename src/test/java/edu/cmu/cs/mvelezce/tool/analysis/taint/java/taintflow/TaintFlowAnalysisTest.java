9aa041dbb150e1189dc86a37d153bddcc0625632
Miguel Velez <miguelvelez@mijecu25.com>
2017-09-19 08:21:58 -0400

    Ran static analysis on optimizer

--- src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/taintflow/TaintFlowAnalysisTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/taintflow/TaintFlowAnalysisTest.java
@@ -41,4 +41,17 @@ public class TaintFlowAnalysisTest {
         StaticAnalysis analysis = new TaintFlowAnalysis(systemName);
         analysis.analyze(args);
     }
+
+    @Test
+    public void optimizer() throws Exception {
+        String systemName = "pngtasticOptimizer";
+
+        // Program arguments
+        String[] args = new String[2];
+        args[0] = "-delres";
+        args[1] = "-saveres";
+
+        StaticAnalysis analysis = new TaintFlowAnalysis(systemName);
+        analysis.analyze(args);
+    }
 }

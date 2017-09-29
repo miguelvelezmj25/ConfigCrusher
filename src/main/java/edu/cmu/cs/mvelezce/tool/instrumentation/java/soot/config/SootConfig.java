7b168a67797bfc8909e202876ca4d6e0fd6c7ddd
Miguel Velez <miguelvelez@mijecu25.com>
2017-09-29 14:07:18 -0400

    Started to prune regions interprocedually

---
+++ src/main/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/soot/config/SootConfig.java
@@ -0,0 +1,16 @@
+package edu.cmu.cs.mvelezce.tool.instrumentation.java.soot.config;
+
+import soot.options.Options;
+
+public class SootConfig {
+
+    public void setSootOptions(Options options) {
+        options.set_output_format(Options.output_format_none);
+        Options.v().setPhaseOption("jb", "use-original-names:true");
+        Options.v().set_keep_line_number(true);
+        Options.v().set_keep_offset(true);
+        Options.v().set_coffi(true);
+        Options.v().set_ignore_classpath_errors(true);
+    }
+
+}

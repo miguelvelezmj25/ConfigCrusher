2d97b572e4443bb5e284adaea08f6de96872c615
Miguel Velez <miguelvelez@mijecu25.com>
2017-10-17 14:29:22 -0400

    Added different types of region managers

--- src/main/java/edu/cmu/cs/mvelezce/tool/analysis/region/Regions.java
+++ src/main/java/edu/cmu/cs/mvelezce/tool/analysis/region/Regions.java
@@ -11,50 +11,33 @@ public class Regions {
 
     public static final String PROGRAM_REGION_ID = "program";
 
-    public static int startCount = 0;
-    public static int endCount = 0;
-
     private static Stack<String> executingRegions = new Stack<>();
     private static Stack<Long> executingRegionsStart = new Stack<>();
     private static Stack<Long> innerRegionsExecutionTime = new Stack<>();
     private static Map<String, Long> regionsToProcessedPerformance = new HashMap<>();
 
-//    private static Map<String, Long> regionsToCount = new HashMap<>();
-
-    public static void main(String[] args) {
-        System.out.println("Started");
-        long start = System.nanoTime();
-
-        for(int i = 0; i < 96313444; i++) {
-            Regions.enter("dsf");
-            Regions.exit("dsf");
-        }
-
-        long end = System.nanoTime();
-        long time = end - start;
-
-        System.out.println("start count " + Regions.startCount);
-        System.out.println("end count " + Regions.endCount);
-        System.out.println(time / 1000000000.0);
-    }
+//    public static void main(String[] args) {
+//        System.out.println("Started");
+//        long start = System.nanoTime();
+//
+//        for(int i = 0; i < 96313444; i++) {
+//            Regions.enter("dsf");
+//            Regions.exit("dsf");
+//        }
+//
+//        long end = System.nanoTime();
+//        long time = end - start;
+//
+//        System.out.println("start count " + Regions.startCount);
+//        System.out.println("end count " + Regions.endCount);
+//        System.out.println(time / 1000000000.0);
+//    }
 
     public static void enter(String id) {
         long start = System.nanoTime();
         Regions.executingRegions.push(id);
         Regions.executingRegionsStart.push(start);
         Regions.innerRegionsExecutionTime.push(0L);
-
-        Regions.startCount++;
-
-
-//        Long count = Regions.regionsToCount.get(id);
-//
-//        if(count == null) {
-//            count = 0L;
-//        }
-//
-//        Regions.regionsToCount.put(id, count + 1L);
-
     }
 
     public static void exit(String id) {
@@ -73,12 +56,6 @@ public class Regions {
 
         Regions.regionsToProcessedPerformance.put(region, regionExecutionTime);
 
-        Regions.endCount++;
-
-//        for(Long l : Regions.innerRegionsExecutionTime) {
-//            l = l + currentRegionExecutionTime;
-//        }
-
         if(executingRegions.isEmpty()) {
             return;
         }
@@ -94,10 +71,6 @@ public class Regions {
             innerRegionsExecutionTime.push(added.pop());
         }
     }
-
-//    public static Map<String, Long> getRegionsToProcessedPerformance() {
-//        return regionsToCount;
-//    }
 
     public static Map<String, Long> getRegionsToProcessedPerformance() {
         return regionsToProcessedPerformance;

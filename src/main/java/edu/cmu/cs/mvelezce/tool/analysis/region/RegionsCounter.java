bae07eea24b1584c50ab2a518143791190c30d52
Miguel Velez <miguelvelez@mijecu25.com>
2017-10-17 14:29:22 -0400

    Added different types of region managers

---
+++ src/main/java/edu/cmu/cs/mvelezce/tool/analysis/region/RegionsCounter.java
@@ -0,0 +1,34 @@
+package edu.cmu.cs.mvelezce.tool.analysis.region;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegionsCounter {
+
+    public static int startCount = 0;
+    public static int endCount = 0;
+
+    private static Map<String, Long> regionsToCount = new HashMap<>();
+
+    public static void enter(String id) {
+        RegionsCounter.startCount++;
+        Long count = RegionsCounter.regionsToCount.get(id);
+
+        if(count == null) {
+            count = 0L;
+        }
+
+        RegionsCounter.regionsToCount.put(id, count + 1L);
+    }
+
+    public static void exit(String id) {
+        RegionsCounter.endCount++;
+
+        Long count = RegionsCounter.regionsToCount.get(id);
+        RegionsCounter.regionsToCount.put(id, count + 1L);
+    }
+
+    public static Map<String, Long> getRegionsToCount() {
+        return RegionsCounter.regionsToCount;
+    }
+}

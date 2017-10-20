dac3a5760af63e0603848b1ef0db347d2573740a
Miguel Velez <miguelvelez@mijecu25.com>
2017-10-20 13:01:31 -0400

    Ran prevayler

---
+++ src/main/java/edu/cmu/cs/mvelezce/tool/execute/java/adapter/prevayler/PrevaylerAdapter.java
@@ -0,0 +1,38 @@
+package edu.cmu.cs.mvelezce.tool.execute.java.adapter.prevayler;
+
+import edu.cmu.cs.mvelezce.tool.execute.java.adapter.BaseAdapter;
+import edu.cmu.cs.mvelezce.tool.execute.java.adapter.prevayler.PrevaylerMain;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class PrevaylerAdapter extends BaseAdapter {
+
+    public PrevaylerAdapter() {
+        this(null, null, null);
+    }
+
+    public PrevaylerAdapter(String programName, String entryPoint, String dir) {
+        super(programName, entryPoint, dir, PrevaylerAdapter.getPrevaylerOptions());
+    }
+
+    public static List<String> getPrevaylerOptions() {
+        String[] options = {"TRANSIENTMODE", "CLOCK", "DEEPCOPY", "DISKSYNC", "FILESIZETHRESHOLD", "FILEAGETHRESHOLD",
+                "MONITOR", "JOURNALSERIALIZER", "SNAPSHOTSERIALIZER"};
+
+        return Arrays.asList(options);
+    }
+
+    @Override
+    public void execute(Set<String> configuration, int iteration) throws IOException, InterruptedException {
+        String[] args = this.configurationAsMainArguments(configuration);
+        String[] newArgs = new String[args.length + 1];
+
+        newArgs[0] = iteration + "";
+        System.arraycopy(args, 0, newArgs, 1, args.length);
+
+        this.execute(PrevaylerMain.PREVAYLER_MAIN, newArgs);
+    }
+}

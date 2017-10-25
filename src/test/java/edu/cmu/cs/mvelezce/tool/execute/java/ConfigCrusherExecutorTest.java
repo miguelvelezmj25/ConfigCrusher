99b5b7dd026dfa45500701afdec2a8ce5a07a97e
Miguel Velez <miguelvelez@mijecu25.com>
2017-10-25 14:35:53 -0400

    Fixed infinite loop while processing a graph since the there might be a recursive method

--- src/test/java/edu/cmu/cs/mvelezce/tool/execute/java/ConfigCrusherExecutorTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/execute/java/ConfigCrusherExecutorTest.java
@@ -186,14 +186,14 @@ public class ConfigCrusherExecutorTest {
         Compression compression = new SimpleCompression(programName);
         Set<Set<String>> configurations = compression.compressConfigurations(args);
 
-//        configurations.clear();
-//        Set<String> n = new HashSet<>();
+        configurations.clear();
+        Set<String> n = new HashSet<>();
 ////        n.add("REMOVEGAMMA");
 ////        n.add("COMPRESSIONLEVEL");
 ////        n.add("COMPRESSOR");
 ////        n.add("ITERATIONS");
 ////        n.add("LOGLEVEL");
-//        configurations.add(n);
+        configurations.add(n);
 
         args = new String[3];
         args[0] = "-delres";

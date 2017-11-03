e39f3b9abb025dc814135f448cf38a9033d57779
Miguel Velez <miguelvelez@mijecu25.com>
2017-11-03 10:08:16 -0400

    Tested removing regions and instrumenting in methods

--- src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/taintflow/TaintFlowAnalysisTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/taintflow/TaintFlowAnalysisTest.java
@@ -60,6 +60,19 @@ public class TaintFlowAnalysisTest {
     }
 
     @Test
+    public void regions() throws Exception {
+        String systemName = "regions";
+
+        // Program arguments
+        String[] args = new String[2];
+        args[0] = "-delres";
+        args[1] = "-saveres";
+
+        StaticAnalysis analysis = new TaintFlowAnalysis(systemName);
+        analysis.analyze(args);
+    }
+
+    @Test
     public void regions0() throws Exception {
         String systemName = "regions0";
 

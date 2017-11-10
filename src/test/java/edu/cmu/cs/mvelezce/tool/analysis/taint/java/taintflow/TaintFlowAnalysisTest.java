61ecea890256d5a44652967533d8b42311f438cd
Miguel Velez <miguelvelez@mijecu25.com>
2017-11-10 10:05:03 -0500

    Filled regions down the graph

--- src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/taintflow/TaintFlowAnalysisTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/taintflow/TaintFlowAnalysisTest.java
@@ -307,6 +307,19 @@ public class TaintFlowAnalysisTest {
     }
 
     @Test
+    public void regions20() throws Exception {
+        String systemName = "regions20";
+
+        // Program arguments
+        String[] args = new String[2];
+        args[0] = "-delres";
+        args[1] = "-saveres";
+
+        StaticAnalysis analysis = new TaintFlowAnalysis(systemName);
+        analysis.analyze(args);
+    }
+
+    @Test
     public void prevayler() throws Exception {
         String systemName = "prevayler";
 

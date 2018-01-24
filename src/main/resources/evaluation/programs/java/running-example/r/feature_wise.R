5631748575f9b98180f593bcd28f565d701cae63
Miguel Velez <miguelvelez@mijecu25.com>
2018-01-24 13:44:07 -0500

    Ran bf running example

--- src/main/resources/evaluation/programs/java/running-example/r/feature_wise.R
+++ src/main/resources/evaluation/programs/java/running-example/r/feature_wise.R
@@ -1,3 +1,3 @@
 feature_wise <- read.csv("src/main/resources/evaluation/programs/java/running-example/r/feature_wise.csv")
-model <- lm(time~A+B+C, data = feature_wise)
+model <- lm(time~A+B+C+D, data = feature_wise)
 coef(model)

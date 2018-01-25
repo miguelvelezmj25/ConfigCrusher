5d0dba4d1624057f57f5e4a1f45816e3ec4328ee
Miguel Velez <miguelvelez@mijecu25.com>
2018-01-25 08:21:18 -0500

    Added find adapter

--- src/test/java/edu/cmu/cs/mvelezce/evaluation/approaches/bruteforce/execute/BruteForceExecutorTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/evaluation/approaches/bruteforce/execute/BruteForceExecutorTest.java
@@ -3,11 +3,15 @@ package edu.cmu.cs.mvelezce.evaluation.approaches.bruteforce.execute;
 import edu.cmu.cs.mvelezce.tool.compression.Compression;
 import edu.cmu.cs.mvelezce.tool.compression.simple.SimpleCompression;
 import edu.cmu.cs.mvelezce.tool.execute.java.Executor;
+import edu.cmu.cs.mvelezce.tool.execute.java.adapter.colorCounter.ColorCounterAdapter;
+import edu.cmu.cs.mvelezce.tool.execute.java.adapter.find.FindAdapter;
+import edu.cmu.cs.mvelezce.tool.execute.java.adapter.runningexample.RunningExampleAdapter;
 import edu.cmu.cs.mvelezce.tool.performance.entry.PerformanceEntryStatistic;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class BruteForceExecutorTest {
@@ -106,15 +110,11 @@ public class BruteForceExecutorTest {
         String classDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/pngtastic-counter/out/production/pngtastic-counter";
         String entryPoint = "counter.com.googlecode.pngtastic.Run";
 
-        // Program arguments
-        String[] args = new String[0];
-
-        Compression compression = new SimpleCompression(programName);
-        Set<Set<String>> configurations = compression.compressConfigurations(args);
-        configurations = BruteForceExecutor.getBruteForceConfigurations(configurations);
+        Set<String> options = new HashSet<>(ColorCounterAdapter.getColorCounterOptions());
+        Set<Set<String>> configurations = BruteForceExecutor.getBruteForceConfigurationsFromOptions(options);
         System.out.println("Configurations to sample: " + configurations.size());
 
-        args = new String[3];
+        String[] args = new String[3];
         args[0] = "-delres";
         args[1] = "-saveres";
         args[2] = "-i5";
@@ -129,15 +129,11 @@ public class BruteForceExecutorTest {
         String classDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/pngtastic-optimizer/out/production/pngtastic-optimizer";
         String entryPoint = "optimizer.com.googlecode.pngtastic.Run";
 
-        // Program arguments
-        String[] args = new String[0];
-
-        Compression compression = new SimpleCompression(programName);
-        Set<Set<String>> configurations = compression.compressConfigurations(args);
-        configurations = BruteForceExecutor.getBruteForceConfigurations(configurations);
+        Set<String> options = new HashSet<>(RunningExampleAdapter.getRunningExampleOptions());
+        Set<Set<String>> configurations = BruteForceExecutor.getBruteForceConfigurationsFromOptions(options);
         System.out.println("Configurations to sample: " + configurations.size());
 
-        args = new String[3];
+        String[] args = new String[3];
         args[0] = "-delres";
         args[1] = "-saveres";
         args[2] = "-i5";
@@ -152,15 +148,11 @@ public class BruteForceExecutorTest {
         String classDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/prevayler/target/classes";
         String entryPoint = "org.prevayler.demos.demo1.PrimeNumbers";
 
-        // Program arguments
-        String[] args = new String[0];
-
-        Compression compression = new SimpleCompression(programName);
-        Set<Set<String>> configurations = compression.compressConfigurations(args);
-        configurations = BruteForceExecutor.getBruteForceConfigurations(configurations);
+        Set<String> options = new HashSet<>(RunningExampleAdapter.getRunningExampleOptions());
+        Set<Set<String>> configurations = BruteForceExecutor.getBruteForceConfigurationsFromOptions(options);
         System.out.println("Configurations to sample: " + configurations.size());
 
-        args = new String[3];
+        String[] args = new String[3];
         args[0] = "-delres";
         args[1] = "-saveres";
         args[2] = "-i5";
@@ -175,15 +167,11 @@ public class BruteForceExecutorTest {
         String entryPoint = "kanzi.Run";
         String classDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/kanzi/target/classes";
 
-        // Program arguments
-        String[] args = new String[0];
-
-        Compression compression = new SimpleCompression(programName);
-        Set<Set<String>> configurations = compression.compressConfigurations(args);
-        configurations = BruteForceExecutor.getBruteForceConfigurations(configurations);
+        Set<String> options = new HashSet<>(RunningExampleAdapter.getRunningExampleOptions());
+        Set<Set<String>> configurations = BruteForceExecutor.getBruteForceConfigurationsFromOptions(options);
         System.out.println("Configurations to sample: " + configurations.size());
 
-        args = new String[3];
+        String[] args = new String[3];
         args[0] = "-delres";
         args[1] = "-saveres";
         args[2] = "-i3";
@@ -247,15 +235,31 @@ public class BruteForceExecutorTest {
 
         String entryPoint = "org.unix4j.grep.Main";
 
-        // Program arguments
-        String[] args = new String[0];
-
-        Compression compression = new SimpleCompression(programName);
-        Set<Set<String>> configurations = compression.compressConfigurations(args);
-        configurations = BruteForceExecutor.getBruteForceConfigurations(configurations);
+        Set<String> options = new HashSet<>(RunningExampleAdapter.getRunningExampleOptions());
+        Set<Set<String>> configurations = BruteForceExecutor.getBruteForceConfigurationsFromOptions(options);
         System.out.println("Configurations to sample: " + configurations.size());
 
-        args = new String[3];
+        String[] args = new String[3];
+        args[0] = "-delres";
+        args[1] = "-saveres";
+        args[2] = "-i5";
+
+        Executor executor = new BruteForceExecutor(programName, entryPoint, classDirectory, configurations);
+        Set<PerformanceEntryStatistic> measuredPerformance = executor.execute(args);
+    }
+
+    @Test
+    public void find() throws IOException, InterruptedException {
+        String programName = "find";
+        String classDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/original/find/target/classes";
+
+        String entryPoint = "org.unix4j.find.Main";
+
+        Set<String> options = new HashSet<>(FindAdapter.getFindOptions());
+        Set<Set<String>> configurations = BruteForceExecutor.getBruteForceConfigurationsFromOptions(options);
+        System.out.println("Configurations to sample: " + configurations.size());
+
+        String[] args = new String[3];
         args[0] = "-delres";
         args[1] = "-saveres";
         args[2] = "-i5";

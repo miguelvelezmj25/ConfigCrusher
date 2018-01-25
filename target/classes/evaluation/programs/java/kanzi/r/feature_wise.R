5bb093a832459844e707a6960f3ac759b2e1e3bf
Miguel Velez <miguelvelez@mijecu25.com>
2018-01-25 16:44:39 -0500

    Removed target folder

--- target/classes/evaluation/programs/java/kanzi/r/feature_wise.R
+++
@@ -1,3 +0,0 @@
-feature_wise <- read.csv("src/main/resources/evaluation/programs/java/kanzi/r/feature_wise.csv")
-model <- lm(time~CHECKSUM+FORCE+BLOCKSIZE+TRANSFORM+LEVEL+VERBOSE+ENTROPY, data = feature_wise)
-coef(model)

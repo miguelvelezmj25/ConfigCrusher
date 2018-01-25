e8eadcc1a59a64246f01041bf8eaafdf413993cd
Miguel Velez <miguelvelez@mijecu25.com>
2018-01-25 16:44:39 -0500

    Removed target folder

--- target/classes/evaluation/programs/java/grep/r/feature_wise.R
+++
@@ -1,3 +0,0 @@
-feature_wise <- read.csv("src/main/resources/evaluation/programs/java/grep/r/feature_wise.csv")
-model <- lm(time~FIXEDSTRINGS+INVERTMATCH+WHOLELINE+COUNT+LINENUMBER+MATCHINGFILES+IGNORECASE, data = feature_wise)
-coef(model)

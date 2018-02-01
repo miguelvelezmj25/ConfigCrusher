c87b8be65ec58402f5bcbdff0d1e0c64f5630a1d
Miguel Velez <miguelvelez@mijecu25.com>
2018-02-01 10:53:31 -0500

    Used matlab to predict model

--- src/main/java/edu/cmu/cs/mvelezce/evaluation/analysis/runningexample/analysis.m
+++ src/main/java/edu/cmu/cs/mvelezce/evaluation/analysis/runningexample/analysis.m
@@ -17,6 +17,10 @@ fileID = fopen('../../../../../../../../resources/evaluation/programs/java/runni
 fprintf(fileID, '%10.2f\n', coefs)
 fclose(fileID);
 
+formula = model.Formula
+fileID = fopen('../../../../../../../../resources/evaluation/programs/java/running-example/data/feature_wise/formula.txt', 'w');
+fprintf(fileID, '%s\n', formula)
+fclose(fileID);
 
 % pair_wise
 
@@ -35,4 +39,9 @@ fclose(fileID);
 coefs = model.Coefficients.Estimate;
 fileID = fopen('../../../../../../../../resources/evaluation/programs/java/running-example/data/pair_wise/coefs.txt', 'w');
 fprintf(fileID, '%10.2f\n', coefs)
+fclose(fileID);
+
+formula = model.Formula
+fileID = fopen('../../../../../../../../resources/evaluation/programs/java/running-example/data/pair_wise/formula.txt', 'w');
+fprintf(fileID, '%s\n', formula)
 fclose(fileID);
--- src/main/java/edu/cmu/cs/mvelezce/evaluation/approaches/pairwise/Pairwise.java
+++ src/main/java/edu/cmu/cs/mvelezce/evaluation/approaches/pairwise/Pairwise.java
@@ -6,7 +6,9 @@ import edu.cmu.cs.mvelezce.tool.performance.entry.PerformanceEntryStatistic;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.math3.util.Combinations;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.*;
 
@@ -126,6 +128,39 @@ public class Pairwise extends Approach {
 //        return outputDir;
 //    }
 
+    @Override
+    public Map<Set<String>, Double> getLearnedModel(List<String> options) throws IOException {
+        Map<Set<String>, Double> learnedModel = new HashMap<>();
+
+        String dataDir = Evaluation.DIRECTORY + "/" + this.getProgramName() + Approach.DATA_DIR + "/"
+                + Evaluation.PAIR_WISE;
+
+        File termsFile = new File(dataDir + "/" + Approach.TERMS_FILE);
+        List<String> rawTerms = this.parseFile(termsFile);
+
+        File coefsFile = new File(dataDir + "/" + Approach.COEFS_FILE);
+        List<String> coefs = this.parseFile(coefsFile);
+
+        if(rawTerms.size() != coefs.size()) {
+            throw new RuntimeException("The terms and coefs files are not the same length");
+        }
+
+        File formulaFile = new File(dataDir + "/" + Approach.FORMULA_FILE);
+        String formula = this.parseFile(formulaFile).get(0);
+        List<String> termsNotInFormula = this.removeTermsNoInFormula(rawTerms, formula);
+
+        List<Set<String>> terms = this.parseTerms(termsNotInFormula, options);
+
+        for(int i = 0; i < terms.size(); i++) {
+            Set<String> term = terms.get(i);
+            String rawTerm = termsNotInFormula.get(i);
+            int index = rawTerms.indexOf(rawTerm);
+            String coef = coefs.get(index);
+            learnedModel.put(term, Double.valueOf(coef));
+        }
+
+        return learnedModel;
+    }
 
     @Override
     public void generateCSVData(Set<PerformanceEntryStatistic> performanceEntries) throws IOException {
@@ -152,8 +187,7 @@ public class Pairwise extends Approach {
             for(String option : options) {
                 if(configuration.contains(option)) {
                     result.append("1");
-                }
-                else {
+                } else {
                     result.append("0");
                 }
 

b0d0ba6059835b378af09c249445a17500ca6ce2
Miguel Velez <miguelvelez@mijecu25.com>
2018-02-01 08:00:54 -0500

    Ran configcrusher on kanzi

--- src/test/java/edu/cmu/cs/mvelezce/tool/compression/simple/SimpleCompressionTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/compression/simple/SimpleCompressionTest.java
@@ -728,11 +728,11 @@ public class SimpleCompressionTest {
         String programName = "kanzi";
 
         // Program arguments
-//        String[] args = new String[0];
-
-        String[] args = new String[2];
-        args[0] = "-delres";
-        args[1] = "-saveres";
+        String[] args = new String[0];
+//
+//        String[] args = new String[2];
+//        args[0] = "-delres";
+//        args[1] = "-saveres";
 
         StaticAnalysis taintflowAnalysis = new TaintFlowAnalysis(programName);
         Map<JavaRegion, Set<Set<String>>> decisionsToOptionsSet = taintflowAnalysis.analyze(args);

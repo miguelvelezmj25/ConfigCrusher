50b6785d9a980caa0e096c94764c119913de4759
Miguel Velez <miguelvelez@mijecu25.com>
2018-02-02 16:37:18 -0500

    Ran cc in counter

--- src/test/java/edu/cmu/cs/mvelezce/tool/execute/java/ConfigCrusherExecutorTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/execute/java/ConfigCrusherExecutorTest.java
@@ -133,6 +133,32 @@ public class ConfigCrusherExecutorTest {
     }
 
     @Test
+    public void colorCounter2() throws Exception {
+        String programName = "pngtasticColorCounter";
+        String classDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/instrumented/pngtastic-counter/out/production/pngtastic-counter";
+        String entryPoint = "counter.com.googlecode.pngtastic.Run";
+
+        // Program arguments
+        String[] args = new String[0];
+
+        Compression compression = new SimpleCompression(programName);
+        Set<Set<String>> configurations = compression.compressConfigurations(args);
+
+        configurations.clear();
+        Set<String> conf = new HashSet<>();
+        configurations.add(conf);
+
+        args = new String[3];
+        args[0] = "-delres";
+        args[1] = "-saveres";
+        args[2] = "-i1";
+
+        Executor executor = new ConfigCrusherExecutor(programName, entryPoint, classDirectory, configurations);
+        Set<PerformanceEntryStatistic> measuredPerformance = executor.execute(args);
+        measuredPerformance.size();
+    }
+
+    @Test
     public void optimizer() throws Exception {
         String programName = "pngtasticOptimizer";
         String classDirectory = "/Users/mvelezce/Documents/Programming/Java/Projects/performance-mapper-evaluation/instrumented/pngtastic-optimizer/out/production/pngtastic-optimizer";

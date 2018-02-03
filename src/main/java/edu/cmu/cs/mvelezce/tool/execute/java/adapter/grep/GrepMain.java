b8ed2ae8d2526ec03f38a814b9f5350763a97788
Miguel Velez <miguelvelez@mijecu25.com>
2018-02-03 16:29:58 -0500

    Ran cc grep

--- src/main/java/edu/cmu/cs/mvelezce/tool/execute/java/adapter/grep/GrepMain.java
+++ src/main/java/edu/cmu/cs/mvelezce/tool/execute/java/adapter/grep/GrepMain.java
@@ -1,11 +1,14 @@
 package edu.cmu.cs.mvelezce.tool.execute.java.adapter.grep;
 
+import edu.cmu.cs.mvelezce.tool.analysis.region.JavaRegion;
 import edu.cmu.cs.mvelezce.tool.analysis.region.Region;
 import edu.cmu.cs.mvelezce.tool.analysis.region.Regions;
 import edu.cmu.cs.mvelezce.tool.execute.java.ConfigCrusherExecutor;
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.Adapter;
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.BaseMain;
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.Main;
+import edu.cmu.cs.mvelezce.tool.instrumentation.java.BaseRegionInstrumenter;
+import edu.cmu.cs.mvelezce.tool.instrumentation.java.ConfigCrusherTimerRegionInstrumenter;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -43,6 +46,16 @@ public class GrepMain extends BaseMain {
 
     @Override
     public void execute(String mainClass, String[] args) throws Exception {
+        BaseRegionInstrumenter instrumenter = new ConfigCrusherTimerRegionInstrumenter("grep");
+        instrumenter.instrument(args);
+        Set<JavaRegion> regions = instrumenter.getRegionsToOptionSet().keySet();
+
+        for(JavaRegion region : regions) {
+            Regions.regionsToOverhead.put(region.getRegionID(),0L);
+        }
+
+        Regions.regionsToOverhead.put(Regions.PROGRAM_REGION_ID,0L);
+
         if(mainClass.contains("Main")) {
             Region program = new Region(Regions.PROGRAM_REGION_ID);
             Regions.enter(program.getRegionID());

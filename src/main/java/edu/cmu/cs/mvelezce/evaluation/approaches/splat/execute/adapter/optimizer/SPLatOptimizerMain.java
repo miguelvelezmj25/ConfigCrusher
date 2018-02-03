2243938ca186e37d33f07e2a25816f7458cccc20
Miguel Velez <miguelvelez@mijecu25.com>
2018-02-03 14:55:57 -0500

    Ran optimizer with splat

--- src/main/java/edu/cmu/cs/mvelezce/evaluation/approaches/splat/execute/adapter/optimizer/SPLatOptimizerMain.java
+++ src/main/java/edu/cmu/cs/mvelezce/evaluation/approaches/splat/execute/adapter/optimizer/SPLatOptimizerMain.java
@@ -1,13 +1,11 @@
 package edu.cmu.cs.mvelezce.evaluation.approaches.splat.execute.adapter.optimizer;
 
-import edu.cmu.cs.mvelezce.tool.execute.java.adapter.optimizer.OptimizerAdapter;
-import optimizer.com.googlecode.pngtastic.Run;
 import edu.cmu.cs.mvelezce.evaluation.approaches.splat.execute.adapter.SPLatMain;
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.Adapter;
+import edu.cmu.cs.mvelezce.tool.execute.java.adapter.optimizer.OptimizerAdapter;
+import optimizer.com.googlecode.pngtastic.Run;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
 public class SPLatOptimizerMain extends SPLatMain {
 
@@ -21,6 +19,8 @@ public class SPLatOptimizerMain extends SPLatMain {
             throw new RuntimeException("Could not find the main class " + this.getProgramName());
         }
 
+        List<String> options = OptimizerAdapter.getOptimizerOptions();
+        Map<Set<String>, Set<Set<String>>> configsToCovered = new HashMap<>();
         Set<Set<String>> splatConfigurations = new HashSet<>();
         Adapter adapter = new OptimizerAdapter();
         Stack<String> stack = new Stack<>();
@@ -29,25 +29,33 @@ public class SPLatOptimizerMain extends SPLatMain {
         splatConfigurations.add(configuration);
         String[] args = adapter.configurationAsMainArguments(configuration);
 
-//        Run.splat(args, stack);
-//
-//        while(!stack.isEmpty()) {
-//            String option = stack.peek();
-//
-//            if(configuration.contains(option)) {
-//                configuration = new HashSet<>(configuration);
-//                configuration.remove(option);
-//                stack.pop();
-//            }
-//            else {
-//                configuration = new HashSet<>(configuration);
-//                configuration.add(option);
-//                splatConfigurations.add(configuration);
-//                args = adapter.configurationAsMainArguments(configuration);
-//                Run.splat(args, stack);
-//            }
-//        }
+        Run.splat(args, stack);
 
+        Set<String> optionsNotInStack = this.getOptionsNotInStack(stack, options);
+        Set<Set<String>> coveredConfigs = this.mapConfigs(configuration, optionsNotInStack);
+        configsToCovered.put(configuration, coveredConfigs);
+
+        while(!stack.isEmpty()) {
+            String option = stack.peek();
+
+            if(configuration.contains(option)) {
+                configuration = new HashSet<>(configuration);
+                configuration.remove(option);
+                stack.pop();
+            } else {
+                configuration = new HashSet<>(configuration);
+                configuration.add(option);
+                splatConfigurations.add(configuration);
+                args = adapter.configurationAsMainArguments(configuration);
+                Run.splat(args, stack);
+
+                optionsNotInStack = this.getOptionsNotInStack(stack, options);
+                coveredConfigs = this.mapConfigs(configuration, optionsNotInStack);
+                configsToCovered.put(configuration, coveredConfigs);
+            }
+        }
+
+        this.setConfigsToCovered(configsToCovered);
         return splatConfigurations;
     }
 }

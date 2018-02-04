1d345cb8dc266c19431752f8d548604c8b3b4907
Miguel Velez <miguelvelez@mijecu25.com>
2018-02-04 00:35:59 -0500

    Ran bf on kanzi

--- src/test/java/edu/cmu/cs/mvelezce/evaluation/approaches/bruteforce/execute/BruteForceExecutorTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/evaluation/approaches/bruteforce/execute/BruteForceExecutorTest.java
@@ -203,7 +203,7 @@ public class BruteForceExecutorTest {
         String[] args = new String[3];
         args[0] = "-delres";
         args[1] = "-saveres";
-        args[2] = "-i3";
+        args[2] = "-i5";
 
         Executor executor = new BruteForceExecutor(programName, entryPoint, classDirectory, configurations);
         Set<PerformanceEntryStatistic> measuredPerformance = executor.execute(args);

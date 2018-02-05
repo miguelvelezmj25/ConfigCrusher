61654a4811a6cef2f4f27ae5b7ac737c6acc6db5
Miguel Velez <miguelvelez@mijecu25.com>
2018-02-05 08:15:30 -0500

    Got feature and pairwise running

--- src/main/java/edu/cmu/cs/mvelezce/evaluation/approaches/splat/execute/adapter/prevalyer/SPLatPrevaylerMain.java
+++ src/main/java/edu/cmu/cs/mvelezce/evaluation/approaches/splat/execute/adapter/prevalyer/SPLatPrevaylerMain.java
@@ -29,41 +29,41 @@ public class SPLatPrevaylerMain extends SPLatMain {
         splatConfigurations.add(configuration);
         String[] args = adapter.configurationAsMainArguments(configuration);
 
-        try {
-            PrimeNumbers.splat(args, stack);
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-
-        Set<String> optionsNotInStack = this.getOptionsNotInStack(stack, options);
-        Set<Set<String>> coveredConfigs = this.mapConfigs(configuration, optionsNotInStack);
-        configsToCovered.put(configuration, coveredConfigs);
-
-        while(!stack.isEmpty()) {
-            String option = stack.peek();
-
-            if(configuration.contains(option)) {
-                configuration = new HashSet<>(configuration);
-                configuration.remove(option);
-                stack.pop();
-            } else {
-                configuration = new HashSet<>(configuration);
-                configuration.add(option);
-                splatConfigurations.add(configuration);
-                args = adapter.configurationAsMainArguments(configuration);
-                try {
-                    PrimeNumbers.splat(args, stack);
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-
-                optionsNotInStack = this.getOptionsNotInStack(stack, options);
-                coveredConfigs = this.mapConfigs(configuration, optionsNotInStack);
-                configsToCovered.put(configuration, coveredConfigs);
-            }
-        }
-
-        this.setConfigsToCovered(configsToCovered);
+//        try {
+//            PrimeNumbers.splat(args, stack);
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        Set<String> optionsNotInStack = this.getOptionsNotInStack(stack, options);
+//        Set<Set<String>> coveredConfigs = this.mapConfigs(configuration, optionsNotInStack);
+//        configsToCovered.put(configuration, coveredConfigs);
+//
+//        while(!stack.isEmpty()) {
+//            String option = stack.peek();
+//
+//            if(configuration.contains(option)) {
+//                configuration = new HashSet<>(configuration);
+//                configuration.remove(option);
+//                stack.pop();
+//            } else {
+//                configuration = new HashSet<>(configuration);
+//                configuration.add(option);
+//                splatConfigurations.add(configuration);
+//                args = adapter.configurationAsMainArguments(configuration);
+//                try {
+//                    PrimeNumbers.splat(args, stack);
+//                } catch(Exception e) {
+//                    e.printStackTrace();
+//                }
+//
+//                optionsNotInStack = this.getOptionsNotInStack(stack, options);
+//                coveredConfigs = this.mapConfigs(configuration, optionsNotInStack);
+//                configsToCovered.put(configuration, coveredConfigs);
+//            }
+//        }
+//
+//        this.setConfigsToCovered(configsToCovered);
 
         return splatConfigurations;
     }

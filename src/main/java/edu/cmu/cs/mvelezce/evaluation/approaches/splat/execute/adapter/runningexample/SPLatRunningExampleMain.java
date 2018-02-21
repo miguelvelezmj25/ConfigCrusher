3ef5720e561fba6006f45c606433a8892390af23
Miguel Velez <miguelvelez@mijecu25.com>
2018-02-21 13:54:20 -0500

    Got data for running example

--- src/main/java/edu/cmu/cs/mvelezce/evaluation/approaches/splat/execute/adapter/runningexample/SPLatRunningExampleMain.java
+++ src/main/java/edu/cmu/cs/mvelezce/evaluation/approaches/splat/execute/adapter/runningexample/SPLatRunningExampleMain.java
@@ -30,32 +30,32 @@ public class SPLatRunningExampleMain extends SPLatMain {
         splatConfigurations.add(configuration);
         String[] args = adapter.configurationAsMainArguments(configuration);
 
-//        Example.splat(args, stack);
-//
-//        Set<String> optionsNotInStack = this.getOptionsNotInStack(stack, options);
-//        Set<Set<String>> coveredConfigs = this.mapConfigs(configuration, optionsNotInStack);
-//        configsToCovered.put(configuration, coveredConfigs);
-//
-//        while(!stack.isEmpty()) {
-//            String option = stack.peek();
-//
-//            if(configuration.contains(option)) {
-//                configuration = new HashSet<>(configuration);
-//                configuration.remove(option);
-//                stack.pop();
-//            }
-//            else {
-//                configuration = new HashSet<>(configuration);
-//                configuration.add(option);
-//                splatConfigurations.add(configuration);
-//                args = adapter.configurationAsMainArguments(configuration);
-//                Example.splat(args, stack);
-//
-//                optionsNotInStack = this.getOptionsNotInStack(stack, options);
-//                coveredConfigs = this.mapConfigs(configuration, optionsNotInStack);
-//                configsToCovered.put(configuration, coveredConfigs);
-//            }
-//        }
+        Example.splat(args, stack);
+
+        Set<String> optionsNotInStack = this.getOptionsNotInStack(stack, options);
+        Set<Set<String>> coveredConfigs = this.mapConfigs(configuration, optionsNotInStack);
+        configsToCovered.put(configuration, coveredConfigs);
+
+        while(!stack.isEmpty()) {
+            String option = stack.peek();
+
+            if(configuration.contains(option)) {
+                configuration = new HashSet<>(configuration);
+                configuration.remove(option);
+                stack.pop();
+            }
+            else {
+                configuration = new HashSet<>(configuration);
+                configuration.add(option);
+                splatConfigurations.add(configuration);
+                args = adapter.configurationAsMainArguments(configuration);
+                Example.splat(args, stack);
+
+                optionsNotInStack = this.getOptionsNotInStack(stack, options);
+                coveredConfigs = this.mapConfigs(configuration, optionsNotInStack);
+                configsToCovered.put(configuration, coveredConfigs);
+            }
+        }
 
         this.setConfigsToCovered(configsToCovered);
 

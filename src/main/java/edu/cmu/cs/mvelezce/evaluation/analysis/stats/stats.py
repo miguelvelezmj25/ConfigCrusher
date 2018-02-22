92a2dbf2d6608f956ebbfdfee122f52b74879528
Miguel Velez <miguelvelez@mijecu25.com>
2018-02-22 13:49:00 -0500

    Check simulation

--- src/main/java/edu/cmu/cs/mvelezce/evaluation/analysis/stats/stats.py
+++ src/main/java/edu/cmu/cs/mvelezce/evaluation/analysis/stats/stats.py
@@ -1,25 +1,31 @@
+import math
 import numpy as np
-import scipy as sp
 import scipy.stats
 
 
 def mean_confidence_interval(data, confidence=0.95):
     a = 1.0 * np.array(data)
-    n = len(a)
-    m, se = np.mean(a), scipy.stats.sem(a)
-    ci = sp.stats.t._ppf((1+confidence)/2., n-1)
-    h = se * ci
-    return m, m-h, m+h
+
+    # for i in a:
+    #     print i
+
+    n, min_max, mean, var, skew, kurt = scipy.stats.describe(a)
+    std = math.sqrt(var)
+
+    ci = scipy.stats.t.interval(confidence, n - 1, loc=mean, scale=std / math.sqrt(n))
+
+    return mean, ci[0], ci[1]
 
 
 def test_fifth_in_ci():
     yes = 0
     no = 0
     mu = 20
-    sigma = 1
+    sigma = 5
+    samples = 10
 
-    for i in range(10000):
-        data = np.random.normal(mu, sigma, 4)
+    for i in range(2000):
+        data = np.random.normal(mu, sigma, samples)
         mean, minci, maxci = mean_confidence_interval(data)
         fifth = np.random.normal(mu, sigma)
 

1bb1595732afd5d7b6d9898ded7a0ecdecca4022
Miguel Velez <miguelvelez@mijecu25.com>
2018-02-23 16:46:30 -0500

    Added family performance model

---
+++ src/main/java/edu/cmu/cs/mvelezce/evaluation/approaches/family/model/FamilyModel.java
@@ -0,0 +1,9 @@
+package edu.cmu.cs.mvelezce.evaluation.approaches.family.model;
+
+import edu.cmu.cs.mvelezce.tool.performance.model.PerformanceModel;
+
+public class FamilyModel extends PerformanceModel {
+
+
+
+}

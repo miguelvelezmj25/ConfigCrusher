2078008453c7df22673624d65fcbb5dac8aac28a
Miguel Velez <miguelvelez@mijecu25.com>
2018-02-23 14:16:36 -0500

    Got splat data for elevator

--- src/main/java/edu/cmu/cs/mvelezce/tool/execute/java/adapter/elevator/ElevatorMain.java
+++ src/main/java/edu/cmu/cs/mvelezce/tool/execute/java/adapter/elevator/ElevatorMain.java
@@ -1,36 +1,84 @@
 package edu.cmu.cs.mvelezce.tool.execute.java.adapter.elevator;
 
+import edu.cmu.cs.mvelezce.Example;
+import edu.cmu.cs.mvelezce.tool.analysis.region.JavaRegion;
 import edu.cmu.cs.mvelezce.tool.analysis.region.Region;
 import edu.cmu.cs.mvelezce.tool.analysis.region.Regions;
 import edu.cmu.cs.mvelezce.tool.execute.java.ConfigCrusherExecutor;
-import edu.cmu.cs.mvelezce.tool.execute.java.Executor;
+import edu.cmu.cs.mvelezce.tool.execute.java.adapter.Adapter;
+import edu.cmu.cs.mvelezce.tool.execute.java.adapter.BaseMain;
+import edu.cmu.cs.mvelezce.tool.execute.java.adapter.Main;
+import edu.cmu.cs.mvelezce.tool.instrumentation.java.BaseRegionInstrumenter;
+import edu.cmu.cs.mvelezce.tool.instrumentation.java.ConfigCrusherTimerRegionInstrumenter;
+import family.PL_Interface_impl;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Set;
 
 /**
  * Created by miguelvelez on 4/30/17.
  */
-public class ElevatorMain {
+public class ElevatorMain extends BaseMain {
 
     public static final String ELEVATOR_MAIN = ElevatorMain.class.getCanonicalName();
 
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public ElevatorMain(String programName, String iteration, String[] args) {
+        super(programName, iteration, args);
+    }
+
+    public static void main(String[] args) throws IOException {
         String programName = args[0];
         String mainClass = args[1];
-        String[] elevatorArgs = Arrays.copyOfRange(args, 2, args.length);
+        String iteration = args[2];
+        String[] sleepArgs = Arrays.copyOfRange(args, 3, args.length);
 
-        if(mainClass.equals("edu.cmu.cs.mvelezce.PL_Interface_impl")) {
-            Region program = new Region(Regions.PROGRAM_REGION_ID);
-            Regions.enter(program.getRegionID());
-//            PL_Interface_impl.main(elevatorArgs);
-            Regions.exit(program.getRegionID());
+        Main main = new ElevatorMain(programName, iteration, sleepArgs);
+        main.execute(mainClass, sleepArgs);
+        main.logExecution();
+    }
+
+    @Override
+    public void logExecution() throws IOException {
+        Adapter adapter = new ElevatorAdapter();
+        Set<String> configuration = adapter.configurationAsSet(this.getArgs());
+
+        ConfigCrusherExecutor executor = new ConfigCrusherExecutor(this.getProgramName());
+        Map<String, Long> results = executor.getResults();
+        executor.writeToFile(this.getIteration(), configuration, results);
+    }
+
+    @Override
+    public void execute(String mainClass, String[] args) {
+        try {
+            BaseRegionInstrumenter instrumenter = new ConfigCrusherTimerRegionInstrumenter("elevator");
+            instrumenter.instrument(args);
+            Set<JavaRegion> regions = instrumenter.getRegionsToOptionSet().keySet();
+
+            for(JavaRegion region : regions) {
+                Regions.regionsToOverhead.put(region.getRegionID(), 0L);
+            }
+            Regions.regionsToOverhead.put(Regions.PROGRAM_REGION_ID, 0L);
+        } catch(InvocationTargetException | NoSuchMethodException | IOException | IllegalAccessException | InterruptedException e) {
+            throw new RuntimeException("Could not add regions to the Regions class");
         }
 
-        Set<String> performanceConfiguration = ElevatorAdapter.adaptConfigurationToPerformanceMeasurement(elevatorArgs);
-        Executor executor = new ConfigCrusherExecutor();
-//        executor.writeToFile(programName, performanceConfiguration, Regions.getRegionsToProcessedPerformance());
-        throw new RuntimeException("Check this main");
+        if(mainClass.contains("PL_Interface_impl")) {
+            Region program = new Region(Regions.PROGRAM_REGION_ID);
+
+            try {
+                Regions.enter(program.getRegionID());
+                PL_Interface_impl.main(args);
+            } catch(Exception e) {
+                e.printStackTrace();
+            } finally {
+                Regions.exit(program.getRegionID());
+            }
+        }
+        else {
+            throw new RuntimeException("Could not find the main class " + mainClass);
+        }
     }
 }

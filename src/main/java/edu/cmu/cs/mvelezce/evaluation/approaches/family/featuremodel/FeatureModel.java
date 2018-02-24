24dadbe5851a4602fbe9cf505aa79152e9c71517
Miguel Velez <miguelvelez@mijecu25.com>
2018-02-24 16:34:36 -0500

    Getting data for elevator

---
+++ src/main/java/edu/cmu/cs/mvelezce/evaluation/approaches/family/featuremodel/FeatureModel.java
@@ -0,0 +1,9 @@
+package edu.cmu.cs.mvelezce.evaluation.approaches.family.featuremodel;
+
+import java.util.Set;
+
+public interface FeatureModel {
+
+    boolean isValidProduct(Set<String> conf);
+
+}

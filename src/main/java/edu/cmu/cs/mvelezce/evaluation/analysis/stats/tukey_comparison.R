c9a130aa47daa0f41efaad7e91ac9aa8d3d35769
Miguel Velez <miguelvelez@mijecu25.com>
2018-02-26 08:44:34 -0500

    Added tuker comparison script

---
+++ src/main/java/edu/cmu/cs/mvelezce/evaluation/analysis/stats/tukey_comparison.R
@@ -0,0 +1,33 @@
+cc = read.csv("~/Documents/Programming/Java/Projects/ConfigCrusher/src/main/resources/evaluation/programs/java/kanzi/comparison/config_crusher_brute_force.csv")
+fw = read.csv("~/Documents/Programming/Java/Projects/ConfigCrusher/src/main/resources/evaluation/programs/java/kanzi/comparison/feature_wise_brute_force.csv")
+pw = read.csv("~/Documents/Programming/Java/Projects/ConfigCrusher/src/main/resources/evaluation/programs/java/kanzi/comparison/pair_wise_brute_force.csv")
+sp = read.csv("~/Documents/Programming/Java/Projects/ConfigCrusher/src/main/resources/evaluation/programs/java/kanzi/comparison/splat_brute_force.csv")
+
+names(cc)
+
+boxplot(list(cc=cc[cc$measured=="false",]$relative.error,
+             fw=fw[fw$measured=="false",]$relative.error,
+             pw=pw[pw$measured=="false",]$relative.error,
+             sp=sp[sp$measured=="false",]$relative.error))
+
+df = data.frame(error = c(cc[cc$measured=="false",]$relative.error,
+                          fw[fw$measured=="false",]$relative.error,
+                          pw[pw$measured=="false",]$relative.error,
+                          sp[sp$measured=="false",]$relative.error), 
+                treat = c(rep("cc", length(cc[cc$measured=="false",]$relative.error)),
+                          rep("fw", length(fw[fw$measured=="false",]$relative.error)),
+                          rep("pw", length(pw[pw$measured=="false",]$relative.error)),
+                          rep("sp", length(sp[sp$measured=="false",]$relative.error))))
+View(df)
+
+library(nparcomp)
+
+b = nparcomp(error ~ treat, data=df, asy.method = "mult.t",
+            type = "Tukey", alternative = "greater", 
+            plot.simci = TRUE, info = FALSE)
+summary(b)
+
+
+
+
+

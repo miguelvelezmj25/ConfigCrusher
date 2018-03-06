fbecc4b0228fc801a44c34cf3d299f10dec74d5d
Miguel Velez <miguelvelez@mijecu25.com>
2018-03-06 08:15:36 -0500

    Instrumented email

--- src/test/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/TimerInstrumenterTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/instrumentation/java/TimerInstrumenterTest.java
@@ -1066,6 +1066,32 @@ public class TimerInstrumenterTest {
     }
 
     @Test
+    public void email() throws NoSuchMethodException, IOException, IllegalAccessException, InvocationTargetException, InterruptedException {
+        String programName = "email";
+        String entry = "family.PL_Interface_impl";
+        TimerInstrumenterTest.srcDir = USER_HOME + "/Documents/Programming/Java/Projects/performance-mapper-evaluation/instrumented/email";
+        TimerInstrumenterTest.classDir = USER_HOME + "/Documents/Programming/Java/Projects/performance-mapper-evaluation/instrumented/email/target/classes";
+
+//        this.compile();
+
+        // Program arguments
+        String[] args = new String[0];
+
+//        String[] args = new String[1];
+//        args[0] = "-saveres";
+
+        StaticAnalysis analysis = new TaintFlowAnalysis(programName);
+        Map<JavaRegion, Set<Set<String>>> decisionsToOptions = analysis.analyze(args);
+
+        args = new String[2];
+        args[0] = "-delres";
+        args[1] = "-saveres";
+
+        Instrumenter instrumenter = new ConfigCrusherTimerRegionInstrumenter(programName, entry, TimerInstrumenterTest.classDir, decisionsToOptions);
+        instrumenter.instrument(args);
+    }
+
+    @Test
     public void sort() throws NoSuchMethodException, IOException, IllegalAccessException, InvocationTargetException, InterruptedException {
         String programName = "sort";
         String entry = "org.unix4j.sort.Main";

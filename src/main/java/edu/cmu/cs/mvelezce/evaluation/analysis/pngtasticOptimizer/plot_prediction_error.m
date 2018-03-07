1de736f5f2a5d4cfff0a9c70c6a9556614986741
Miguel Velez <miguelvelez@mijecu25.com>
2018-03-07 13:30:26 -0500

    Added supplementary materials

--- src/main/java/edu/cmu/cs/mvelezce/evaluation/analysis/pngtasticOptimizer/plot_prediction_error.m
+++ src/main/java/edu/cmu/cs/mvelezce/evaluation/analysis/pngtasticOptimizer/plot_prediction_error.m
@@ -1,6 +1,6 @@
 x = [5, 16, 10];
 y = [19.67, 0.99, 1.07];
-n = ["PW", "FW/SAD", "CC"];
+n = ["FW", "PW/SAD", "CC"];
 
 scatter(x,y,1500,'.');
 text(x, y, n, 'horizontal','left', 'vertical','bottom')
@@ -18,3 +18,5 @@ fig.PaperSize = [fig_pos(3) fig_pos(4)];
 mkdir('../../../../../../../../resources/evaluation/programs/java/pngtasticOptimizer/plots/');
 fileID = '../../../../../../../../resources/evaluation/programs/java/pngtasticOptimizer/plots/prediction_error.pdf';
 print(fig, fileID,'-dpdf');
+fileID = '../../../../../../../../resources/evaluation/programs/java/pngtasticOptimizer/plots/prediction_error.png';
+print(fig, fileID,'-dpng');
--- src/main/java/edu/cmu/cs/mvelezce/evaluation/analysis/runningexample/plot_prediction_error.m
+++ src/main/java/edu/cmu/cs/mvelezce/evaluation/analysis/runningexample/plot_prediction_error.m
@@ -1,6 +1,6 @@
 x = [10, 56, 512, 8];
 y = [56.91, 6.22, 0.18, 0.07];
-n = ["PW", "FW", "SAD", "CC"];
+n = ["FW", "PW", "SAD", "CC"];
 
 scatter(x,y,1500,'.');
 text(x, y, n, 'horizontal','left', 'vertical','bottom')
@@ -19,3 +19,5 @@ fig.PaperSize = [fig_pos(3) fig_pos(4)];
 mkdir('../../../../../../../../resources/evaluation/programs/java/running-example/plots/');
 fileID = '../../../../../../../../resources/evaluation/programs/java/running-example/plots/prediction_error.pdf';
 print(fig, fileID,'-dpdf');
+fileID = '../../../../../../../../resources/evaluation/programs/java/running-example/plots/prediction_error.png';
+print(fig, fileID,'-dpng');

c6b37ca71e53272efc3c1e31cce709ea1f50b580
Miguel Velez <miguelvelez@mijecu25.com>
2018-03-07 13:30:26 -0500

    Added supplementary materials

--- src/main/java/edu/cmu/cs/mvelezce/evaluation/analysis/prevayler/plot_prediction_error.m
+++ src/main/java/edu/cmu/cs/mvelezce/evaluation/analysis/prevayler/plot_prediction_error.m
@@ -1,6 +1,6 @@
 x = [9, 46, 144, 32];
 y = [111.21, 29.23, 2.95, 9.23];
-n = ["PW", "FW", "SAD", "CC"];
+n = ["FW", "PW", "SAD", "CC"];
 
 scatter(x,y,1500,'.');
 text(x, y, n, 'horizontal','left', 'vertical','bottom')
@@ -18,3 +18,5 @@ fig.PaperSize = [fig_pos(3) fig_pos(4)];
 mkdir('../../../../../../../../resources/evaluation/programs/java/prevayler/plots/');
 fileID = '../../../../../../../../resources/evaluation/programs/java/prevayler/plots/prediction_error.pdf';
 print(fig, fileID,'-dpdf');
+fileID = '../../../../../../../../resources/evaluation/programs/java/prevayler/plots/prediction_error.png';
+print(fig, fileID,'-dpng');

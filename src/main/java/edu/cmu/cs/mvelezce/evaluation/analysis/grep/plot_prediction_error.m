44d14dc1b0ff75a1eecbb71bb7aa36c3fe83425b
Miguel Velez <miguelvelez@mijecu25.com>
2018-03-07 13:30:26 -0500

    Added supplementary materials

--- src/main/java/edu/cmu/cs/mvelezce/evaluation/analysis/grep/plot_prediction_error.m
+++ src/main/java/edu/cmu/cs/mvelezce/evaluation/analysis/grep/plot_prediction_error.m
@@ -1,6 +1,6 @@
 x = [7, 29, 48, 64];
 y = [32.14, 114.74, 1.94, 3.58];
-n = ["PW", "FW", "SAD", "CC"];
+n = ["FW", "PW", "SAD", "CC"];
 
 scatter(x,y,1500,'.');
 text(x, y, n, 'horizontal','left', 'vertical','bottom')
@@ -18,3 +18,5 @@ fig.PaperSize = [fig_pos(3) fig_pos(4)];
 mkdir('../../../../../../../../resources/evaluation/programs/java/grep/plots/');
 fileID = '../../../../../../../../resources/evaluation/programs/java/grep/plots/prediction_error.pdf';
 print(fig, fileID,'-dpdf');
+fileID = '../../../../../../../../resources/evaluation/programs/java/grep/plots/prediction_error.png';
+print(fig, fileID,'-dpng');

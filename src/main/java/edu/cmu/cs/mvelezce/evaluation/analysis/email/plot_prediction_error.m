a2b1f6d87c7630758f083022c3db55a4d97f30f2
Miguel Velez <miguelvelez@mijecu25.com>
2018-03-07 17:08:22 -0500

    Added last error plot

--- src/main/java/edu/cmu/cs/mvelezce/evaluation/analysis/email/plot_prediction_error.m
+++ src/main/java/edu/cmu/cs/mvelezce/evaluation/analysis/email/plot_prediction_error.m
@@ -1,6 +1,6 @@
-x = [4, 11, 1];
-y = [100, 44.41, 2.34];
-n = ["FW", "PW"', "FB"];
+x = [4, 11, 1, 256];
+y = [100, 44.23, 2.34, 23.02];
+n = ["FW", "PW"', "FB", "CC"];
 
 scatter(x,y,1500,'.');
 text(x, y, n, 'horizontal','left', 'vertical','bottom')
@@ -18,3 +18,5 @@ fig.PaperSize = [fig_pos(3) fig_pos(4)];
 mkdir('../../../../../../../../resources/evaluation/programs/java/email/plots/');
 fileID = '../../../../../../../../resources/evaluation/programs/java/email/plots/prediction_error.pdf';
 print(fig, fileID,'-dpdf');
+fileID = '../../../../../../../../resources/evaluation/programs/java/email/plots/prediction_error.png';
+print(fig, fileID,'-dpng');

700f1f484c09f92a83d0b4855044054fcb56d727
Miguel Velez <miguelvelez@mijecu25.com>
2018-03-09 11:45:27 -0500

    got bf/splat data

--- src/main/java/edu/cmu/cs/mvelezce/evaluation/approaches/splat/execute/SPLatExecutor.java
+++ src/main/java/edu/cmu/cs/mvelezce/evaluation/approaches/splat/execute/SPLatExecutor.java
@@ -1,6 +1,6 @@
 package edu.cmu.cs.mvelezce.evaluation.approaches.splat.execute;
 
-import edu.cmu.cs.mvelezce.evaluation.approaches.UsesBFExecutor;
+import edu.cmu.cs.mvelezce.evaluation.approaches.bruteforce.UsesBFExecutor;
 
 public class SPLatExecutor extends UsesBFExecutor {
 

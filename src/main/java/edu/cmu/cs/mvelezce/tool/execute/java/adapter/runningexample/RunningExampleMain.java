ee9e3ab7707c3a40f1934500efb9f5555b81b430
Miguel Velez <miguelvelez@mijecu25.com>
2018-10-08 08:43:11 -0400

    ran running example in different directory

--- src/main/java/edu/cmu/cs/mvelezce/tool/execute/java/adapter/runningexample/RunningExampleMain.java
+++ src/main/java/edu/cmu/cs/mvelezce/tool/execute/java/adapter/runningexample/RunningExampleMain.java
@@ -10,7 +10,6 @@ import edu.cmu.cs.mvelezce.tool.execute.java.adapter.BaseMain;
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.Main;
 import edu.cmu.cs.mvelezce.tool.instrumentation.java.BaseRegionInstrumenter;
 import edu.cmu.cs.mvelezce.tool.instrumentation.java.ConfigCrusherTimerRegionInstrumenter;
-
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
@@ -19,65 +18,68 @@ import java.util.Set;
 
 public class RunningExampleMain extends BaseMain {
 
-    public static final String RUNNING_EXAMPLE_MAIN = RunningExampleMain.class.getCanonicalName();
+  public static final String RUNNING_EXAMPLE_MAIN = RunningExampleMain.class.getCanonicalName();
+  public static final String PROGRAM_NAME = "running-example";
+  public static final String ENTRY_POINT = "edu.cmu.cs.mvelezce.Example";
 
-    public RunningExampleMain(String programName, String iteration, String[] args) {
-        super(programName, iteration, args);
+  public RunningExampleMain(String programName, String iteration, String[] args) {
+    super(programName, iteration, args);
+  }
+
+  public static void main(String[] args) throws IOException {
+    String programName = args[0];
+    String mainClass = args[1];
+    String iteration = args[2];
+    String[] sleepArgs = Arrays.copyOfRange(args, 3, args.length);
+
+    Main main = new RunningExampleMain(programName, iteration, sleepArgs);
+    main.execute(mainClass, sleepArgs);
+    main.logExecution();
+  }
+
+  @Override
+  public void logExecution() throws IOException {
+    Adapter adapter = new RunningExampleAdapter();
+    Set<String> configuration = adapter.configurationAsSet(this.getArgs());
+
+    ConfigCrusherExecutor executor = new ConfigCrusherExecutor(this.getProgramName());
+    Map<String, Long> results = executor.getResults();
+    executor.writeToFile(this.getIteration(), configuration, results);
+  }
+
+  @Override
+  public void execute(String mainClass, String[] args) {
+    try {
+      BaseRegionInstrumenter instrumenter = new ConfigCrusherTimerRegionInstrumenter(PROGRAM_NAME);
+      instrumenter.instrument(args);
+      Set<JavaRegion> regions = instrumenter.getRegionsToOptionSet().keySet();
+
+      for (JavaRegion region : regions) {
+        Regions.regionsToOverhead.put(region.getRegionID(), 0L);
+      }
+
+      Regions.regionsToOverhead.put(Regions.PROGRAM_REGION_ID, 0L);
+    } catch (InvocationTargetException
+        | NoSuchMethodException
+        | IOException
+        | IllegalAccessException
+        | InterruptedException e) {
+      throw new RuntimeException("Could not add regions to the Regions class");
     }
 
-    public static void main(String[] args) throws IOException {
-        String programName = args[0];
-        String mainClass = args[1];
-        String iteration = args[2];
-        String[] sleepArgs = Arrays.copyOfRange(args, 3, args.length);
+    if (mainClass.contains("Example")) {
+      Region program = new Region(Regions.PROGRAM_REGION_ID);
 
-        Main main = new RunningExampleMain(programName, iteration, sleepArgs);
-        main.execute(mainClass, sleepArgs);
-        main.logExecution();
+      try {
+        Regions.enter(program.getRegionID());
+        Example.main(args);
+      } catch (Exception e) {
+        e.printStackTrace();
+      } finally {
+        Regions.exit(program.getRegionID());
+      }
+    } else {
+      throw new RuntimeException("Could not find the main class " + mainClass);
     }
-
-    @Override
-    public void logExecution() throws IOException {
-        Adapter adapter = new RunningExampleAdapter();
-        Set<String> configuration = adapter.configurationAsSet(this.getArgs());
-
-        ConfigCrusherExecutor executor = new ConfigCrusherExecutor(this.getProgramName());
-        Map<String, Long> results = executor.getResults();
-        executor.writeToFile(this.getIteration(), configuration, results);
-    }
-
-    @Override
-    public void execute(String mainClass, String[] args) {
-        try {
-            BaseRegionInstrumenter instrumenter = new ConfigCrusherTimerRegionInstrumenter("running-example");
-            instrumenter.instrument(args);
-            Set<JavaRegion> regions = instrumenter.getRegionsToOptionSet().keySet();
-
-            for(JavaRegion region : regions) {
-                Regions.regionsToOverhead.put(region.getRegionID(), 0L);
-            }
-            Regions.regionsToOverhead.put(Regions.PROGRAM_REGION_ID, 0L);
-        }
-        catch(InvocationTargetException | NoSuchMethodException | IOException | IllegalAccessException | InterruptedException e) {
-            throw new RuntimeException("Could not add regions to the Regions class");
-        }
-
-        if(mainClass.contains("Example")) {
-            Region program = new Region(Regions.PROGRAM_REGION_ID);
-
-            try {
-                Regions.enter(program.getRegionID());
-                Example.main(args);
-            }
-            catch(Exception e) {
-                e.printStackTrace();
-            }
-            finally {
-                Regions.exit(program.getRegionID());
-            }
-        }
-        else {
-            throw new RuntimeException("Could not find the main class " + mainClass);
-        }
-    }
+  }
 }

af115e4b06cddddf4a7312169987a618682d3eca
Miguel Velez <miguelvelez@mijecu25.com>
2018-10-09 16:02:09 -0400

    refactored ground truth

--- src/main/java/edu/cmu/cs/mvelezce/tool/execute/java/adapter/find/FindMain.java
+++ src/main/java/edu/cmu/cs/mvelezce/tool/execute/java/adapter/find/FindMain.java
@@ -9,7 +9,6 @@ import edu.cmu.cs.mvelezce.tool.execute.java.adapter.BaseMain;
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.Main;
 import edu.cmu.cs.mvelezce.tool.instrumentation.java.BaseRegionInstrumenter;
 import edu.cmu.cs.mvelezce.tool.instrumentation.java.ConfigCrusherTimerRegionInstrumenter;
-
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
@@ -18,65 +17,63 @@ import java.util.Set;
 
 public class FindMain extends BaseMain {
 
-    public static final String FIND_MAIN = FindMain.class.getCanonicalName();
+  public static final String FIND_MAIN = FindMain.class.getCanonicalName();
+  public static final String PROGRAM_NAME = "find";
 
-    public FindMain(String programName, String iteration, String[] args) {
-        super(programName, iteration, args);
+  public FindMain(String programName, String iteration, String[] args) {
+    super(programName, iteration, args);
+  }
+
+  public static void main(String[] args) throws IOException {
+    String programName = args[0];
+    String mainClass = args[1];
+    String iteration = args[2];
+    String[] sleepArgs = Arrays.copyOfRange(args, 3, args.length);
+
+    Main main = new FindMain(programName, iteration, sleepArgs);
+    main.execute(mainClass, sleepArgs);
+    main.logExecution();
+  }
+
+  @Override
+  public void logExecution() throws java.io.IOException {
+    Adapter adapter = new FindAdapter();
+    Set<String> configuration = adapter.configurationAsSet(this.getArgs());
+
+    ConfigCrusherExecutor executor = new ConfigCrusherExecutor(this.getProgramName());
+    Map<String, Long> results = executor.getResults();
+    executor.writeToFile(this.getIteration(), configuration, results);
+  }
+
+  @Override
+  public void execute(String mainClass, String[] args) {
+    try {
+      BaseRegionInstrumenter instrumenter = new ConfigCrusherTimerRegionInstrumenter("find");
+      instrumenter.instrument(args);
+      Set<JavaRegion> regions = instrumenter.getRegionsToOptionSet().keySet();
+
+      for (JavaRegion region : regions) {
+        Regions.regionsToOverhead.put(region.getRegionID(), 0L);
+      }
+      Regions.regionsToOverhead.put(Regions.PROGRAM_REGION_ID, 0L);
+    } catch (InvocationTargetException | NoSuchMethodException | IOException | IllegalAccessException | InterruptedException e) {
+      throw new RuntimeException("Could not add regions to the Regions class");
     }
 
-    public static void main(String[] args) throws IOException {
-        String programName = args[0];
-        String mainClass = args[1];
-        String iteration = args[2];
-        String[] sleepArgs = Arrays.copyOfRange(args, 3, args.length);
-
-        Main main = new FindMain(programName, iteration, sleepArgs);
-        main.execute(mainClass, sleepArgs);
-        main.logExecution();
+    if (mainClass.contains("Main")) {
+      Region program = new Region(Regions.PROGRAM_REGION_ID);
+      try {
+        Regions.enter(program.getRegionID());
+        org.unix4j.find.Main.main(args);
+      } catch (Exception e) {
+        e.printStackTrace();
+      } finally {
+        Regions.exit(program.getRegionID());
+      }
+    }
+    else {
+      throw new RuntimeException("Could not find the main class " + mainClass);
     }
 
-    @Override
-    public void logExecution() throws java.io.IOException {
-        Adapter adapter = new FindAdapter();
-        Set<String> configuration = adapter.configurationAsSet(this.getArgs());
-
-        ConfigCrusherExecutor executor = new ConfigCrusherExecutor(this.getProgramName());
-        Map<String, Long> results = executor.getResults();
-        executor.writeToFile(this.getIteration(), configuration, results);
-    }
-
-    @Override
-    public void execute(String mainClass, String[] args) {
-        try {
-            BaseRegionInstrumenter instrumenter = new ConfigCrusherTimerRegionInstrumenter("find");
-            instrumenter.instrument(args);
-            Set<JavaRegion> regions = instrumenter.getRegionsToOptionSet().keySet();
-
-            for(JavaRegion region : regions) {
-                Regions.regionsToOverhead.put(region.getRegionID(), 0L);
-            }
-            Regions.regionsToOverhead.put(Regions.PROGRAM_REGION_ID, 0L);
-        }
-        catch(InvocationTargetException | NoSuchMethodException | IOException | IllegalAccessException | InterruptedException e) {
-            throw new RuntimeException("Could not add regions to the Regions class");
-        }
-
-        if(mainClass.contains("Main")) {
-            Region program = new Region(Regions.PROGRAM_REGION_ID);
-            try {
-                Regions.enter(program.getRegionID());
-                org.unix4j.find.Main.main(args);
-            }
-            catch(Exception e) {
-                e.printStackTrace();
-            }
-            finally {
-                Regions.exit(program.getRegionID());
-            }
-        }
-        else {
-            throw new RuntimeException("Could not find the main class " + mainClass);
-        }
-
-    }
+  }
 }
--- src/main/java/edu/cmu/cs/mvelezce/tool/execute/java/adapter/kanzi/KanziMain.java
+++ src/main/java/edu/cmu/cs/mvelezce/tool/execute/java/adapter/kanzi/KanziMain.java
@@ -9,75 +9,72 @@ import edu.cmu.cs.mvelezce.tool.execute.java.adapter.BaseMain;
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.Main;
 import edu.cmu.cs.mvelezce.tool.instrumentation.java.BaseRegionInstrumenter;
 import edu.cmu.cs.mvelezce.tool.instrumentation.java.ConfigCrusherTimerRegionInstrumenter;
-import kanzi.Run;
-
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
+import kanzi.Run;
 
 public class KanziMain extends BaseMain {
 
-    public static final String KANZI_MAIN = KanziMain.class.getCanonicalName();
+  public static final String KANZI_MAIN = KanziMain.class.getCanonicalName();
+  public static final String PROGRAM_NAME = "kanzi";
 
-    public KanziMain(String programName, String iteration, String[] args) {
-        super(programName, iteration, args);
+  public KanziMain(String programName, String iteration, String[] args) {
+    super(programName, iteration, args);
+  }
+
+  public static void main(String[] args) throws IOException {
+    String programName = args[0];
+    String mainClass = args[1];
+    String iteration = args[2];
+    String[] sleepArgs = Arrays.copyOfRange(args, 3, args.length);
+
+    Main main = new KanziMain(programName, iteration, sleepArgs);
+    main.execute(mainClass, sleepArgs);
+    main.logExecution();
+  }
+
+  @Override
+  public void logExecution() throws IOException {
+    Adapter adapter = new KanziAdapter();
+    Set<String> configuration = adapter.configurationAsSet(this.getArgs());
+
+    ConfigCrusherExecutor executor = new ConfigCrusherExecutor(this.getProgramName());
+    Map<String, Long> results = executor.getResults();
+    executor.writeToFile(this.getIteration(), configuration, results);
+  }
+
+  @Override
+  public void execute(String mainClass, String[] args) {
+    try {
+      BaseRegionInstrumenter instrumenter = new ConfigCrusherTimerRegionInstrumenter("kanzi");
+      instrumenter.instrument(args);
+      Set<JavaRegion> regions = instrumenter.getRegionsToOptionSet().keySet();
+
+      for (JavaRegion region : regions) {
+        Regions.regionsToOverhead.put(region.getRegionID(), 0L);
+      }
+      Regions.regionsToOverhead.put(Regions.PROGRAM_REGION_ID, 0L);
+    } catch (InvocationTargetException | NoSuchMethodException | IOException | IllegalAccessException | InterruptedException e) {
+      throw new RuntimeException("Could not add regions to the Regions class");
     }
 
-    public static void main(String[] args) throws IOException {
-        String programName = args[0];
-        String mainClass = args[1];
-        String iteration = args[2];
-        String[] sleepArgs = Arrays.copyOfRange(args, 3, args.length);
-
-        Main main = new KanziMain(programName, iteration, sleepArgs);
-        main.execute(mainClass, sleepArgs);
-        main.logExecution();
+    if (mainClass.contains("Run")) {
+      Region program = new Region(Regions.PROGRAM_REGION_ID);
+      try {
+        Regions.enter(program.getRegionID());
+        Run.main(args);
+      } catch (Exception e) {
+        e.printStackTrace();
+      } finally {
+        Regions.exit(program.getRegionID());
+      }
+    }
+    else {
+      throw new RuntimeException("Could not find the main class " + mainClass);
     }
 
-    @Override
-    public void logExecution() throws IOException {
-        Adapter adapter = new KanziAdapter();
-        Set<String> configuration = adapter.configurationAsSet(this.getArgs());
-
-        ConfigCrusherExecutor executor = new ConfigCrusherExecutor(this.getProgramName());
-        Map<String, Long> results = executor.getResults();
-        executor.writeToFile(this.getIteration(), configuration, results);
-    }
-
-    @Override
-    public void execute(String mainClass, String[] args) {
-        try {
-            BaseRegionInstrumenter instrumenter = new ConfigCrusherTimerRegionInstrumenter("kanzi");
-            instrumenter.instrument(args);
-            Set<JavaRegion> regions = instrumenter.getRegionsToOptionSet().keySet();
-
-            for(JavaRegion region : regions) {
-                Regions.regionsToOverhead.put(region.getRegionID(), 0L);
-            }
-            Regions.regionsToOverhead.put(Regions.PROGRAM_REGION_ID, 0L);
-        }
-        catch(InvocationTargetException | NoSuchMethodException | IOException | IllegalAccessException | InterruptedException e) {
-            throw new RuntimeException("Could not add regions to the Regions class");
-        }
-
-        if(mainClass.contains("Run")) {
-            Region program = new Region(Regions.PROGRAM_REGION_ID);
-            try {
-                Regions.enter(program.getRegionID());
-                Run.main(args);
-            }
-            catch(Exception e) {
-                e.printStackTrace();
-            }
-            finally {
-                Regions.exit(program.getRegionID());
-            }
-        }
-        else {
-            throw new RuntimeException("Could not find the main class " + mainClass);
-        }
-
-    }
+  }
 }

2cc8f01a4a03db47c6b42d965f5827de14d5ed62
Miguel Velez <miguelvelez@mijecu25.com>
2018-10-12 16:58:10 -0400

    implemented build configuration

--- src/main/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/phosphor/TaintAnalysis.java
+++ src/main/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/phosphor/TaintAnalysis.java
@@ -3,19 +3,31 @@ package edu.cmu.cs.mvelezce.tool.analysis.taint.java.phosphor;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import org.apache.commons.lang3.tuple.Pair;
 
 public class TaintAnalysis {
 
   /**
-   * In theory, this method should also take the set of options O of the program. However,
-   * since we represent options set to false by not including them in the set that represents
-   * configurations, there is no need to pass them in the method.
+   * In theory, this method should also take the set of options O of the program. However, since we
+   * represent options set to false by not including them in the set that represents configurations,
+   * there is no need to pass them in the method.
    *
-   * Example:
-   * config = {A, C} means that the configurations is A=T, B=F, C=T.
+   * Example: config = {A, C} means that the configurations is A=T, B=F, C=T.
+   */
+  static Pair<Map<String, Boolean>, Set<String>> buildConfiguration(
+      Set<Map<String, Boolean>> constraintsToEvaluate) {
+    Map<String, Boolean> constraintToEvaluate = constraintsToEvaluate.iterator().next();
+    Set<String> config = completeConfig(constraintToEvaluate);
+
+    return Pair.of(constraintToEvaluate, config);
+  }
+
+  /**
+   * In theory, this method should also take the set of options O of the program. However, since we
+   * represent options set to false by not including them in the set that represents configurations,
+   * there is no need to pass them in the method.
    *
-   * @param constraintToEvaluate
-   * @return
+   * Example: config = {A, C} means that the configurations is A=T, B=F, C=T.
    */
   static Set<String> completeConfig(Map<String, Boolean> constraintToEvaluate) {
     if (constraintToEvaluate.isEmpty()) {
--- src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/phosphor/TaintAnalysisTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/phosphor/TaintAnalysisTest.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -17,25 +18,70 @@ public class TaintAnalysisTest {
   }
 
   @Test
-  public void completeConfig_forConstraintsWithFalse() {
-    Map<String, Boolean> constraintToEvaluate = new HashMap<>();
-    constraintToEvaluate.put("A", false);
-    constraintToEvaluate.put("B", false);
+  public void completeConfig_forConstraint_NotA_NotB() {
+    Map<String, Boolean> constraintToEvaluate = this.buildConstraint_notA_notB();
 
     Set<String> config = TaintAnalysis.completeConfig(constraintToEvaluate);
     Assert.assertTrue(config.isEmpty());
   }
 
   @Test
-  public void completeConfig_forConstraintsWithTrue() {
-    Map<String, Boolean> constraintToEvaluate = new HashMap<>();
-    constraintToEvaluate.put("A", true);
-    constraintToEvaluate.put("B", false);
+  public void completeConfig_forConstraint_A_notB() {
+    Map<String, Boolean> constraintToEvaluate = this.buildConstraint_A_notB();
 
     Set<String> expectedConfig = new HashSet<>();
     expectedConfig.add("A");
 
     Set<String> config = TaintAnalysis.completeConfig(constraintToEvaluate);
     Assert.assertEquals(expectedConfig, config);
+  }
+
+  @Test
+  public void buildConfiguration_forConstraint_notA_B() {
+    Map<String, Boolean> constraint_notA_b = this.buildConstraint_notA_B();
+
+    Set<Map<String, Boolean>> constraintsToEvaluate = new HashSet<>();
+    constraintsToEvaluate.add(constraint_notA_b);
+
+    Set<String> expectedConfig = new HashSet<>();
+    expectedConfig.add("B");
+
+    Pair<Map<String, Boolean>, Set<String>> results = TaintAnalysis
+        .buildConfiguration(constraintsToEvaluate);
+
+    Assert.assertEquals(constraint_notA_b, results.getLeft());
+    Assert.assertEquals(expectedConfig, results.getRight());
+  }
+
+  private Map<String, Boolean> buildConstraint_notA_notB() {
+    Map<String, Boolean> constraint = new HashMap<>();
+    constraint.put("A", false);
+    constraint.put("B", false);
+
+    return constraint;
+  }
+
+  private Map<String, Boolean> buildConstraint_notA_B() {
+    Map<String, Boolean> constraint = new HashMap<>();
+    constraint.put("A", false);
+    constraint.put("B", true);
+
+    return constraint;
+  }
+
+  private Map<String, Boolean> buildConstraint_A_notB() {
+    Map<String, Boolean> constraint = new HashMap<>();
+    constraint.put("A", true);
+    constraint.put("B", false);
+
+    return constraint;
+  }
+
+  private Map<String, Boolean> buildConstraint_A_B() {
+    Map<String, Boolean> constraint = new HashMap<>();
+    constraint.put("A", true);
+    constraint.put("B", true);
+
+    return constraint;
   }
 }

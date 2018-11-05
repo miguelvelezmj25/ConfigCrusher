7c402dcccb9f0bf513b25e0760c7a0cbe3d79bad
Miguel Velez <miguelvelez@mijecu25.com>
2018-11-05 12:44:28 -0500

    added logic to track the number of times the branches are executed

--- src/main/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/groundtruth/BranchCoverageLogger.java
+++ src/main/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/groundtruth/BranchCoverageLogger.java
@@ -7,11 +7,12 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 import jdk.internal.org.objectweb.asm.Type;
+import org.apache.commons.lang3.tuple.MutablePair;
 
 public class BranchCoverageLogger {
 
   private static final Map<String, String> METHODS_TO_DESCRIPTORS = new HashMap<>();
-  private static final Map<String, Boolean> DECISIONS_TO_RESULT = new HashMap<>();
+  private static final Map<String, MutablePair<Integer, Integer>> DECISIONS_TO_BRANCH_COUNTS = new HashMap<>();
 
   static final String RESULTS_FILE = "results.ser";
   static final String INTERNAL_NAME = Type.getInternalName(BranchCoverageLogger.class);
@@ -20,27 +21,40 @@ public class BranchCoverageLogger {
   static {
     Method[] methods = BranchCoverageLogger.class.getDeclaredMethods();
 
-    for (int i = 0; i < methods.length; i++) {
-      Method method = methods[i];
+    for (Method method : methods) {
       METHODS_TO_DESCRIPTORS.put(method.getName(), Type.getMethodDescriptor(method));
     }
   }
 
   public static void logIFEQDecision(int decisionValue, String methodName, int decisionCount) {
-    boolean result = false;
+    String decision = methodName + "." + decisionCount;
+    addDecision(decision);
+    MutablePair<Integer, Integer> thenElseCounts = DECISIONS_TO_BRANCH_COUNTS.get(decision);
 
     if (decisionValue == 1) {
-      result = true;
+      int thenCounts = thenElseCounts.getLeft();
+      thenCounts++;
+      thenElseCounts.setLeft(thenCounts);
     }
+    else {
+      int elseCounts = thenElseCounts.getRight();
+      elseCounts++;
+      thenElseCounts.setRight(elseCounts);
+    }
+  }
 
-    DECISIONS_TO_RESULT.put(methodName + "." + decisionCount, result);
+  private static void addDecision(String decision) {
+    if (!DECISIONS_TO_BRANCH_COUNTS.containsKey(decision)) {
+      MutablePair<Integer, Integer> thenElseCounts = MutablePair.of(0, 0);
+      DECISIONS_TO_BRANCH_COUNTS.put(decision, thenElseCounts);
+    }
   }
 
   public static void saveExecutedDecisions() {
     try {
       FileOutputStream fos = new FileOutputStream(RESULTS_FILE);
       ObjectOutputStream oos = new ObjectOutputStream(fos);
-      oos.writeObject(DECISIONS_TO_RESULT);
+      oos.writeObject(DECISIONS_TO_BRANCH_COUNTS);
       oos.close();
       fos.close();
     }

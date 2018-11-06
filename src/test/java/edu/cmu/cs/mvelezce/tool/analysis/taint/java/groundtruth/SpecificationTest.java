6f199c8551e48a9e8d56da03cb62f3eea7d6be82
Miguel Velez <miguelvelez@mijecu25.com>
2018-11-06 10:33:42 -0500

    ran the tracking specification on simple example1

--- src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/groundtruth/SpecificationTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/groundtruth/SpecificationTest.java
@@ -3,6 +3,7 @@ package edu.cmu.cs.mvelezce.tool.analysis.taint.java.groundtruth;
 import edu.cmu.cs.mvelezce.tool.analysis.region.JavaRegion;
 import edu.cmu.cs.mvelezce.tool.analysis.taint.java.dynamic.DynamicAnalysis;
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.dynamicrunningexample.DynamicRunningExampleAdapter;
+import edu.cmu.cs.mvelezce.tool.execute.java.adapter.simpleexample1.SimpleExample1Adapter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -28,6 +29,32 @@ public class SpecificationTest {
     expectedDecisionsToOptions.put("main([Ljava/lang/String;)V.2", SpecificationTest.setA());
     expectedDecisionsToOptions.put("foo(Z)V.1", SpecificationTest.setAB());
 
+    Map<String, Set<String>> results = SpecificationTest.getDecisionToOptions(table);
+
+    Assert.assertEquals(expectedDecisionsToOptions, results);
+  }
+
+  @Test
+  public void simpleExample1() throws IOException, InterruptedException {
+    String programName = SimpleExample1Adapter.PROGRAM_NAME;
+
+    // Program arguments
+    String[] args = new String[0];
+
+    DynamicAnalysis<DecisionBranchCountTable> analysis = new BranchCoverageAnalysis(programName);
+    Map<JavaRegion, DecisionBranchCountTable> table = analysis.analyze(args);
+
+    Map<String, Set<String>> expectedDecisionsToOptions = new HashMap<>();
+    expectedDecisionsToOptions.put("main([Ljava/lang/String;)V.1", SpecificationTest.setA());
+    expectedDecisionsToOptions.put("main([Ljava/lang/String;)V.2", SpecificationTest.setA());
+
+    Map<String, Set<String>> results = SpecificationTest.getDecisionToOptions(table);
+
+    Assert.assertEquals(expectedDecisionsToOptions, results);
+  }
+
+  private static Map<String, Set<String>> getDecisionToOptions(
+      Map<JavaRegion, DecisionBranchCountTable> table) {
     Map<String, Set<String>> results = new HashMap<>();
 
     Specification spec = new Specification();
@@ -38,7 +65,7 @@ public class SpecificationTest {
       results.put(region.getRegionMethod() + "." + region.getStartRegionIndex(), options);
     }
 
-    Assert.assertEquals(expectedDecisionsToOptions, results);
+    return results;
   }
 
   private static Set<String> emptySet() {

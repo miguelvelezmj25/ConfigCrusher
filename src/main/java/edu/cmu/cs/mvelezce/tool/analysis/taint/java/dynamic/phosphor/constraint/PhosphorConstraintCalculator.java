13c807363e342cac7467e9ac9e976e62d66860fb
Miguel Velez <miguelvelez@mijecu25.com>
2019-04-05 15:31:35 -0400

    implemented algo to derive constraints after running one execution of phosphor

---
+++ src/main/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/dynamic/phosphor/constraint/PhosphorConstraintCalculator.java
@@ -0,0 +1,104 @@
+package edu.cmu.cs.mvelezce.tool.analysis.taint.java.dynamic.phosphor.constraint;
+
+import edu.cmu.cs.mvelezce.cc.DecisionTaints;
+import edu.cmu.cs.mvelezce.tool.Helper;
+import edu.cmu.cs.mvelezce.tool.analysis.taint.java.dynamic.phosphor.ConfigConstraint;
+import edu.columbia.cs.psl.phosphor.runtime.Taint;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class PhosphorConstraintCalculator {
+
+  private final List<String> options;
+
+  PhosphorConstraintCalculator(List<String> options) {
+    this.options = options;
+  }
+
+  Set<ConfigConstraint> deriveConstraints(Set<DecisionTaints> results, Set<String> config) {
+    Set<ConfigConstraint> constraints = new HashSet<>();
+
+    for (DecisionTaints decisionTaints : results) {
+      constraints.addAll(deriveConstraints(decisionTaints, config));
+    }
+
+    return constraints;
+  }
+
+  private Set<ConfigConstraint> deriveConstraints(DecisionTaints decisionTaints,
+      Set<String> config) {
+    Set<ConfigConstraint> constraints = new HashSet<>();
+    Set<String> conditionTaints = this.getConditionTaints(decisionTaints);
+    Set<String> contextTaints = this.getContextTaints(decisionTaints);
+
+    Set<String> taintedOptions = new HashSet<>(conditionTaints);
+    taintedOptions.addAll(contextTaints);
+    Set<String> activatedContextOptions = new HashSet<>(contextTaints);
+    activatedContextOptions.retainAll(config);
+    Set<String> optionsInCondition = new HashSet<>(conditionTaints);
+    optionsInCondition.removeAll(contextTaints);
+    Set<Set<String>> combos = Helper.getConfigurations(optionsInCondition);
+
+    for (Set<String> combo : combos) {
+      Set<String> activatedTaintOptions = new HashSet<>(activatedContextOptions);
+      activatedTaintOptions.addAll(combo);
+
+      Set<String> partialConfig = new HashSet<>();
+
+      for (String taintedOption : taintedOptions) {
+        if (activatedTaintOptions.contains(taintedOption)) {
+          partialConfig.add(taintedOption);
+        }
+      }
+
+      ConfigConstraint constraint = ConfigConstraint.fromConfig(partialConfig, taintedOptions);
+      constraints.add(constraint);
+    }
+
+    return constraints;
+  }
+
+  private Set<String> getContextTaints(DecisionTaints decisionTaints) {
+    Taint contextTaintObject = decisionTaints.getExecCtxTaints();
+    Set<String> contextTaints = new HashSet<>();
+
+    if (contextTaintObject != null) {
+      contextTaints = getTaintingOptions(contextTaintObject);
+    }
+
+    return contextTaints;
+  }
+
+  private Set<String> getConditionTaints(DecisionTaints decisionTaints) {
+    Taint conditionTaintObject = decisionTaints.getConditionTaints();
+    return getTaintingOptions(conditionTaintObject);
+  }
+
+  private Set<String> getTaintingOptions(Taint taint) {
+    Set<String> taintingOptions = new HashSet<>();
+    int[] tags = taint.getTags();
+
+    if (tags == null) {
+      throw new RuntimeException("You need to use the tags array for tainting");
+    }
+
+    if (tags.length > 1) {
+      throw new RuntimeException("Implement how to handle array tags with more than 1 entry");
+    }
+
+    int tag = tags[0];
+
+    for (int i = 0; tag != 0; i++) {
+      if (tag % 2 == 1) {
+        String taintingOption = this.options.get(i);
+        taintingOptions.add(taintingOption);
+      }
+
+      tag = tag >> 1;
+    }
+
+    return taintingOptions;
+  }
+
+}
---
+++ src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/dynamic/phosphor/constraint/PhosphorConstraintCalculatorTest.java
@@ -0,0 +1,282 @@
+package edu.cmu.cs.mvelezce.tool.analysis.taint.java.dynamic.phosphor.constraint;
+
+import edu.cmu.cs.mvelezce.cc.DecisionTaints;
+import edu.cmu.cs.mvelezce.tool.analysis.taint.java.dynamic.phosphor.ConfigConstraint;
+import edu.columbia.cs.psl.phosphor.runtime.Taint;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+public class PhosphorConstraintCalculatorTest {
+
+  private static final List<String> OPTIONS = new ArrayList<>();
+
+  @Mock
+  private Taint execCtxTaints;
+
+  @Mock
+  private Taint conditionTaints;
+
+  @Mock
+  private DecisionTaints decisionTaints;
+
+  @BeforeClass
+  public static void init() {
+    OPTIONS.add("A");
+    OPTIONS.add("B");
+    OPTIONS.add("C");
+  }
+
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+
+    Mockito.when(decisionTaints.getConditionTaints()).thenReturn(conditionTaints);
+    Mockito.when(decisionTaints.getExecCtxTaints()).thenReturn(execCtxTaints);
+  }
+
+  @Test
+  public void deriveConstraints_forEmpty() {
+    Set<DecisionTaints> results = new HashSet<>();
+    Set<String> config = new HashSet<>();
+
+    PhosphorConstraintCalculator phosphorConstraintCalculator = new PhosphorConstraintCalculator(
+        OPTIONS);
+    Set<ConfigConstraint> constraints = phosphorConstraintCalculator
+        .deriveConstraints(results, config);
+
+    Assert.assertTrue(constraints.isEmpty());
+  }
+
+  @Test
+  public void deriveConstraints_forSimpleConditionNotContext() {
+    Mockito.when(conditionTaints.getTags()).thenReturn(new int[]{1});
+    Mockito.when(execCtxTaints.getTags()).thenReturn(new int[]{0});
+
+    Set<DecisionTaints> results = new HashSet<>();
+    results.add(decisionTaints);
+
+    Set<String> config = new HashSet<>();
+
+    PhosphorConstraintCalculator phosphorConstraintCalculator = new PhosphorConstraintCalculator(
+        OPTIONS);
+    Set<ConfigConstraint> constraints = phosphorConstraintCalculator
+        .deriveConstraints(results, config);
+
+    Assert.assertEquals(2, constraints.size());
+    Assert.assertTrue(constraints.contains(getNotAConstraint()));
+    Assert.assertTrue(constraints.contains(getAConstraint()));
+  }
+
+  @Test
+  public void deriveConstraints_forMultiConditionNotContext() {
+    Mockito.when(conditionTaints.getTags()).thenReturn(new int[]{5});
+    Mockito.when(execCtxTaints.getTags()).thenReturn(new int[]{0});
+
+    Set<DecisionTaints> results = new HashSet<>();
+    results.add(decisionTaints);
+
+    Set<String> config = new HashSet<>();
+
+    PhosphorConstraintCalculator phosphorConstraintCalculator = new PhosphorConstraintCalculator(
+        OPTIONS);
+    Set<ConfigConstraint> constraints = phosphorConstraintCalculator
+        .deriveConstraints(results, config);
+
+    Assert.assertEquals(4, constraints.size());
+    Assert.assertTrue(constraints.contains(getACConstraint()));
+    Assert.assertTrue(constraints.contains(getANotCConstraint()));
+    Assert.assertTrue(constraints.contains(getNotACConstraint()));
+    Assert.assertTrue(constraints.contains(getNotANotCConstraint()));
+  }
+
+  @Test
+  public void deriveConstraints_forMultiConditionSimpleContext() {
+    Mockito.when(conditionTaints.getTags()).thenReturn(new int[]{5});
+    Mockito.when(execCtxTaints.getTags()).thenReturn(new int[]{2});
+
+    Set<DecisionTaints> results = new HashSet<>();
+    results.add(decisionTaints);
+
+    Set<String> config = new HashSet<>();
+    config.add("B");
+
+    PhosphorConstraintCalculator phosphorConstraintCalculator = new PhosphorConstraintCalculator(
+        OPTIONS);
+    Set<ConfigConstraint> constraints = phosphorConstraintCalculator
+        .deriveConstraints(results, config);
+
+    Assert.assertEquals(4, constraints.size());
+    Assert.assertTrue(constraints.contains(getABCConstraint()));
+    Assert.assertTrue(constraints.contains(getABNotCConstraint()));
+    Assert.assertTrue(constraints.contains(getNotABCConstraint()));
+    Assert.assertTrue(constraints.contains(getNotABNotCConstraint()));
+  }
+
+  @Test
+  public void deriveConstraints_forMultiConditionMultiContext() {
+    Mockito.when(conditionTaints.getTags()).thenReturn(new int[]{3});
+    Mockito.when(execCtxTaints.getTags()).thenReturn(new int[]{2});
+
+    Set<DecisionTaints> results = new HashSet<>();
+    results.add(decisionTaints);
+
+    Set<String> config = new HashSet<>();
+    config.add("B");
+
+    PhosphorConstraintCalculator phosphorConstraintCalculator = new PhosphorConstraintCalculator(
+        OPTIONS);
+    Set<ConfigConstraint> constraints = phosphorConstraintCalculator
+        .deriveConstraints(results, config);
+
+    Assert.assertEquals(2, constraints.size());
+    Assert.assertTrue(constraints.contains(getABConstraint()));
+    Assert.assertTrue(constraints.contains(getNotABConstraint()));
+  }
+
+  private static ConfigConstraint getNotAConstraint() {
+    Set<String> config = new HashSet<>();
+
+    Set<String> options = new HashSet<>();
+    options.add("A");
+
+    return ConfigConstraint.fromConfig(config, options);
+  }
+
+  private static ConfigConstraint getAConstraint() {
+    Set<String> config = new HashSet<>();
+    config.add("A");
+
+    Set<String> options = new HashSet<>();
+    options.add("A");
+
+    return ConfigConstraint.fromConfig(config, options);
+  }
+
+  private static ConfigConstraint getACConstraint() {
+    Set<String> config = new HashSet<>();
+    config.add("A");
+    config.add("C");
+
+    Set<String> options = new HashSet<>();
+    options.add("A");
+    options.add("C");
+
+    return ConfigConstraint.fromConfig(config, options);
+  }
+
+  private static ConfigConstraint getNotACConstraint() {
+    Set<String> config = new HashSet<>();
+    config.add("C");
+
+    Set<String> options = new HashSet<>();
+    options.add("A");
+    options.add("C");
+
+    return ConfigConstraint.fromConfig(config, options);
+  }
+
+  private static ConfigConstraint getANotCConstraint() {
+    Set<String> config = new HashSet<>();
+    config.add("A");
+
+    Set<String> options = new HashSet<>();
+    options.add("A");
+    options.add("C");
+
+    return ConfigConstraint.fromConfig(config, options);
+  }
+
+  private static ConfigConstraint getNotANotCConstraint() {
+    Set<String> config = new HashSet<>();
+
+    Set<String> options = new HashSet<>();
+    options.add("A");
+    options.add("C");
+
+    return ConfigConstraint.fromConfig(config, options);
+  }
+
+  private static ConfigConstraint getABCConstraint() {
+    Set<String> config = new HashSet<>();
+    config.add("A");
+    config.add("B");
+    config.add("C");
+
+    Set<String> options = new HashSet<>();
+    options.add("A");
+    options.add("B");
+    options.add("C");
+
+    return ConfigConstraint.fromConfig(config, options);
+  }
+
+  private static ConfigConstraint getNotABCConstraint() {
+    Set<String> config = new HashSet<>();
+    config.add("B");
+    config.add("C");
+
+    Set<String> options = new HashSet<>();
+    options.add("A");
+    options.add("B");
+    options.add("C");
+
+    return ConfigConstraint.fromConfig(config, options);
+  }
+
+  private static ConfigConstraint getABNotCConstraint() {
+    Set<String> config = new HashSet<>();
+    config.add("A");
+    config.add("B");
+
+    Set<String> options = new HashSet<>();
+    options.add("A");
+    options.add("B");
+    options.add("C");
+
+    return ConfigConstraint.fromConfig(config, options);
+  }
+
+  private static ConfigConstraint getNotABNotCConstraint() {
+    Set<String> config = new HashSet<>();
+    config.add("B");
+
+    Set<String> options = new HashSet<>();
+    options.add("A");
+    options.add("B");
+    options.add("C");
+
+    return ConfigConstraint.fromConfig(config, options);
+  }
+
+  private static ConfigConstraint getABConstraint() {
+    Set<String> config = new HashSet<>();
+    config.add("A");
+    config.add("B");
+
+    Set<String> options = new HashSet<>();
+    options.add("A");
+    options.add("B");
+
+    return ConfigConstraint.fromConfig(config, options);
+  }
+
+  private static ConfigConstraint getNotABConstraint() {
+    Set<String> config = new HashSet<>();
+    config.add("B");
+
+    Set<String> options = new HashSet<>();
+    options.add("A");
+    options.add("B");
+
+    return ConfigConstraint.fromConfig(config, options);
+  }
+}

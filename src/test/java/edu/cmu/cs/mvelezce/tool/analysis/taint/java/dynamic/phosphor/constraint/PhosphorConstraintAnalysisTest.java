785f26a68587014f2c948b72b7ecc7b511d5fd6c
Miguel Velez <miguelvelez@mijecu25.com>
2019-04-08 08:46:55 -0400

    got the constraints for the subtraces example

--- src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/dynamic/phosphor/constraint/PhosphorConstraintAnalysisTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/dynamic/phosphor/constraint/PhosphorConstraintAnalysisTest.java
@@ -1,6 +1,7 @@
 package edu.cmu.cs.mvelezce.tool.analysis.taint.java.dynamic.phosphor.constraint;
 
 import edu.cmu.cs.mvelezce.tool.analysis.taint.java.dynamic.phosphor.ConfigConstraint;
+import edu.cmu.cs.mvelezce.tool.execute.java.adapter.subtraces.SubtracesAdapter;
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.trivial.TrivialAdapter;
 import java.io.IOException;
 import java.util.HashSet;
@@ -15,6 +16,28 @@ public class PhosphorConstraintAnalysisTest {
   public void Trivial() throws IOException, InterruptedException {
     String programName = TrivialAdapter.PROGRAM_NAME;
     List<String> options = TrivialAdapter.getListOfOptions();
+    Set<String> initialConfig = new HashSet<>();
+
+    // Program arguments
+    String[] args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+
+    PhosphorConstraintAnalysis analysis = new PhosphorConstraintAnalysis(programName, options,
+        initialConfig);
+    Set<ConfigConstraint> write = analysis.analyze(args);
+
+    args = new String[0];
+    analysis = new PhosphorConstraintAnalysis(programName);
+    Set<ConfigConstraint> read = analysis.analyze(args);
+
+    Assert.assertEquals(write, read);
+  }
+
+  @Test
+  public void Subtraces() throws IOException, InterruptedException {
+    String programName = SubtracesAdapter.PROGRAM_NAME;
+    List<String> options = SubtracesAdapter.getListOfOptions();
     Set<String> initialConfig = new HashSet<>();
 
     // Program arguments

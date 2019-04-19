8a2f205be7c26411f2550ab59d13c7322444469f
Miguel Velez <miguelvelez@mijecu25.com>
2019-04-19 16:27:53 -0400

    used a different version to deserialize the taint info

--- src/main/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/dynamic/phosphor/constraint/PhosphorConstraintExecutionAnalysis.java
+++ src/main/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/dynamic/phosphor/constraint/PhosphorConstraintExecutionAnalysis.java
@@ -2,15 +2,19 @@ package edu.cmu.cs.mvelezce.tool.analysis.taint.java.dynamic.phosphor.constraint
 
 import edu.cmu.cs.mvelezce.cc.DecisionTaints;
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.BaseAdapter;
+import edu.columbia.cs.psl.phosphor.runtime.Taint;
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import javax.annotation.Nullable;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.ArrayUtils;
 
 public class PhosphorConstraintExecutionAnalysis {
 
@@ -48,18 +52,45 @@ public class PhosphorConstraintExecutionAnalysis {
   }
 
   private List<DecisionTaints> deserialize(File file) throws IOException {
-    FileInputStream fis = new FileInputStream(file);
-    ObjectInputStream ois = new ObjectInputStream(fis);
+    List<DecisionTaints> results = new ArrayList<>();
 
-    try {
-      return (List<DecisionTaints>) ois.readObject();
+    BufferedReader reader = new BufferedReader(new FileReader(file));
+    String line;
+    while ((line = reader.readLine()) != null) {
+      Taint contextTaints = this.getTaints(reader.readLine());
+      Taint conditionTaints = this.getTaints(reader.readLine());
+
+      DecisionTaints decisionTaints = new DecisionTaints(line, contextTaints, conditionTaints);
+      results.add(decisionTaints);
     }
-    catch (ClassNotFoundException cnfe) {
-      throw new RuntimeException(cnfe);
-    } finally {
-      ois.close();
-      fis.close();
+
+    reader.close();
+
+    return results;
+  }
+
+  @Nullable
+  private Taint getTaints(String data) {
+    if (data.isEmpty()) {
+      return null;
     }
+
+    int[] tags = this.getTags(data);
+
+    return Taint.getCCResultTaint(tags);
+  }
+
+  private int[] getTags(String data) {
+//    if (data.length() != 1) {
+//      throw new RuntimeException("The entry should have only one character");
+//    }
+//
+    List<Integer> tags = new ArrayList<>();
+//    char characer = data.charAt(0);
+//    int tag = (int) characer;
+    tags.add(Integer.valueOf(data));
+
+    return ArrayUtils.toPrimitive(tags.toArray(new Integer[0]));
   }
 
 }

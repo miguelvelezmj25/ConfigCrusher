2027c5c674e1843569f1f2bf7beae015c1e21795
Miguel Velez <miguelvelez@mijecu25.com>
2019-04-22 08:43:23 -0400

    added test to run gc test to read large file

---
+++ src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/dynamic/phosphor/constraint/TestingGCTest.java
@@ -0,0 +1,12 @@
+package edu.cmu.cs.mvelezce.tool.analysis.taint.java.dynamic.phosphor.constraint;
+
+import java.io.IOException;
+import org.junit.Test;
+
+public class TestingGCTest {
+
+  @Test
+  public void main() throws IOException {
+    TestingGC.main(null);
+  }
+}

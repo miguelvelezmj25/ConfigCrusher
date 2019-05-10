c5b6b8d049ed8a083197fd5db6f843fa0cd1cfa9
Miguel Velez <miguelvelez@mijecu25.com>
2019-05-10 08:58:48 -0400

    deleted subtraces deprecated files

--- src/main/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/groundtruth/SubtracesPipeline.java
+++ src/main/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/groundtruth/SubtracesPipeline.java
@@ -19,34 +19,24 @@ public class SubtracesPipeline {
   public Set<Set<String>> getMinConfigsToExecute(String[] args)
       throws IOException, InterruptedException {
     Map<Set<String>, List<String>> configsToTraces = this.executeSubtraceInstrumentedProgram(args);
-    List<String> alignedTrace = this.alignTraces(args, configsToTraces);
 
     throw new UnsupportedOperationException("Implement");
-//    return this.getMinConfigsToExecute(args, subtraceAnalysisInfos);
-  }
-
-  private List<String> alignTraces(String[] args, Map<Set<String>, List<String>> configsToTraces)
-      throws IOException {
-    TracesAligner tracesAligner = new TracesAligner(this.programName, configsToTraces);
-
-    return tracesAligner.analyze(args);
+    //    return this.getMinConfigsToExecute(args, subtraceAnalysisInfos);
   }
 
   private Map<Set<String>, List<String>> executeSubtraceInstrumentedProgram(String[] args)
       throws IOException, InterruptedException {
-    SubtracesAnalysisExecutor executor = new SubtracesAnalysisExecutor(this.programName,
-        this.options);
+    SubtracesAnalysisExecutor executor =
+        new SubtracesAnalysisExecutor(this.programName, this.options);
 
     return executor.analyze(args);
   }
 
-  private Set<Set<String>> getMinConfigsToExecute(String[] args,
-      Set<SubtraceAnalysisInfo> subtraceAnalysisInfos)
-      throws IOException {
-    SatConfigAnalyzer satConfigAnalyzer = new SatConfigAnalyzer(this.programName,
-        subtraceAnalysisInfos, this.options);
+  private Set<Set<String>> getMinConfigsToExecute(
+      String[] args, Set<SubtraceAnalysisInfo> subtraceAnalysisInfos) throws IOException {
+    SatConfigAnalyzer satConfigAnalyzer =
+        new SatConfigAnalyzer(this.programName, subtraceAnalysisInfos, this.options);
 
     return satConfigAnalyzer.analyze(args);
   }
-
 }

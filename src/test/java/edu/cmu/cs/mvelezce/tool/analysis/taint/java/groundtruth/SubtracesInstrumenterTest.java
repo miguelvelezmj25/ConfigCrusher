5e16a9810da5342b39e20cf2e5fdbf03f6ef65e5
Miguel Velez <miguelvelez@mijecu25.com>
2019-09-06 12:51:42 -0400

    instrumented trivial for subtrace analysis

--- src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/groundtruth/SubtracesInstrumenterTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/groundtruth/SubtracesInstrumenterTest.java
@@ -1,41 +1,35 @@
 package edu.cmu.cs.mvelezce.tool.analysis.taint.java.groundtruth;
 
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.measureDiskOrderedScan.MeasureDiskOrderedScanAdapter;
+import edu.cmu.cs.mvelezce.tool.execute.java.adapter.trivial.TrivialAdapter;
 import edu.cmu.cs.mvelezce.tool.instrumentation.java.Instrumenter;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import org.junit.Test;
 
+// TODO MIGUEL maybe have a separate dir for projects instrumented for this evaluation?
 public class SubtracesInstrumenterTest {
 
   @Test
-  public void compileTraces() throws IOException, InterruptedException {
-    String programName = "Traces";
-    String srcDir = "../performance-mapper-evaluation/instrumented/phosphor-examples";
-    String classDir = "../performance-mapper-evaluation/instrumented/phosphor-examples/target/classes";
-
-    Instrumenter instrumenter = new SubtracesInstrumenter(programName, srcDir, classDir);
-    instrumenter.compile();
-  }
-
-  @Test
   public void instrumentTraces()
-      throws IOException, InterruptedException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+      throws IOException, InterruptedException, NoSuchMethodException, IllegalAccessException,
+          InvocationTargetException {
     String programName = "phosphorExamples";
-    String srcDir = "../performance-mapper-evaluation/instrumented/phosphor-examples";
-    String classDir = "../performance-mapper-evaluation/instrumented/phosphor-examples/target/classes";
+    String srcDir = TrivialAdapter.INSTRUMENTED_DIR_PATH;
+    String classDir = TrivialAdapter.INSTRUMENTED_CLASS_PATH;
+    Instrumenter instrumenter = new SubtracesInstrumenter(programName, srcDir, classDir);
 
     String[] args = new String[2];
     args[0] = "-delres";
     args[1] = "-saveres";
 
-    Instrumenter instrumenter = new SubtracesInstrumenter(programName, srcDir, classDir);
     instrumenter.instrument(args);
   }
 
   @Test
   public void instrumentPrevayler()
-      throws IOException, InterruptedException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+      throws IOException, InterruptedException, NoSuchMethodException, IllegalAccessException,
+          InvocationTargetException {
     String programName = "prevayler";
     String srcDir = "../performance-mapper-evaluation/instrumented/prevayler";
     String classDir = "../performance-mapper-evaluation/instrumented/prevayler/target/classes";
@@ -50,7 +44,8 @@ public class SubtracesInstrumenterTest {
 
   @Test
   public void instrumentMeasureDiskOrderedScan()
-      throws IOException, InterruptedException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+      throws IOException, InterruptedException, NoSuchMethodException, IllegalAccessException,
+          InvocationTargetException {
     String programName = MeasureDiskOrderedScanAdapter.PROGRAM_NAME;
     String srcDir = MeasureDiskOrderedScanAdapter.INSTRUMENTED_DIR_PATH;
     String classDir = MeasureDiskOrderedScanAdapter.INSTRUMENTED_CLASS_PATH;

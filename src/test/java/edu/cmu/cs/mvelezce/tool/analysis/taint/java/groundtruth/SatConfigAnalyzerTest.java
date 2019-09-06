5be15589523321b492fc279caff9a6c7167e81e2
Miguel Velez <miguelvelez@mijecu25.com>
2019-09-06 13:13:06 -0400

    found the min set of configs to execute to explore all subtraces in trivial

--- src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/groundtruth/SatConfigAnalyzerTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/groundtruth/SatConfigAnalyzerTest.java
@@ -5,6 +5,7 @@ import edu.cmu.cs.mvelezce.tool.execute.java.adapter.implicit2.Implicit2Adapter;
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.measureDiskOrderedScan.MeasureDiskOrderedScanAdapter;
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.subtraces.SubtracesAdapter;
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.subtraces2.Subtraces2Adapter;
+import edu.cmu.cs.mvelezce.tool.execute.java.adapter.trivial.TrivialAdapter;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
@@ -133,6 +134,29 @@ public class SatConfigAnalyzerTest {
     args = new String[0];
 
     analysis = new SatConfigAnalyzer(programName);
+    Set<Set<String>> read = analysis.analyze(args);
+
+    Assert.assertEquals(write, read);
+  }
+
+  @Test
+  public void trivial() throws IOException {
+    String programName = TrivialAdapter.PROGRAM_NAME;
+    SubtracesValueAnalysis subtracesValueAnalysis = new SubtracesValueAnalysis(programName);
+
+    String[] args = new String[0];
+    Set<SubtraceAnalysisInfo> subtraceAnalysisInfos = subtracesValueAnalysis.analyze(args);
+
+    Set<String> options = new HashSet<>(TrivialAdapter.getListOfOptions());
+    SatConfigAnalyzer analysis = new SatConfigAnalyzer(programName, subtraceAnalysisInfos, options);
+
+    args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    Set<Set<String>> write = analysis.analyze(args);
+
+    analysis = new SatConfigAnalyzer(programName);
+    args = new String[0];
     Set<Set<String>> read = analysis.analyze(args);
 
     Assert.assertEquals(write, read);

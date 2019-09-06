e49553606b00b8a88e6a4b2d99801244090a067d
Miguel Velez <miguelvelez@mijecu25.com>
2019-09-06 13:04:08 -0400

    created labels from traces

--- src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/groundtruth/SubtraceLabelerTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/analysis/taint/java/groundtruth/SubtraceLabelerTest.java
@@ -4,6 +4,7 @@ import edu.cmu.cs.mvelezce.tool.execute.java.adapter.measureDiskOrderedScan.Meas
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.return2Example.Return2ExampleAdapter;
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.simpleForExample.SimpleForExampleAdapter;
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.subtraces.SubtracesAdapter;
+import edu.cmu.cs.mvelezce.tool.execute.java.adapter.trivial.TrivialAdapter;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -100,6 +101,27 @@ public class SubtraceLabelerTest {
     args = new String[0];
 
     subtraceLabeler = new SubtraceLabeler(programName);
+    Map<Set<String>, List<String>> read = subtraceLabeler.analyze(args);
+
+    Assert.assertEquals(write, read);
+  }
+
+  @Test
+  public void trivial() throws IOException, InterruptedException {
+    String programName = TrivialAdapter.PROGRAM_NAME;
+    SubtracesAnalysisExecutor analysis = new SubtracesAnalysisExecutor(programName);
+
+    String[] args = new String[0];
+    Map<Set<String>, List<String>> configsToTraces = analysis.analyze(args);
+
+    SubtraceLabeler subtraceLabeler = new SubtraceLabeler(programName, configsToTraces);
+    args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    Map<Set<String>, List<String>> write = subtraceLabeler.analyze(args);
+
+    subtraceLabeler = new SubtraceLabeler(programName);
+    args = new String[0];
     Map<Set<String>, List<String>> read = subtraceLabeler.analyze(args);
 
     Assert.assertEquals(write, read);

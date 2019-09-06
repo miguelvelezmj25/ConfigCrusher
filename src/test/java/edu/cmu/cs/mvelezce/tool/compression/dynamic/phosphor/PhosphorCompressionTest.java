f52fceb5145aa3603402bf0ab3453c6f5c36a1b4
Miguel Velez <miguelvelez@mijecu25.com>
2019-09-06 13:24:01 -0400

    found min set of configs to explore with dta

--- src/test/java/edu/cmu/cs/mvelezce/tool/compression/dynamic/phosphor/PhosphorCompressionTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/tool/compression/dynamic/phosphor/PhosphorCompressionTest.java
@@ -16,23 +16,21 @@ public class PhosphorCompressionTest {
   @Test
   public void Trivial() throws IOException, InterruptedException {
     String programName = TrivialAdapter.PROGRAM_NAME;
+    PhosphorConstraintAnalysis constraintAnalysis = new PhosphorConstraintAnalysis(programName);
 
     String[] args = new String[0];
-
-    PhosphorConstraintAnalysis constraintAnalysis = new PhosphorConstraintAnalysis(programName);
     Set<ConfigConstraint> constraints = constraintAnalysis.analyze(args);
+
+    Set<String> options = new HashSet<>(TrivialAdapter.getListOfOptions());
+    PhosphorCompression phosphorCompression =
+        new PhosphorCompression(programName, options, constraints);
 
     args = new String[2];
     args[0] = "-delres";
     args[1] = "-saveres";
-
-    Set<String> options = new HashSet<>(TrivialAdapter.getListOfOptions());
-    PhosphorCompression phosphorCompression = new PhosphorCompression(programName, options,
-        constraints);
     Set<Set<String>> write = phosphorCompression.compressConfigurations(args);
 
     args = new String[0];
-
     phosphorCompression = new PhosphorCompression(programName);
     Set<Set<String>> read = phosphorCompression.compressConfigurations(args);
 

6e2f6970c8364fcfa0a838d885b89f5e8b2c136d
Miguel Velez <miguelvelez@mijecu25.com>
2019-09-25 11:22:39 -0400

    found where we are missing constraints in lucene

--- src/test/java/edu/cmu/cs/mvelezce/evaluation/dta/constraints/subtraces/ControlFlowStatementWithMissingConstraintAnalyzerTest.java
+++ src/test/java/edu/cmu/cs/mvelezce/evaluation/dta/constraints/subtraces/ControlFlowStatementWithMissingConstraintAnalyzerTest.java
@@ -1,5 +1,6 @@
 package edu.cmu.cs.mvelezce.evaluation.dta.constraints.subtraces;
 
+import edu.cmu.cs.mvelezce.tool.execute.java.adapter.indexFiles.IndexFilesAdapter;
 import edu.cmu.cs.mvelezce.tool.execute.java.adapter.measureDiskOrderedScan.MeasureDiskOrderedScanAdapter;
 import org.junit.Assert;
 import org.junit.Test;
@@ -69,6 +70,174 @@ public class ControlFlowStatementWithMissingConstraintAnalyzerTest {
   public void berkeleyDB_1() throws Exception {
     String programName = MeasureDiskOrderedScanAdapter.PROGRAM_NAME;
     String missingConstraint = "(!DUPLICATES && SEQUENTIAL)";
+    Set<SubtraceOutcomeConstraint> subtracesOutcomeConstraint =
+        getSubtracesOutcomeConstraint(programName);
+    ControlFlowStatementWithMissingConstraintAnalyzer analysis =
+        new ControlFlowStatementWithMissingConstraintAnalyzer(
+            programName, missingConstraint, subtracesOutcomeConstraint);
+
+    String[] args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    ControlFlowStatementsWithMissingConstraint write = analysis.analyze(args);
+
+    analysis =
+        new ControlFlowStatementWithMissingConstraintAnalyzer(programName, missingConstraint);
+    args = new String[0];
+    ControlFlowStatementsWithMissingConstraint read = analysis.analyze(args);
+
+    Assert.assertEquals(write, read);
+  }
+
+  @Test
+  public void lucene_1() throws Exception {
+    String programName = IndexFilesAdapter.PROGRAM_NAME;
+    String missingConstraint =
+        "((READER_POOLING && ((COMMIT_ON_CLOSE && !CHECK_PENDING_FLUSH_UPDATE && MERGE_SCHEDULER) || (COMMIT_ON_CLOSE && CHECK_PENDING_FLUSH_UPDATE && !MERGE_SCHEDULER))) || (MERGE_SCHEDULER && CHECK_PENDING_FLUSH_UPDATE && !READER_POOLING))";
+    Set<SubtraceOutcomeConstraint> subtracesOutcomeConstraint =
+        getSubtracesOutcomeConstraint(programName);
+    ControlFlowStatementWithMissingConstraintAnalyzer analysis =
+        new ControlFlowStatementWithMissingConstraintAnalyzer(
+            programName, missingConstraint, subtracesOutcomeConstraint);
+
+    String[] args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    ControlFlowStatementsWithMissingConstraint write = analysis.analyze(args);
+
+    analysis =
+        new ControlFlowStatementWithMissingConstraintAnalyzer(programName, missingConstraint);
+    args = new String[0];
+    ControlFlowStatementsWithMissingConstraint read = analysis.analyze(args);
+
+    Assert.assertEquals(write, read);
+  }
+
+  @Test
+  public void lucene_2() throws Exception {
+    String programName = IndexFilesAdapter.PROGRAM_NAME;
+    String missingConstraint =
+        "((READER_POOLING && ((COMMIT_ON_CLOSE && CHECK_PENDING_FLUSH_UPDATE && MERGE_SCHEDULER) || (COMMIT_ON_CLOSE && !CHECK_PENDING_FLUSH_UPDATE && !MERGE_SCHEDULER))) || (!READER_POOLING && COMMIT_ON_CLOSE))";
+    Set<SubtraceOutcomeConstraint> subtracesOutcomeConstraint =
+        getSubtracesOutcomeConstraint(programName);
+    ControlFlowStatementWithMissingConstraintAnalyzer analysis =
+        new ControlFlowStatementWithMissingConstraintAnalyzer(
+            programName, missingConstraint, subtracesOutcomeConstraint);
+
+    String[] args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    ControlFlowStatementsWithMissingConstraint write = analysis.analyze(args);
+
+    analysis =
+        new ControlFlowStatementWithMissingConstraintAnalyzer(programName, missingConstraint);
+    args = new String[0];
+    ControlFlowStatementsWithMissingConstraint read = analysis.analyze(args);
+
+    Assert.assertEquals(write, read);
+  }
+
+  @Test
+  public void lucene_3() throws Exception {
+    String programName = IndexFilesAdapter.PROGRAM_NAME;
+    String missingConstraint =
+        "((COMMIT_ON_CLOSE && !CHECK_PENDING_FLUSH_UPDATE && MERGE_SCHEDULER && READER_POOLING) || (COMMIT_ON_CLOSE && !CHECK_PENDING_FLUSH_UPDATE && !READER_POOLING))";
+    Set<SubtraceOutcomeConstraint> subtracesOutcomeConstraint =
+        getSubtracesOutcomeConstraint(programName);
+    ControlFlowStatementWithMissingConstraintAnalyzer analysis =
+        new ControlFlowStatementWithMissingConstraintAnalyzer(
+            programName, missingConstraint, subtracesOutcomeConstraint);
+
+    String[] args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    ControlFlowStatementsWithMissingConstraint write = analysis.analyze(args);
+
+    analysis =
+        new ControlFlowStatementWithMissingConstraintAnalyzer(programName, missingConstraint);
+    args = new String[0];
+    ControlFlowStatementsWithMissingConstraint read = analysis.analyze(args);
+
+    Assert.assertEquals(write, read);
+  }
+
+  @Test
+  public void lucene_4() throws Exception {
+    String programName = IndexFilesAdapter.PROGRAM_NAME;
+    String missingConstraint =
+        "((READER_POOLING && ((COMMIT_ON_CLOSE && CHECK_PENDING_FLUSH_UPDATE && MERGE_SCHEDULER) || (!MERGE_SCHEDULER && COMMIT_ON_CLOSE))) || (COMMIT_ON_CLOSE && CHECK_PENDING_FLUSH_UPDATE && !READER_POOLING))";
+    Set<SubtraceOutcomeConstraint> subtracesOutcomeConstraint =
+        getSubtracesOutcomeConstraint(programName);
+    ControlFlowStatementWithMissingConstraintAnalyzer analysis =
+        new ControlFlowStatementWithMissingConstraintAnalyzer(
+            programName, missingConstraint, subtracesOutcomeConstraint);
+
+    String[] args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    ControlFlowStatementsWithMissingConstraint write = analysis.analyze(args);
+
+    analysis =
+        new ControlFlowStatementWithMissingConstraintAnalyzer(programName, missingConstraint);
+    args = new String[0];
+    ControlFlowStatementsWithMissingConstraint read = analysis.analyze(args);
+
+    Assert.assertEquals(write, read);
+  }
+
+  @Test
+  public void lucene_5() throws Exception {
+    String programName = IndexFilesAdapter.PROGRAM_NAME;
+    String missingConstraint =
+        "((COMMIT_ON_CLOSE && !CHECK_PENDING_FLUSH_UPDATE && !MERGE_SCHEDULER && READER_POOLING) || (!READER_POOLING && ((!COMMIT_ON_CLOSE && !CHECK_PENDING_FLUSH_UPDATE && MERGE_SCHEDULER) || (!MERGE_SCHEDULER && (!COMMIT_ON_CLOSE || CHECK_PENDING_FLUSH_UPDATE)))))";
+    Set<SubtraceOutcomeConstraint> subtracesOutcomeConstraint =
+        getSubtracesOutcomeConstraint(programName);
+    ControlFlowStatementWithMissingConstraintAnalyzer analysis =
+        new ControlFlowStatementWithMissingConstraintAnalyzer(
+            programName, missingConstraint, subtracesOutcomeConstraint);
+
+    String[] args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    ControlFlowStatementsWithMissingConstraint write = analysis.analyze(args);
+
+    analysis =
+        new ControlFlowStatementWithMissingConstraintAnalyzer(programName, missingConstraint);
+    args = new String[0];
+    ControlFlowStatementsWithMissingConstraint read = analysis.analyze(args);
+
+    Assert.assertEquals(write, read);
+  }
+
+  @Test
+  public void lucene_6() throws Exception {
+    String programName = IndexFilesAdapter.PROGRAM_NAME;
+    String missingConstraint =
+        "((COMMIT_ON_CLOSE && !CHECK_PENDING_FLUSH_UPDATE && !MERGE_SCHEDULER && READER_POOLING) || (!COMMIT_ON_CLOSE && !CHECK_PENDING_FLUSH_UPDATE && !READER_POOLING))";
+    Set<SubtraceOutcomeConstraint> subtracesOutcomeConstraint =
+        getSubtracesOutcomeConstraint(programName);
+    ControlFlowStatementWithMissingConstraintAnalyzer analysis =
+        new ControlFlowStatementWithMissingConstraintAnalyzer(
+            programName, missingConstraint, subtracesOutcomeConstraint);
+
+    String[] args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    ControlFlowStatementsWithMissingConstraint write = analysis.analyze(args);
+
+    analysis =
+        new ControlFlowStatementWithMissingConstraintAnalyzer(programName, missingConstraint);
+    args = new String[0];
+    ControlFlowStatementsWithMissingConstraint read = analysis.analyze(args);
+
+    Assert.assertEquals(write, read);
+  }
+
+  @Test
+  public void lucene_7() throws Exception {
+    String programName = IndexFilesAdapter.PROGRAM_NAME;
+    String missingConstraint =
+        "(READER_POOLING && ((COMMIT_ON_CLOSE && !CHECK_PENDING_FLUSH_UPDATE && MERGE_SCHEDULER) || (COMMIT_ON_CLOSE && CHECK_PENDING_FLUSH_UPDATE && !MERGE_SCHEDULER)))";
     Set<SubtraceOutcomeConstraint> subtracesOutcomeConstraint =
         getSubtracesOutcomeConstraint(programName);
     ControlFlowStatementWithMissingConstraintAnalyzer analysis =

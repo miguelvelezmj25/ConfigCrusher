2d91784ac6c1aeb3ad0ac6bc09a613c39bec69f8
Miguel Velez <miguelvelez@mijecu25.com>
2019-09-30 14:37:48 -0400

    added module for util classes

---
+++ cc-utils/src/main/java/edu/cmu/cs/mvelezce/utils/ConfigHelper.java
@@ -0,0 +1,143 @@
+package edu.cmu.cs.mvelezce.utils;
+
+import org.apache.commons.math3.util.Combinations;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * Helping class for performing analyses.
+ *
+ * @author Miguel Velez - miguelvelezmj25
+ * @version 0.1.0.1
+ */
+public class ConfigHelper {
+
+  /** Get all combinations of the specified options. */
+  public static Set<Set<String>> getConfigurations(Collection<String> options) {
+    if (options == null) {
+      throw new IllegalArgumentException("The options passed cannot be null");
+    }
+
+    Set<Set<String>> configs = new HashSet<>(ConfigHelper.getCombinations(options));
+    configs.add(new HashSet<>());
+
+    return configs;
+  }
+
+  public static Set<Set<String>> getCombinations(Collection<String> options) {
+    if (options == null) {
+      throw new IllegalArgumentException("The options passed cannot be null");
+    }
+
+    List<String> optionsList = new ArrayList<>(options);
+    Set<Set<String>> combos = new HashSet<>();
+    int comboMaxLength = options.size();
+
+    for (int i = 1; i <= comboMaxLength; i++) {
+      Combinations currentCombos = new Combinations(comboMaxLength, i);
+
+      for (int[] currentCombo : currentCombos) {
+        Set<String> combo = new HashSet<>();
+
+        for (int element : currentCombo) {
+          combo.add(optionsList.get(element));
+        }
+
+        combos.add(combo);
+      }
+    }
+
+    return combos;
+  }
+
+  //    public static void removeSampledConfigurations(String name, Set<Set<String>> configurations)
+  // throws IOException, InterruptedException {
+  //        // arguments
+  //        String[] args = new String[0];
+  //
+  //        Executor executor = new BruteForceExecutor(name);
+  //        Set<PerformanceEntryStatistic> performanceEntries = executor.execute(args);
+  //
+  //        for(PerformanceEntryStatistic entry : performanceEntries) {
+  //            configurations.remove(entry.getConfiguration());
+  //        }
+  //    }
+
+  public static Set<Set<String>> getRandomConfigs(
+      List<String> options, int size, Set<Set<String>> excludeConfigs) {
+    Set<Set<String>> configs = new HashSet<>(size);
+    int length = options.size();
+
+    Random numOfOpts = new Random();
+
+    while (configs.size() < size) {
+      int num = numOfOpts.nextInt(length + 1);
+      Set<String> config = new HashSet<>(num);
+      Random opts = new Random();
+
+      while (config.size() < num) {
+        int index = opts.nextInt(length);
+        String opt = options.get(index);
+        config.add(opt);
+      }
+
+      if (!configs.contains(config) && !excludeConfigs.contains(config)) {
+        configs.add(config);
+      }
+    }
+
+    return configs;
+  }
+
+  public static Set<Set<String>> mergeConfigs(
+      Set<Set<String>> configs1, Set<Set<String>> configs2) {
+    Set<Set<String>> configs = new HashSet<>(configs1);
+    configs.addAll(configs2);
+
+    return configs;
+  }
+
+  public static void processOutput(Process process) throws IOException {
+    System.out.println("Output: ");
+    BufferedReader inputReader =
+        new BufferedReader(new InputStreamReader(process.getInputStream()));
+    String string;
+
+    while ((string = inputReader.readLine()) != null) {
+      if (!string.isEmpty()) {
+        System.out.println(string);
+      }
+    }
+
+    System.out.println();
+  }
+
+  public static void processError(Process process) throws IOException {
+    StringBuilder stringBuilder = new StringBuilder();
+    System.out.println("Errors: ");
+    BufferedReader errorReader =
+        new BufferedReader(new InputStreamReader(process.getErrorStream()));
+    String string;
+
+    while ((string = errorReader.readLine()) != null) {
+      if (!string.isEmpty()) {
+        stringBuilder.append(string);
+        System.out.println(string);
+      }
+    }
+
+    if (stringBuilder.length() != 0) {
+      String potentialErrors = stringBuilder.toString();
+      potentialErrors = potentialErrors.toLowerCase();
+
+      if (potentialErrors.contains("error") || potentialErrors.contains("exception")) {
+        throw new RuntimeException("There was an error in the application code");
+      }
+    }
+
+    System.out.println();
+  }
+}

1ba0c3709f79bc8edf8a25235499fd26618cc3e2
Miguel Velez <miguelvelez@mijecu25.com>
2019-10-09 08:13:01 -0400

    updated adapter

--- cc-adapters/src/main/java/edu/cmu/cs/mvelezce/adapter/adapters/cannotExpandConstraintsDown/BaseCannotExpandConstraintsDownAdapter.java
+++ cc-adapters/src/main/java/edu/cmu/cs/mvelezce/adapter/adapters/cannotExpandConstraintsDown/BaseCannotExpandConstraintsDownAdapter.java
@@ -1,4 +1,4 @@
-package edu.cmu.cs.mvelezce.adapter.adapters.expandDown;
+package edu.cmu.cs.mvelezce.adapter.adapters.cannotExpandConstraintsDown;
 
 import edu.cmu.cs.mvelezce.adapter.adapters.BaseAdapter;
 
@@ -7,10 +7,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
-public class BaseExpandDownAdapter extends BaseAdapter {
+public class BaseCannotExpandConstraintsDownAdapter extends BaseAdapter {
 
-  public static final String PROGRAM_NAME = "expandDown";
-  public static final String MAIN_CLASS = "edu.cmu.cs.mvelezce.analysis.ExpandDown";
+  public static final String PROGRAM_NAME = "cannotExpandConstraintsDown";
+  public static final String MAIN_CLASS =
+      "edu.cmu.cs.mvelezce.analysis.CannotExpandConstraintsDown";
   public static final String ROOT_PACKAGE = "edu.cmu.cs.mvelezce.analysis";
   public static final String ORIGINAL_CLASS_PATH =
       "../performance-mapper-evaluation/original/phosphor-examples/target/classes";
@@ -21,22 +22,27 @@ public class BaseExpandDownAdapter extends BaseAdapter {
 
   private static final String[] OPTIONS = {"A", "B"};
 
-  public BaseExpandDownAdapter() {
+  public BaseCannotExpandConstraintsDownAdapter() {
     // TODO check why are we passing empty string. Empty string represents the directory of the
     // class files to execute.
     super(
-        BaseExpandDownAdapter.PROGRAM_NAME,
-        BaseExpandDownAdapter.MAIN_CLASS,
+        BaseCannotExpandConstraintsDownAdapter.PROGRAM_NAME,
+        BaseCannotExpandConstraintsDownAdapter.MAIN_CLASS,
         "",
-        BaseExpandDownAdapter.getListOfOptions());
+        BaseCannotExpandConstraintsDownAdapter.getListOfOptions());
   }
 
-  public BaseExpandDownAdapter(String programName, String entryPoint, String classDir) {
-    super(programName, entryPoint, classDir, BaseExpandDownAdapter.getListOfOptions());
+  public BaseCannotExpandConstraintsDownAdapter(
+      String programName, String entryPoint, String classDir) {
+    super(
+        programName,
+        entryPoint,
+        classDir,
+        BaseCannotExpandConstraintsDownAdapter.getListOfOptions());
   }
 
   public static List<String> getListOfOptions() {
-    return Arrays.asList(BaseExpandDownAdapter.OPTIONS);
+    return Arrays.asList(BaseCannotExpandConstraintsDownAdapter.OPTIONS);
   }
 
   @Override

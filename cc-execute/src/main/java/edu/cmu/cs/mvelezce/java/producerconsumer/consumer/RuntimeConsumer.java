85490d7fe80438fd23875bd45a2ea29ad7610810
Miguel Velez <miguelvelez@mijecu25.com>
2019-11-04 16:20:13 -0500

    wrote execution times to a file

--- cc-execute/src/main/java/edu/cmu/cs/mvelezce/java/producerconsumer/consumer/RuntimeConsumer.java
+++ cc-execute/src/main/java/edu/cmu/cs/mvelezce/java/producerconsumer/consumer/RuntimeConsumer.java
@@ -3,19 +3,38 @@ package edu.cmu.cs.mvelezce.java.producerconsumer.consumer;
 import edu.cmu.cs.mvelezce.consumer.IConsumer;
 import edu.cmu.cs.mvelezce.setup.IProducerConsumer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.concurrent.BlockingQueue;
 
 public class RuntimeConsumer implements IConsumer<String> {
 
+  private static byte[] NEW_LINE_BYTES = "\n".getBytes();
+
   private final BlockingQueue<String> queue;
+  private final FileOutputStream fos;
 
   public RuntimeConsumer(BlockingQueue<String> queue) {
     this.queue = queue;
+
+    try {
+      File outputFile = new File("data.ser");
+      this.fos = new FileOutputStream(outputFile);
+    } catch (FileNotFoundException fnfe) {
+      throw new RuntimeException("Could not initialize the file output stream", fnfe);
+    }
   }
 
   @Override
   public void consume(String data) {
-    System.out.println(data);
+    try {
+      this.fos.write(data.getBytes());
+      this.fos.write(NEW_LINE_BYTES);
+    } catch (IOException ioe) {
+      throw new RuntimeException(ioe);
+    }
   }
 
   @Override
@@ -37,6 +56,13 @@ public class RuntimeConsumer implements IConsumer<String> {
       }
     } catch (InterruptedException ie) {
       throw new RuntimeException(ie);
+    }
+
+    try {
+      this.fos.flush();
+      this.fos.close();
+    } catch (IOException ioe) {
+      ioe.printStackTrace();
     }
   }
 }

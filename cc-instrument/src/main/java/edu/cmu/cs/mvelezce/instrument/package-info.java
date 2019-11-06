7f1b502be5aec7f652a8d638bf9ca904feebd8f0
Miguel Velez <miguelvelez@mijecu25.com>
2019-11-06 11:28:51 -0500

    added annotations

---
+++ cc-instrument/src/main/java/edu/cmu/cs/mvelezce/instrument/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@ReturnTypesAreNonNullByDefault
+package edu.cmu.cs.mvelezce.instrument;
+
+import edu.cmu.cs.mvelezce.utils.annotations.ReturnTypesAreNonNullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;

cc1118334750322a4e1870350da7c8d5325da4a3
Miguel Velez <miguelvelez@mijecu25.com>
2019-11-06 11:31:11 -0500

    added annotations

---
+++ cc-adapters/src/main/java/edu/cmu/cs/mvelezce/adapter/adapters/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@ReturnTypesAreNonNullByDefault
+package edu.cmu.cs.mvelezce.adapter.adapters;
+
+import edu.cmu.cs.mvelezce.utils.annotations.ReturnTypesAreNonNullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ cc-analysis/src/main/java/edu/cmu/cs/mvelezce/analysis/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@ReturnTypesAreNonNullByDefault
+package edu.cmu.cs.mvelezce.analysis;
+
+import edu.cmu.cs.mvelezce.utils.annotations.ReturnTypesAreNonNullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;

6c60e79c899d622171d5eb7b063267737c49d55e
Miguel Velez <miguelvelez@mijecu25.com>
2019-11-22 13:36:00 -0500

    removed implied constraints

--- cc-compress/src/main/java/edu/cmu/cs/mvelezce/compress/idta/IDTACompression.java
+++ cc-compress/src/main/java/edu/cmu/cs/mvelezce/compress/idta/IDTACompression.java
@@ -2,9 +2,9 @@ package edu.cmu.cs.mvelezce.compress.idta;
 
 import de.fosd.typechef.featureexpr.FeatureExpr;
 import edu.cmu.cs.mvelezce.compress.BaseCompression;
+import edu.cmu.cs.mvelezce.compress.idta.utils.simplify.ImpliedConstraintsRemover;
 import edu.cmu.cs.mvelezce.utils.config.Options;
 
-import java.io.IOException;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -23,8 +23,9 @@ public class IDTACompression extends BaseCompression {
   }
 
   @Override
-  public Set<Set<String>> analyze() throws IOException, InterruptedException {
+  public Set<Set<String>> analyze() {
     Set<FeatureExpr> constraints = this.expandAllConstraints();
+    ImpliedConstraintsRemover.removeImpliedConstraints(constraints);
 
     throw new UnsupportedOperationException("implement");
   }
---
+++ cc-compress/src/main/java/edu/cmu/cs/mvelezce/compress/idta/utils/simplify/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@ReturnTypesAreNonNullByDefault
+package edu.cmu.cs.mvelezce.compress.idta.utils.simplify;
+
+import com.mijecu25.meme.utils.annotations.ReturnTypesAreNonNullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;

173c805dda2c73b6338243da2ec61fefdc572bee
Miguel Velez <miguelvelez@mijecu25.com>
2019-12-03 08:26:26 -0500

    averaged the execution times of gt berkeley db

---
+++ cc-execute-blackbox/src/main/java/edu/cmu/cs/mvelezce/blackbox/processor/aggregator/gt/GroundTruthPerfAggregatorProcessor.java
@@ -0,0 +1,28 @@
+package edu.cmu.cs.mvelezce.blackbox.processor.aggregator.gt;
+
+import edu.cmu.cs.mvelezce.blackbox.execute.gt.GroundTruthExecutor;
+import edu.cmu.cs.mvelezce.java.processor.aggregator.PerfAggregatorProcessor;
+import edu.cmu.cs.mvelezce.java.results.processed.ProcessedPerfExecution;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class GroundTruthPerfAggregatorProcessor extends PerfAggregatorProcessor {
+
+  private static final String OUTPUT_DIR = GroundTruthExecutor.OUTPUT_DIR;
+
+  public GroundTruthPerfAggregatorProcessor(String programName) {
+    this(programName, new HashMap<>());
+  }
+
+  GroundTruthPerfAggregatorProcessor(
+      String programName, Map<Integer, Set<ProcessedPerfExecution>> itersToProcessedPerfExecution) {
+    super(programName, itersToProcessedPerfExecution);
+  }
+
+  @Override
+  public String outputDir() {
+    return OUTPUT_DIR;
+  }
+}

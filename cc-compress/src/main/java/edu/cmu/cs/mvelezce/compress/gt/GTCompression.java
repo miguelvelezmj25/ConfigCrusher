d827bc8ccd1ba6e37306e42cea8a9939b737c3d7
Miguel Velez <miguelvelez@mijecu25.com>
2019-12-09 14:39:44 -0500

    'compressed' gt berkeley configs to 1000 configs

---
+++ cc-compress/src/main/java/edu/cmu/cs/mvelezce/compress/gt/GTCompression.java
@@ -0,0 +1,41 @@
+package edu.cmu.cs.mvelezce.compress.gt;
+
+import edu.cmu.cs.mvelezce.compress.BaseCompression;
+import edu.cmu.cs.mvelezce.utils.config.Options;
+import edu.cmu.cs.mvelezce.utils.configurations.ConfigHelper;
+
+import java.util.List;
+import java.util.Set;
+
+public class GTCompression extends BaseCompression {
+
+  private static final String OUTPUT_DIR =
+      "../cc-compress/" + Options.DIRECTORY + "/compression/java/programs/gt";
+  private static final int ALL_CONFIGS = -1;
+
+  private final int configNumber;
+
+  public GTCompression(String programName, List<String> options) {
+    this(programName, options, ALL_CONFIGS);
+  }
+
+  public GTCompression(String programName, List<String> options, int configNumber) {
+    super(programName, options);
+
+    this.configNumber = configNumber;
+  }
+
+  @Override
+  public Set<Set<String>> analyze() {
+    if (this.configNumber == ALL_CONFIGS) {
+      return ConfigHelper.getConfigurations(this.getOptions());
+    }
+
+    return ConfigHelper.getRandomConfigs(this.getOptions(), this.configNumber);
+  }
+
+  @Override
+  public String outputDir() {
+    return OUTPUT_DIR;
+  }
+}
---
+++ cc-compress/src/main/java/edu/cmu/cs/mvelezce/compress/gt/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@ReturnTypesAreNonNullByDefault
+package edu.cmu.cs.mvelezce.compress.gt;
+
+import com.mijecu25.meme.utils.annotations.ReturnTypesAreNonNullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;

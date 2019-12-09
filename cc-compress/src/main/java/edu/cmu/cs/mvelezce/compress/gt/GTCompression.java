ede3cb755f358a3c79d80d801e688db3eb096f67
Miguel Velez <miguelvelez@mijecu25.com>
2019-12-09 14:45:02 -0500

    got the configs to execute with gt

--- cc-compress/src/main/java/edu/cmu/cs/mvelezce/compress/gt/GTCompression.java
+++ cc-compress/src/main/java/edu/cmu/cs/mvelezce/compress/gt/GTCompression.java
@@ -4,6 +4,7 @@ import edu.cmu.cs.mvelezce.compress.BaseCompression;
 import edu.cmu.cs.mvelezce.utils.config.Options;
 import edu.cmu.cs.mvelezce.utils.configurations.ConfigHelper;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -14,6 +15,10 @@ public class GTCompression extends BaseCompression {
   private static final int ALL_CONFIGS = -1;
 
   private final int configNumber;
+
+  public GTCompression(String programName) {
+    this(programName, new ArrayList<>(), 0);
+  }
 
   public GTCompression(String programName, List<String> options) {
     this(programName, options, ALL_CONFIGS);
--- cc-execute-e2e/src/test/java/edu/cmu/cs/mvelezce/e2e/execute/gt/GroundTruthExecutorTest.java
+++ cc-execute-e2e/src/test/java/edu/cmu/cs/mvelezce/e2e/execute/gt/GroundTruthExecutorTest.java
@@ -1,12 +1,12 @@
 package edu.cmu.cs.mvelezce.e2e.execute.gt;
 
 import edu.cmu.cs.mvelezce.adapters.measureDiskOrderedScan.BaseMeasureDiskOrderedScanAdapter;
+import edu.cmu.cs.mvelezce.compress.BaseCompression;
+import edu.cmu.cs.mvelezce.compress.gt.GTCompression;
 import edu.cmu.cs.mvelezce.java.execute.Executor;
-import edu.cmu.cs.mvelezce.utils.configurations.ConfigHelper;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Set;
 
 public class GroundTruthExecutorTest {
@@ -14,17 +14,15 @@ public class GroundTruthExecutorTest {
   @Test
   public void berkeleyDb() throws IOException, InterruptedException {
     String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
-    List<String> options = BaseMeasureDiskOrderedScanAdapter.getListOfOptions();
-    Set<Set<String>> configurations = ConfigHelper.getRandomConfigs(options, 1_000);
-    System.out.println("Sampling " + configurations.size() + " configs");
+    BaseCompression compression = new GTCompression(programName);
+    String[] args = new String[0];
+    Set<Set<String>> configs = compression.analyze(args);
+    Executor executor = new GroundTruthExecutor(programName, configs);
 
-    Executor executor = new GroundTruthExecutor(programName, configurations);
-
-    String[] args = new String[3];
+    args = new String[3];
     args[0] = "-delres";
     args[1] = "-saveres";
     args[2] = "-i5";
-
     executor.execute(args);
   }
 }

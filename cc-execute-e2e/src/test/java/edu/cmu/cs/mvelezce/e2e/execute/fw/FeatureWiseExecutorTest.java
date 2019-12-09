07b7c44eae87b3865f27b3e0f67d5df91a312b07
Miguel Velez <miguelvelez@mijecu25.com>
2019-12-09 15:06:26 -0500

    changed how to get configs for fw and pw

--- cc-execute-e2e/src/test/java/edu/cmu/cs/mvelezce/e2e/execute/fw/FeatureWiseExecutorTest.java
+++ cc-execute-e2e/src/test/java/edu/cmu/cs/mvelezce/e2e/execute/fw/FeatureWiseExecutorTest.java
@@ -1,12 +1,12 @@
 package edu.cmu.cs.mvelezce.e2e.execute.fw;
 
 import edu.cmu.cs.mvelezce.adapters.measureDiskOrderedScan.BaseMeasureDiskOrderedScanAdapter;
-import edu.cmu.cs.mvelezce.approaches.sampling.fw.FeatureWiseSampling;
+import edu.cmu.cs.mvelezce.compress.BaseCompression;
+import edu.cmu.cs.mvelezce.compress.fw.FWCompression;
 import edu.cmu.cs.mvelezce.java.execute.Executor;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Set;
 
 public class FeatureWiseExecutorTest {
@@ -14,15 +14,15 @@ public class FeatureWiseExecutorTest {
   @Test
   public void berkeleyDb() throws IOException, InterruptedException {
     String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
-    List<String> options = BaseMeasureDiskOrderedScanAdapter.getListOfOptions();
-    Set<Set<String>> configurations = FeatureWiseSampling.getInstance().getConfigs(options);
-    Executor executor = new FeatureWiseExecutor(programName, configurations);
+    BaseCompression compression = new FWCompression(programName);
+    String[] args = new String[0];
+    Set<Set<String>> configs = compression.analyze(args);
+    Executor executor = new FeatureWiseExecutor(programName, configs);
 
-    String[] args = new String[3];
+    args = new String[3];
     args[0] = "-delres";
     args[1] = "-saveres";
     args[2] = "-i5";
-
     executor.execute(args);
   }
 }
--- cc-execute-e2e/src/test/java/edu/cmu/cs/mvelezce/e2e/execute/pw/PairWiseExecutorTest.java
+++ cc-execute-e2e/src/test/java/edu/cmu/cs/mvelezce/e2e/execute/pw/PairWiseExecutorTest.java
@@ -1,12 +1,12 @@
 package edu.cmu.cs.mvelezce.e2e.execute.pw;
 
 import edu.cmu.cs.mvelezce.adapters.measureDiskOrderedScan.BaseMeasureDiskOrderedScanAdapter;
-import edu.cmu.cs.mvelezce.approaches.sampling.pw.PairWiseSampling;
+import edu.cmu.cs.mvelezce.compress.BaseCompression;
+import edu.cmu.cs.mvelezce.compress.gt.GTCompression;
 import edu.cmu.cs.mvelezce.java.execute.Executor;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Set;
 
 public class PairWiseExecutorTest {
@@ -14,15 +14,15 @@ public class PairWiseExecutorTest {
   @Test
   public void berkeleyDb() throws IOException, InterruptedException {
     String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
-    List<String> options = BaseMeasureDiskOrderedScanAdapter.getListOfOptions();
-    Set<Set<String>> configurations = PairWiseSampling.getInstance().getConfigs(options);
-    Executor executor = new PairWiseExecutor(programName, configurations);
+    BaseCompression compression = new GTCompression(programName);
+    String[] args = new String[0];
+    Set<Set<String>> configs = compression.analyze(args);
+    Executor executor = new PairWiseExecutor(programName, configs);
 
-    String[] args = new String[3];
+    args = new String[3];
     args[0] = "-delres";
     args[1] = "-saveres";
     args[2] = "-i5";
-
     executor.execute(args);
   }
 }

eb9b7bc4969f97d1fee53bb72079095c92f4ef01
Miguel Velez <miguelvelez@mijecu25.com>
2019-12-11 08:02:42 -0500

    averaged gt lucene

--- cc-execute-e2e/src/test/java/edu/cmu/cs/mvelezce/e2e/processor/aggregator/gt/GroundTruthPerfAggregatorProcessorTest.java
+++ cc-execute-e2e/src/test/java/edu/cmu/cs/mvelezce/e2e/processor/aggregator/gt/GroundTruthPerfAggregatorProcessorTest.java
@@ -1,5 +1,6 @@
 package edu.cmu.cs.mvelezce.e2e.processor.aggregator.gt;
 
+import edu.cmu.cs.mvelezce.adapters.indexFiles.BaseIndexFilesAdapter;
 import edu.cmu.cs.mvelezce.adapters.measureDiskOrderedScan.BaseMeasureDiskOrderedScanAdapter;
 import edu.cmu.cs.mvelezce.analysis.Analysis;
 import edu.cmu.cs.mvelezce.e2e.execute.gt.GroundTruthExecutor;
@@ -16,6 +17,21 @@ public class GroundTruthPerfAggregatorProcessorTest {
   @Test
   public void berkeleyDB() throws IOException, InterruptedException {
     String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    BaseExecutor<ProcessedPerfExecution> executor = new GroundTruthExecutor(programName);
+    Map<Integer, Set<ProcessedPerfExecution>> itersToResults =
+        executor.getRawExecutionParser().readResults();
+    Analysis perfAggregatorProcessor =
+        new GroundTruthPerfAggregatorProcessor(programName, itersToResults);
+
+    String[] args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    perfAggregatorProcessor.analyze(args);
+  }
+
+  @Test
+  public void lucene() throws IOException, InterruptedException {
+    String programName = BaseIndexFilesAdapter.PROGRAM_NAME;
     BaseExecutor<ProcessedPerfExecution> executor = new GroundTruthExecutor(programName);
     Map<Integer, Set<ProcessedPerfExecution>> itersToResults =
         executor.getRawExecutionParser().readResults();

4d87335bfd295ca642f33296d69032d5f1f3be41
Miguel Velez <miguelvelez@mijecu25.com>
2019-12-17 16:17:31 -0500

    got the number of configs to sample for lucene

--- cc-compress/src/test/java/edu/cmu/cs/mvelezce/compress/idta/suboptimal/greedy/conjunctions/IDTASuboptimalGreedyConjunctionsCompressionTest.java
+++ cc-compress/src/test/java/edu/cmu/cs/mvelezce/compress/idta/suboptimal/greedy/conjunctions/IDTASuboptimalGreedyConjunctionsCompressionTest.java
@@ -1,6 +1,7 @@
 package edu.cmu.cs.mvelezce.compress.idta.suboptimal.greedy.conjunctions;
 
 import de.fosd.typechef.featureexpr.FeatureExpr;
+import edu.cmu.cs.mvelezce.adapters.indexFiles.BaseIndexFilesAdapter;
 import edu.cmu.cs.mvelezce.adapters.measureDiskOrderedScan.BaseMeasureDiskOrderedScanAdapter;
 import edu.cmu.cs.mvelezce.adapters.trivial.BaseTrivialAdapter;
 import edu.cmu.cs.mvelezce.analysis.Analysis;
@@ -41,6 +42,26 @@ public class IDTASuboptimalGreedyConjunctionsCompressionTest {
   public void berkeleyDb() throws IOException, InterruptedException {
     String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
     List<String> options = BaseMeasureDiskOrderedScanAdapter.getListOfOptions();
+
+    String workloadSize = "small";
+    Analysis<Map<JavaRegion, Set<FeatureExpr>>> analysis =
+        new IDTAAnalysis(programName, workloadSize);
+    Collection<Set<FeatureExpr>> allConstraints = analysis.analyze().values();
+
+    BaseCompression compression =
+        new IDTASuboptimalGreedyConjunctionsCompression(programName, options, allConstraints);
+    String[] args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    Set<Set<String>> configs = compression.analyze(args);
+
+    System.out.println(configs.size() + " to sample");
+  }
+
+  @Test
+  public void lucene() throws IOException, InterruptedException {
+    String programName = BaseIndexFilesAdapter.PROGRAM_NAME;
+    List<String> options = BaseIndexFilesAdapter.getListOfOptions();
 
     String workloadSize = "small";
     Analysis<Map<JavaRegion, Set<FeatureExpr>>> analysis =

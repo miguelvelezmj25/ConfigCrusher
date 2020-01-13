0555aed9551f02fa1e5e2a06ba388c3470b06dd2
Miguel Velez <miguelvelez@mijecu25.com>
2020-01-13 10:06:27 -0500

    got the statistics to build an exhaustive performance model

--- cc-perf-model-exhaustive/src/main/java/edu/cmu/cs/mvelezce/exhaustive/builder/ExhaustiveModelBuilder.java
+++ cc-perf-model-exhaustive/src/main/java/edu/cmu/cs/mvelezce/exhaustive/builder/ExhaustiveModelBuilder.java
@@ -7,13 +7,12 @@ import edu.cmu.cs.mvelezce.builder.E2EModelBuilder;
 import edu.cmu.cs.mvelezce.exhaustive.model.constraint.ExhaustiveLocalPerformanceModel;
 import edu.cmu.cs.mvelezce.java.results.processed.PerformanceEntry;
 import edu.cmu.cs.mvelezce.model.LocalPerformanceModel;
+import edu.cmu.cs.mvelezce.model.MultiEntryLocalPerformanceModel;
 import edu.cmu.cs.mvelezce.model.PerformanceModel;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public abstract class ExhaustiveModelBuilder extends E2EModelBuilder {
 
@@ -48,5 +47,89 @@ public abstract class ExhaustiveModelBuilder extends E2EModelBuilder {
     }
 
     return new PerformanceModel<>(localModels);
+  }
+
+  @Override
+  protected void populateMultiEntryLocalModel(
+      MultiEntryLocalPerformanceModel<FeatureExpr> localModel) {
+    UUID programRegion = localModel.getRegion();
+
+    for (PerformanceEntry entry : this.getPerformanceEntries()) {
+      FeatureExpr configConstraint = this.getPerfEntryToExecConstraint().get(entry);
+
+      for (UUID regionUUID : entry.getRegionsToPerf().keySet()) {
+        if (!programRegion.equals(regionUUID)) {
+          throw new RuntimeException(
+              "Expected the performance entry to have thr program region '"
+                  + programRegion
+                  + "', but found '"
+                  + regionUUID
+                  + "'");
+        }
+
+        this.addEntry(
+            localModel.getModel(), configConstraint, entry.getRegionsToPerf().get(regionUUID));
+        this.addEntry(
+            localModel.getModelToMins(), configConstraint, entry.getRegionsToMin().get(regionUUID));
+        this.addEntry(
+            localModel.getModelToMaxs(), configConstraint, entry.getRegionsToMax().get(regionUUID));
+        this.addEntry(
+            localModel.getModelToDiffs(),
+            configConstraint,
+            entry.getRegionsToDiff().get(regionUUID));
+        this.addEntry(
+            localModel.getModelToSampleVariances(),
+            configConstraint,
+            entry.getRegionsToSampleVariance().get(regionUUID));
+        this.addEntry(
+            localModel.getModelToConfidenceIntervals(),
+            configConstraint,
+            entry.getRegionsToConfidenceInterval().get(regionUUID));
+      }
+    }
+  }
+
+  private void addEntry(
+      Map<FeatureExpr, Set<Double>> model, FeatureExpr configConstraint, double value) {
+    if (!model.containsKey(configConstraint)) {
+      throw new RuntimeException("Could not find config constraint " + configConstraint);
+    }
+
+    Set<Double> values = model.get(configConstraint);
+
+    if (!values.isEmpty()) {
+      throw new RuntimeException(
+          "Expected the entry of '"
+              + configConstraint
+              + "' to be empty, but found "
+              + values
+              + " and was about to add "
+              + value);
+    }
+
+    values.add(value);
+  }
+
+  private void addEntry(
+      Map<FeatureExpr, Set<List<Double>>> model,
+      FeatureExpr configConstraint,
+      List<Double> values) {
+    if (!model.containsKey(configConstraint)) {
+      throw new RuntimeException("Could not find config constraint " + configConstraint);
+    }
+
+    Set<List<Double>> entries = model.get(configConstraint);
+
+    if (!entries.isEmpty()) {
+      throw new RuntimeException(
+          "Expected the entry of '"
+              + configConstraint
+              + "' to be empty, but found "
+              + entries
+              + " and was about to add "
+              + values);
+    }
+
+    entries.add(values);
   }
 }

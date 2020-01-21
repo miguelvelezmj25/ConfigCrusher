f184d9b2ca451672dedff9cc5cabcac843503971
Miguel Velez <miguelvelez@mijecu25.com>
2020-01-21 10:07:40 -0500

    collected the entries of multiple models

--- cc-eval/src/main/java/edu/cmu/cs/mvelezce/eval/java/models/ModelsEvaluation.java
+++ cc-eval/src/main/java/edu/cmu/cs/mvelezce/eval/java/models/ModelsEvaluation.java
@@ -16,8 +16,57 @@ public abstract class ModelsEvaluation<T> {
   public void compare(Set<PerformanceModel<T>> models) {
     Set<Map<UUID, Map<T, Double>>> regionsToPerfModels = this.getRegionsToPerfModels(models);
     Set<UUID> regions = this.getRegions(regionsToPerfModels);
+    Map<UUID, Set<T>> allModelEntries = this.getAllModelEntries(regions, regionsToPerfModels);
+    Map<UUID, Map<T, List<Double>>> comparedModels =
+        this.comparePerfs(allModelEntries, regionsToPerfModels);
 
     throw new UnsupportedOperationException("implement");
+  }
+
+  private Map<UUID, Map<T, List<Double>>> comparePerfs(
+      Map<UUID, Set<T>> allModelEntries, Set<Map<UUID, Map<T, Double>>> regionsToPerfModels) {
+    Map<UUID, Map<T, List<Double>>> comparedModels = new HashMap<>();
+
+    for (Map.Entry<UUID, Set<T>> entry : allModelEntries.entrySet()) {
+      Map<T, List<Double>> comparisonMap = new HashMap<>();
+
+      for (T row : entry.getValue()) {
+        comparisonMap.put(row, new ArrayList<>());
+      }
+
+      comparedModels.put(entry.getKey(), comparisonMap);
+    }
+
+    for (Map<UUID, Map<T, Double>> oneRegionsToPerfModels : regionsToPerfModels) {
+      for (Map.Entry<UUID, Map<T, Double>> entry : oneRegionsToPerfModels.entrySet()) {
+        Map<T, List<Double>> comparisonMap = comparedModels.get(entry.getKey());
+
+        for (Map.Entry<T, Double> perfEntry : entry.getValue().entrySet()) {
+          comparisonMap.get(perfEntry.getKey()).add(perfEntry.getValue());
+        }
+      }
+    }
+
+    return comparedModels;
+  }
+
+  private Map<UUID, Set<T>> getAllModelEntries(
+      Set<UUID> regions, Set<Map<UUID, Map<T, Double>>> regionsToPerfModels) {
+    Map<UUID, Set<T>> entries = new HashMap<>();
+
+    for (UUID region : regions) {
+      entries.put(region, new HashSet<>());
+    }
+
+    for (Map<UUID, Map<T, Double>> oneRegionsToPerfModels : regionsToPerfModels) {
+
+      for (Map.Entry<UUID, Map<T, Double>> entry : oneRegionsToPerfModels.entrySet()) {
+        Set<T> modelEntries = entry.getValue().keySet();
+        entries.get(entry.getKey()).addAll(modelEntries);
+      }
+    }
+
+    return entries;
   }
 
   private Set<UUID> getRegions(Set<Map<UUID, Map<T, Double>>> regionsToPerfModels) {

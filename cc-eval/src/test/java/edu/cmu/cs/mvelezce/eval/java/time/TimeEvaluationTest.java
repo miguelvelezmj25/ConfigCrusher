67e3a6bc6de75caf97a43970acdc007e57e29ac4
Miguel Velez <miguelvelez@mijecu25.com>
2020-01-29 16:05:45 -0500

    measured the time to measure lucene with diff approaches

--- cc-eval/src/test/java/edu/cmu/cs/mvelezce/eval/java/time/TimeEvaluationTest.java
+++ cc-eval/src/test/java/edu/cmu/cs/mvelezce/eval/java/time/TimeEvaluationTest.java
@@ -1,5 +1,6 @@
 package edu.cmu.cs.mvelezce.eval.java.time;
 
+import edu.cmu.cs.mvelezce.adapters.indexFiles.BaseIndexFilesAdapter;
 import edu.cmu.cs.mvelezce.adapters.measureDiskOrderedScan.BaseMeasureDiskOrderedScanAdapter;
 import edu.cmu.cs.mvelezce.analysis.Analysis;
 import edu.cmu.cs.mvelezce.e2e.processor.aggregator.bf.BruteForcePerfAggregatorProcessor;
@@ -51,6 +52,50 @@ public class TimeEvaluationTest {
   @Test
   public void berkeleyDB_PW_MeasuredTime() throws IOException, InterruptedException {
     String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    Analysis<Set<PerformanceEntry>> perfAggregatorProcessor =
+        new PairWisePerfAggregatorProcessor(programName);
+    String[] args = new String[0];
+    Set<PerformanceEntry> performanceEntries = perfAggregatorProcessor.analyze(args);
+
+    TimeEvaluation.getE2EMeasuredTime(Evaluation.PW, performanceEntries);
+  }
+
+  @Test
+  public void lucene_GT_MeasuredTime() throws IOException, InterruptedException {
+    String programName = BaseIndexFilesAdapter.PROGRAM_NAME;
+    Analysis<Set<PerformanceEntry>> perfAggregatorProcessor =
+        new GroundTruthPerfAggregatorProcessor(programName);
+    String[] args = new String[0];
+    Set<PerformanceEntry> performanceEntries = perfAggregatorProcessor.analyze(args);
+
+    TimeEvaluation.getE2EMeasuredTime(Evaluation.GT, performanceEntries);
+  }
+
+  @Test
+  public void lucene_BF_MeasuredTime() throws IOException, InterruptedException {
+    String programName = BaseIndexFilesAdapter.PROGRAM_NAME;
+    Analysis<Set<PerformanceEntry>> perfAggregatorProcessor =
+        new BruteForcePerfAggregatorProcessor(programName);
+    String[] args = new String[0];
+    Set<PerformanceEntry> performanceEntries = perfAggregatorProcessor.analyze(args);
+
+    TimeEvaluation.getE2EMeasuredTime(Evaluation.BF, performanceEntries);
+  }
+
+  @Test
+  public void lucene_FW_MeasuredTime() throws IOException, InterruptedException {
+    String programName = BaseIndexFilesAdapter.PROGRAM_NAME;
+    Analysis<Set<PerformanceEntry>> perfAggregatorProcessor =
+        new FeatureWisePerfAggregatorProcessor(programName);
+    String[] args = new String[0];
+    Set<PerformanceEntry> performanceEntries = perfAggregatorProcessor.analyze(args);
+
+    TimeEvaluation.getE2EMeasuredTime(Evaluation.FW, performanceEntries);
+  }
+
+  @Test
+  public void lucene_PW_MeasuredTime() throws IOException, InterruptedException {
+    String programName = BaseIndexFilesAdapter.PROGRAM_NAME;
     Analysis<Set<PerformanceEntry>> perfAggregatorProcessor =
         new PairWisePerfAggregatorProcessor(programName);
     String[] args = new String[0];

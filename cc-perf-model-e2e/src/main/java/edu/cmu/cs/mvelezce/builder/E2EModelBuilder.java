14a5eb962b5d8acb94ec315826da8093d017889d
Miguel Velez <miguelvelez@mijecu25.com>
2020-01-31 09:32:34 -0500

    migrated partitions to cc-perf-model-e2e

--- cc-perf-model-e2e/src/main/java/edu/cmu/cs/mvelezce/builder/E2EModelBuilder.java
+++ cc-perf-model-e2e/src/main/java/edu/cmu/cs/mvelezce/builder/E2EModelBuilder.java
@@ -1,9 +1,11 @@
 package edu.cmu.cs.mvelezce.builder;
 
-import de.fosd.typechef.featureexpr.FeatureExpr;
 import edu.cmu.cs.mvelezce.analysis.region.java.JavaRegion;
-import edu.cmu.cs.mvelezce.builder.constraint.BaseConstraintPerformanceModelBuilder;
+import edu.cmu.cs.mvelezce.builder.partition.BasePartitionPerformanceModelBuilder;
 import edu.cmu.cs.mvelezce.explorer.idta.IDTA;
+import edu.cmu.cs.mvelezce.explorer.idta.partition.Partition;
+import edu.cmu.cs.mvelezce.explorer.idta.partition.Partitioning;
+import edu.cmu.cs.mvelezce.explorer.idta.partition.TotalPartition;
 import edu.cmu.cs.mvelezce.explorer.utils.ConstraintUtils;
 import edu.cmu.cs.mvelezce.explorer.utils.FeatureExprUtils;
 import edu.cmu.cs.mvelezce.java.results.processed.PerformanceEntry;
@@ -11,9 +13,9 @@ import edu.cmu.cs.mvelezce.region.RegionsManager;
 
 import java.util.*;
 
-public abstract class E2EModelBuilder extends BaseConstraintPerformanceModelBuilder {
+public abstract class E2EModelBuilder extends BasePartitionPerformanceModelBuilder {
 
-  protected static final Map<JavaRegion, Set<FeatureExpr>> REGIONS_TO_DATA = new HashMap<>();
+  protected static final Map<JavaRegion, Partitioning> REGIONS_TO_DATA = new HashMap<>();
 
   public E2EModelBuilder(
       String programName, List<String> options, Set<PerformanceEntry> performanceEntries) {
@@ -23,16 +25,17 @@ public abstract class E2EModelBuilder extends BaseConstraintPerformanceModelBuil
   }
 
   private void addProgramRegionToData() {
-    Set<FeatureExpr> constraints = new HashSet<>();
+    Set<Partition> partitions = new HashSet<>();
     List<String> options = this.getOptions();
 
     for (PerformanceEntry entry : this.getPerformanceEntries()) {
       Set<String> config = entry.getConfiguration();
-      String configConstraint = ConstraintUtils.parseAsConstraint(config, options);
-      FeatureExpr constraint = FeatureExprUtils.parseAsFeatureExpr(IDTA.USE_BDD, configConstraint);
-      constraints.add(constraint);
+      String configPartition = ConstraintUtils.parseAsConstraint(config, options);
+      Partition partition =
+          new Partition(FeatureExprUtils.parseAsFeatureExpr(IDTA.USE_BDD, configPartition));
+      partitions.add(partition);
     }
 
-    REGIONS_TO_DATA.put(RegionsManager.PROGRAM_REGION, constraints);
+    REGIONS_TO_DATA.put(RegionsManager.PROGRAM_REGION, new TotalPartition(partitions));
   }
 }

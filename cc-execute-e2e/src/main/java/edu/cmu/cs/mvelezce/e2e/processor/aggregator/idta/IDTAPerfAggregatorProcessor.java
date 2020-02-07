030d24e1b12207f49f7f434d0ed408bce6d614f1
Miguel Velez <miguelvelez@mijecu25.com>
2020-02-07 09:32:46 -0500

    averaged e2e berkeley with idta configs

---
+++ cc-execute-e2e/src/main/java/edu/cmu/cs/mvelezce/e2e/processor/aggregator/idta/IDTAPerfAggregatorProcessor.java
@@ -0,0 +1,28 @@
+package edu.cmu.cs.mvelezce.e2e.processor.aggregator.idta;
+
+import edu.cmu.cs.mvelezce.e2e.execute.idta.IDTAExecutor;
+import edu.cmu.cs.mvelezce.java.processor.aggregator.PerfAggregatorProcessor;
+import edu.cmu.cs.mvelezce.java.results.processed.ProcessedPerfExecution;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class IDTAPerfAggregatorProcessor extends PerfAggregatorProcessor {
+
+  private static final String OUTPUT_DIR = IDTAExecutor.OUTPUT_DIR;
+
+  public IDTAPerfAggregatorProcessor(String programName) {
+    this(programName, new HashMap<>());
+  }
+
+  IDTAPerfAggregatorProcessor(
+      String programName, Map<Integer, Set<ProcessedPerfExecution>> itersToProcessedPerfExecution) {
+    super(programName, itersToProcessedPerfExecution);
+  }
+
+  @Override
+  public String outputDir() {
+    return OUTPUT_DIR;
+  }
+}
---
+++ cc-execute-e2e/src/test/java/edu/cmu/cs/mvelezce/e2e/processor/aggregator/idta/IDTAPerfAggregatorProcessorTest.java
@@ -0,0 +1,29 @@
+package edu.cmu.cs.mvelezce.e2e.processor.aggregator.idta;
+
+import edu.cmu.cs.mvelezce.adapters.measureDiskOrderedScan.BaseMeasureDiskOrderedScanAdapter;
+import edu.cmu.cs.mvelezce.analysis.Analysis;
+import edu.cmu.cs.mvelezce.e2e.execute.idta.IDTAExecutor;
+import edu.cmu.cs.mvelezce.java.execute.BaseExecutor;
+import edu.cmu.cs.mvelezce.java.results.processed.ProcessedPerfExecution;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+
+public class IDTAPerfAggregatorProcessorTest {
+
+  @Test
+  public void berkeleyDB() throws IOException, InterruptedException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    BaseExecutor<ProcessedPerfExecution> executor = new IDTAExecutor(programName);
+    Map<Integer, Set<ProcessedPerfExecution>> itersToResults =
+        executor.getRawExecutionParser().readResults();
+    Analysis perfAggregatorProcessor = new IDTAPerfAggregatorProcessor(programName, itersToResults);
+
+    String[] args = new String[2];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    perfAggregatorProcessor.analyze(args);
+  }
+}

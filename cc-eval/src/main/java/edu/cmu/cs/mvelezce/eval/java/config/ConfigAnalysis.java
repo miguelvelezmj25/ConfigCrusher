b3410fae23d5a219d9145d849ee84725ca4504fa
Miguel Velez <miguelvelez@mijecu25.com>
2020-02-13 15:42:49 -0500

    added analysis to compare the measured time of regions and the predicted time of regions from the model

---
+++ cc-eval/src/main/java/edu/cmu/cs/mvelezce/eval/java/config/ConfigAnalysis.java
@@ -0,0 +1,116 @@
+package edu.cmu.cs.mvelezce.eval.java.config;
+
+import edu.cmu.cs.mvelezce.eval.java.Evaluation;
+import edu.cmu.cs.mvelezce.explorer.idta.partition.Partition;
+import edu.cmu.cs.mvelezce.java.results.processed.PerformanceEntry;
+import edu.cmu.cs.mvelezce.model.LocalPerformanceModel;
+import edu.cmu.cs.mvelezce.model.PerformanceModel;
+import edu.cmu.cs.mvelezce.utils.config.Options;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+public final class ConfigAnalysis {
+
+  private static final String OUTPUT_DIR =
+      "../cc-eval/" + Options.DIRECTORY + "/eval/java/programs/config/";
+
+  private final String programName;
+  private final List<String> options;
+
+  public ConfigAnalysis(String programName, List<String> options) {
+    this.programName = programName;
+    this.options = options;
+  }
+
+  public void compareMeasurementAndPrediction(
+      String approach,
+      Set<PerformanceEntry> performanceEntries,
+      PerformanceModel<Partition> model,
+      Set<String> config)
+      throws IOException {
+    Map<UUID, Double> measuredTimes = this.getMeasuredTimes(performanceEntries, config);
+    Map<UUID, Double> predictedTimes = this.getPredictedTimes(model, config);
+
+    if (!measuredTimes.keySet().equals(predictedTimes.keySet())) {
+      throw new RuntimeException(
+          "The regions do not match between the profiled times and the predicted times");
+    }
+
+    double thresholdToPrint = 1E6;
+    StringBuilder result = new StringBuilder();
+    result.append("region,measured,predicted,diff");
+    result.append("\n");
+
+    for (UUID region : measuredTimes.keySet()) {
+      double measuredTime = measuredTimes.get(region);
+      double predictedTime = predictedTimes.get(region);
+
+      if (measuredTime < thresholdToPrint && predictedTime < thresholdToPrint) {
+        continue;
+      }
+
+      double diff = Math.abs(predictedTime - measuredTime);
+
+      result.append(region);
+      result.append(",");
+      result.append(Evaluation.DECIMAL_FORMAT.format(measuredTime / 1E9));
+      result.append(",");
+      result.append(Evaluation.DECIMAL_FORMAT.format(predictedTime / 1E9));
+      result.append(",");
+      result.append(Evaluation.DECIMAL_FORMAT.format(diff / 1E9));
+      result.append("\n");
+    }
+
+    File outputFile =
+        new File(
+            OUTPUT_DIR
+                + "/"
+                + this.programName
+                + "/"
+                + approach
+                + "/"
+                + config
+                + Evaluation.DOT_CSV);
+
+    outputFile.getParentFile().mkdirs();
+
+    if (outputFile.exists()) {
+      FileUtils.forceDelete(outputFile);
+    }
+
+    PrintWriter writer = new PrintWriter(outputFile);
+    writer.write(result.toString());
+    writer.flush();
+    writer.close();
+  }
+
+  private Map<UUID, Double> getPredictedTimes(
+      PerformanceModel<Partition> model, Set<String> config) {
+    Map<UUID, Double> predictedTimes = new HashMap<>();
+
+    for (LocalPerformanceModel<Partition> localModel : model.getLocalModels()) {
+      predictedTimes.put(localModel.getRegion(), localModel.evaluate(config, this.options));
+    }
+
+    return predictedTimes;
+  }
+
+  private Map<UUID, Double> getMeasuredTimes(
+      Set<PerformanceEntry> performanceEntries, Set<String> config) {
+    Map<UUID, Double> measuredTimes = new HashMap<>();
+
+    for (PerformanceEntry entry : performanceEntries) {
+      if (!entry.getConfiguration().equals(config)) {
+        continue;
+      }
+
+      measuredTimes = entry.getRegionsToPerf();
+    }
+
+    return measuredTimes;
+  }
+}
---
+++ cc-eval/src/test/java/edu/cmu/cs/mvelezce/eval/java/config/ConfigAnalysisTest.java
@@ -0,0 +1,42 @@
+package edu.cmu.cs.mvelezce.eval.java.config;
+
+import edu.cmu.cs.mvelezce.adapters.indexFiles.BaseIndexFilesAdapter;
+import edu.cmu.cs.mvelezce.analysis.Analysis;
+import edu.cmu.cs.mvelezce.analysis.BaseAnalysis;
+import edu.cmu.cs.mvelezce.builder.idta.IDTAPerformanceModelBuilder;
+import edu.cmu.cs.mvelezce.eval.java.Evaluation;
+import edu.cmu.cs.mvelezce.explorer.idta.partition.Partition;
+import edu.cmu.cs.mvelezce.java.processor.aggregator.sampling.profiler.jprofiler.idta.IDTAPerfAggregatorProcessor;
+import edu.cmu.cs.mvelezce.java.results.processed.PerformanceEntry;
+import edu.cmu.cs.mvelezce.model.PerformanceModel;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ConfigAnalysisTest {
+
+  @Test
+  public void compare_idta_model_lucene_0() throws IOException, InterruptedException {
+    String programName = BaseIndexFilesAdapter.PROGRAM_NAME;
+    Analysis<Set<PerformanceEntry>> perfAggregatorProcessor =
+        new IDTAPerfAggregatorProcessor(programName);
+    String[] args = new String[0];
+    Set<PerformanceEntry> performanceEntries = perfAggregatorProcessor.analyze(args);
+
+    BaseAnalysis<PerformanceModel<Partition>> builder =
+        new IDTAPerformanceModelBuilder(programName);
+    args = new String[0];
+    PerformanceModel<Partition> model = builder.analyze(args);
+
+    List<String> options = BaseIndexFilesAdapter.getListOfOptions();
+    ConfigAnalysis configAnalysis = new ConfigAnalysis(programName, options);
+
+    Set<String> config = new HashSet<>();
+    config.add("RAM_BUFFER_SIZE_MB");
+    configAnalysis.compareMeasurementAndPrediction(
+        Evaluation.IDTA, performanceEntries, model, config);
+  }
+}

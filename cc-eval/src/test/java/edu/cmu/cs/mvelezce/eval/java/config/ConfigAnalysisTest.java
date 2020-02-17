ce8abe0c0fbc05587a8574e80f9c7001cf8d8ac5
Miguel Velez <miguelvelez@mijecu25.com>
2020-02-17 13:03:04 -0500

    compared predicted vs measured local models for berkeley

--- cc-eval/src/test/java/edu/cmu/cs/mvelezce/eval/java/config/ConfigAnalysisTest.java
+++ cc-eval/src/test/java/edu/cmu/cs/mvelezce/eval/java/config/ConfigAnalysisTest.java
@@ -1,6 +1,7 @@
 package edu.cmu.cs.mvelezce.eval.java.config;
 
 import edu.cmu.cs.mvelezce.adapters.indexFiles.BaseIndexFilesAdapter;
+import edu.cmu.cs.mvelezce.adapters.measureDiskOrderedScan.BaseMeasureDiskOrderedScanAdapter;
 import edu.cmu.cs.mvelezce.analysis.Analysis;
 import edu.cmu.cs.mvelezce.analysis.BaseAnalysis;
 import edu.cmu.cs.mvelezce.builder.idta.IDTAPerformanceModelBuilder;
@@ -71,6 +72,32 @@ public class ConfigAnalysisTest {
     PerformanceModel<Partition> model = builder.analyze(args);
 
     List<String> options = BaseIndexFilesAdapter.getListOfOptions();
+    ConfigAnalysis configAnalysis = new ConfigAnalysis(programName, options);
+
+    BaseCompression compression = new IDTASuboptimalGreedyConjunctionsCompression(programName);
+    args = new String[0];
+    Set<Set<String>> executedConfigs = compression.analyze(args);
+
+    for (Set<String> config : executedConfigs) {
+      configAnalysis.compareMeasurementAndPrediction(
+          Evaluation.IDTA, performanceEntries, model, config);
+    }
+  }
+
+  @Test
+  public void compare_idta_model_berkeley_all() throws IOException, InterruptedException {
+    String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    Analysis<Set<PerformanceEntry>> perfAggregatorProcessor =
+        new IDTAPerfAggregatorProcessor(programName);
+    String[] args = new String[0];
+    Set<PerformanceEntry> performanceEntries = perfAggregatorProcessor.analyze(args);
+
+    BaseAnalysis<PerformanceModel<Partition>> builder =
+        new IDTAPerformanceModelBuilder(programName);
+    args = new String[0];
+    PerformanceModel<Partition> model = builder.analyze(args);
+
+    List<String> options = BaseMeasureDiskOrderedScanAdapter.getListOfOptions();
     ConfigAnalysis configAnalysis = new ConfigAnalysis(programName, options);
 
     BaseCompression compression = new IDTASuboptimalGreedyConjunctionsCompression(programName);

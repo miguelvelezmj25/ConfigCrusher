71d00885e5cab377352c3f34a00637c54aa97b79
Miguel Velez <miguelvelez@mijecu25.com>
2020-02-18 11:29:32 -0500

    analyzed the overhead of sampling lucene

--- cc-eval/src/test/java/edu/cmu/cs/mvelezce/eval/java/sampling/profiler/jprofiler/JProfilerOverheadAnalysisTest.java
+++ cc-eval/src/test/java/edu/cmu/cs/mvelezce/eval/java/sampling/profiler/jprofiler/JProfilerOverheadAnalysisTest.java
@@ -1,5 +1,6 @@
 package edu.cmu.cs.mvelezce.eval.java.sampling.profiler.jprofiler;
 
+import edu.cmu.cs.mvelezce.adapters.indexFiles.BaseIndexFilesAdapter;
 import edu.cmu.cs.mvelezce.adapters.measureDiskOrderedScan.BaseMeasureDiskOrderedScanAdapter;
 import edu.cmu.cs.mvelezce.analysis.Analysis;
 import edu.cmu.cs.mvelezce.e2e.processor.aggregator.idta.IDTAE2EPerfAggregatorProcessor;
@@ -15,6 +16,22 @@ public class JProfilerOverheadAnalysisTest {
   @Test
   public void berkeleyDB() throws IOException, InterruptedException {
     String programName = BaseMeasureDiskOrderedScanAdapter.PROGRAM_NAME;
+    Analysis<Set<PerformanceEntry>> perfAggregatorProcessor =
+        new IDTAPerfAggregatorProcessor(programName);
+    String[] args = new String[0];
+    Set<PerformanceEntry> idtaPerfEntries = perfAggregatorProcessor.analyze(args);
+
+    perfAggregatorProcessor = new IDTAE2EPerfAggregatorProcessor(programName);
+    args = new String[0];
+    Set<PerformanceEntry> e2ePerfEntries = perfAggregatorProcessor.analyze(args);
+
+    JProfilerOverheadAnalysis analysis = new JProfilerOverheadAnalysis(programName);
+    analysis.analyze(idtaPerfEntries, e2ePerfEntries);
+  }
+
+  @Test
+  public void lucene() throws IOException, InterruptedException {
+    String programName = BaseIndexFilesAdapter.PROGRAM_NAME;
     Analysis<Set<PerformanceEntry>> perfAggregatorProcessor =
         new IDTAPerfAggregatorProcessor(programName);
     String[] args = new String[0];

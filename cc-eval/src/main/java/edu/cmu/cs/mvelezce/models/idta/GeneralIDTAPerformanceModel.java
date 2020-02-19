c3ec9fd0dcccccdfbeaba661e09e109528f23a4a
Miguel Velez <miguelvelez@mijecu25.com>
2020-02-19 09:31:36 -0500

    added general idta performance model that takes into account overhead of profiling

---
+++ cc-eval/src/main/java/edu/cmu/cs/mvelezce/models/idta/GeneralIDTAPerformanceModel.java
@@ -0,0 +1,23 @@
+package edu.cmu.cs.mvelezce.models.idta;
+
+import edu.cmu.cs.mvelezce.model.LocalPerformanceModel;
+import edu.cmu.cs.mvelezce.model.PerformanceModel;
+import edu.cmu.cs.mvelezce.model.idta.IDTAPerformanceModel;
+
+import java.util.Set;
+
+public class GeneralIDTAPerformanceModel<T> extends IDTAPerformanceModel<T> {
+
+  private static final double INTERCEPT = 0;
+  private static final double SLOPE = 1;
+  //  private static final double R_SQUARED = 0.9988;
+
+  public GeneralIDTAPerformanceModel(Set<LocalPerformanceModel<T>> localModels) {
+    super(localModels, INTERCEPT, SLOPE);
+  }
+
+  public static <T> GeneralIDTAPerformanceModel<T> toGeneralIDTAPerformanceModel(
+      PerformanceModel<T> performanceModel) {
+    return new GeneralIDTAPerformanceModel<>(performanceModel.getLocalModels());
+  }
+}

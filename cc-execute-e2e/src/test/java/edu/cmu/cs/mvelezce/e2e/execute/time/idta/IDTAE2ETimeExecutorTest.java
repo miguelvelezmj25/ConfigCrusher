7cb261f0d4d5957e8c546818609cd7e2d51ff506
Miguel Velez <miguelvelez@mijecu25.com>
2020-03-19 10:09:44 -0400

    measured h2 configs

--- cc-execute-e2e/src/test/java/edu/cmu/cs/mvelezce/e2e/execute/time/idta/IDTAE2ETimeExecutorTest.java
+++ cc-execute-e2e/src/test/java/edu/cmu/cs/mvelezce/e2e/execute/time/idta/IDTAE2ETimeExecutorTest.java
@@ -1,6 +1,7 @@
 package edu.cmu.cs.mvelezce.e2e.execute.time.idta;
 
 import edu.cmu.cs.mvelezce.adapters.convert.BaseConvertAdapter;
+import edu.cmu.cs.mvelezce.adapters.runBenchC.BaseRunBenchCAdapter;
 import edu.cmu.cs.mvelezce.compress.BaseCompression;
 import edu.cmu.cs.mvelezce.compress.idta.suboptimal.greedy.conjunctions.IDTASuboptimalGreedyConjunctionsCompression;
 import edu.cmu.cs.mvelezce.java.execute.Executor;
@@ -23,6 +24,21 @@ public class IDTAE2ETimeExecutorTest {
     args[0] = "-delres";
     args[1] = "-saveres";
     args[2] = "-i5";
+    executor.execute(args);
+  }
+
+  @Test
+  public void runBenchC() throws IOException, InterruptedException {
+    String programName = BaseRunBenchCAdapter.PROGRAM_NAME;
+    BaseCompression compression = new IDTASuboptimalGreedyConjunctionsCompression(programName);
+    String[] args = new String[0];
+    Set<Set<String>> configs = compression.analyze(args);
+    Executor executor = new IDTAE2ETimeExecutor(programName, configs, 30000);
+
+    args = new String[3];
+    args[0] = "-delres";
+    args[1] = "-saveres";
+    args[2] = "-i3";
     executor.execute(args);
   }
 }

fc3929492fd1c2e0391c7d30bbc1f5f43a624779
Miguel Velez <miguelvelez@mijecu25.com>
2020-04-08 08:35:42 -0400

    removed profiler overhead for berkeley db

--- cc-eval/src/main/java/edu/cmu/cs/mvelezce/models/idta/BerkeleyIDTAPerformanceModel.java
+++ cc-eval/src/main/java/edu/cmu/cs/mvelezce/models/idta/BerkeleyIDTAPerformanceModel.java
@@ -8,9 +8,9 @@ import java.util.Set;
 
 public class BerkeleyIDTAPerformanceModel<T> extends IDTAPerformanceModel<T> {
 
-  private static final double INTERCEPT = 0;
-  private static final double SLOPE = 1;
-  private static final double R_SQUARED = 0;
+  //  private static final double INTERCEPT = 0;
+  //  private static final double SLOPE = 1;
+  //  private static final double R_SQUARED = 0;
 
   // all configs
   //  private static final double INTERCEPT = -0.0983;
@@ -18,25 +18,25 @@ public class BerkeleyIDTAPerformanceModel<T> extends IDTAPerformanceModel<T> {
   //  private static final double R_SQUARED = 0.9988;
 
   // 5 random configs
-  //  private static final double INTERCEPT = -0.0754;
-  //  private static final double SLOPE = 1.0785;
-  //  private static final double R_SQUARED = 0.9997;
+  //    private static final double INTERCEPT = -0.3169;
+  //    private static final double SLOPE = 1.0933;
+  //    private static final double R_SQUARED = 0.9995;
 
-  //      private static final double INTERCEPT = -0.0522;
-  //      private static final double SLOPE = 1.0857;
-  //      private static final double R_SQUARED = 0.9992;
+  //        private static final double INTERCEPT = -0.0607;
+  //        private static final double SLOPE = 1.0598;
+  //        private static final double R_SQUARED = 0.9979;
 
-  //      private static final double INTERCEPT = 0.9102;
-  //      private static final double SLOPE = 0.9534;
-  //      private static final double R_SQUARED = 0.9928;
+  //        private static final double INTERCEPT = 0.2838;
+  //        private static final double SLOPE = 1.0261;
+  //        private static final double R_SQUARED = 0.9985;
 
-  //      private static final double INTERCEPT = -0.0454;
-  //      private static final double SLOPE = 1.0642;
-  //      private static final double R_SQUARED = 0.9991;
+  //        private static final double INTERCEPT = 0.2404;
+  //        private static final double SLOPE = 1.0316;
+  //        private static final double R_SQUARED = 0.9996;
 
-  //  private static final double INTERCEPT = 0.1364;
-  //  private static final double SLOPE = 1.0311;
-  //  private static final double R_SQUARED = 0.9994;
+  private static final double INTERCEPT = -0.3195;
+  private static final double SLOPE = 1.0881;
+  private static final double R_SQUARED = 0.9998;
 
   public BerkeleyIDTAPerformanceModel(Set<LocalPerformanceModel<T>> localModels) {
     super(localModels, INTERCEPT, SLOPE);
